a97b2cc4e2cf29da2c342a2e717b87dfb674d18c
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-01-03 16:25:45 +0900

    Replace SPACES->TAB with go fmt & bugfix Remove

--- tstConio/tstConio.go
+++ tstConio/tstConio.go
@@ -1,9 +1,10 @@
 package main
+
 import "../conio"
 import "fmt"
 
-func main(){
-    fmt.Print("conio.ReadLine>")
-    result := conio.ReadLine()
-    fmt.Println("Result=" + result)
+func main() {
+	fmt.Print("conio.ReadLine>")
+	result := conio.ReadLine()
+	fmt.Println("Result=" + result)
 }
