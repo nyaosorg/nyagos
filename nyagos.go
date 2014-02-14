f962f54ae6ea9fd87ad12bcecf3aeae6248d98c0
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-02-14 19:45:03 +0900

    Prototype NYAGOS released

---
+++ nyagos.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+import "./conio"
+import "./interpreter"
+
+func main(){
+	for{
+		fmt.Print("$ ")
+		line := conio.ReadLine()
+		if line == "exit" {
+			break
+		}
+		// fmt.Println(line)
+		_ , err := interpreter.Interpret(line)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
