7a7fd31f0a1ca73054c62a35b349ac80f25be941
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-02-22 20:30:05 +0900

    completion worked except for \ starting.(c:\ ok)

--- completion/completion.go
+++ completion/completion.go
@@ -5,12 +5,20 @@ import "fmt"
 import "os"
 import "path"
 import "strings"
+import "unicode"
 
 import "../box"
 import "../conio"
 
 func listUpFiles(str string) ([]string, error) {
-	directory := path.Dir(str)
+	str = strings.Replace(strings.Replace(str, "\\", "/", -1), "\"", "", -1)
+	var directory string
+	str = strings.Replace(str, "\\", "/", -1)
+	if strings.HasSuffix(str, "/") {
+		directory = path.Join(str, ".")
+	} else {
+		directory = path.Dir(str)
+	}
 	fd, err := os.Open(directory)
 	if err != nil {
 		return nil, err
@@ -21,10 +29,13 @@ func listUpFiles(str string) ([]string, error) {
 		return nil, err2
 	}
 	commons := make([]string, 0)
-	STR := strings.Replace(strings.ToUpper(str), "\\", "/", -1)
+	STR := strings.ToUpper(str)
 	for _, node1 := range files {
 		name := path.Join(directory, node1.Name())
-		NAME := strings.Replace(strings.ToUpper(name), "\\", "/", -1)
+		if node1.IsDir() {
+			name += "/"
+		}
+		NAME := strings.ToUpper(name)
 		if strings.HasPrefix(NAME, STR) {
 			commons = append(commons, name)
 		}
@@ -44,23 +55,74 @@ func KeyFuncCompletionList(this *conio.ReadLineBuffer) conio.KeyFuncResult {
 	return conio.CONTINUE
 }
 
+func getCommmon(list []string) string {
+	common := list[0]
+	for _, f := range list[1:] {
+		cr := strings.NewReader(common)
+		fr := strings.NewReader(f)
+		i := 0
+		var buffer bytes.Buffer
+		for {
+			ch, _, cerr := cr.ReadRune()
+			fh, _, ferr := fr.ReadRune()
+			if cerr != nil || ferr != nil || unicode.ToUpper(ch) != unicode.ToUpper(fh) {
+				break
+			}
+			buffer.WriteRune(ch)
+			i++
+		}
+		common = buffer.String()
+	}
+	return common
+}
+
+func compareWithoutQuote(this string, that string) bool {
+	return strings.Replace(this, "\"", "", -1) == strings.Replace(that, "\"", "", -1)
+}
+
 func KeyFuncCompletion(this *conio.ReadLineBuffer) conio.KeyFuncResult {
 	str, wordStart := this.CurrentWord()
+
+	slashToBackSlash := true
+	firstFoundSlashPos := strings.IndexRune(str, '/')
+	firstFoundBackSlashPos := strings.IndexRune(str, '\\')
+	if firstFoundSlashPos >= 0 && firstFoundBackSlashPos >= 0 && firstFoundSlashPos < firstFoundBackSlashPos {
+		slashToBackSlash = false
+	}
+
 	list, err := listUpFiles(str)
-	if err == nil {
-		if len(list) == 1 {
-			str = list[0]
-			if strings.ContainsRune(str, ' ') {
-				var buffer bytes.Buffer
-				buffer.WriteRune('"')
-				buffer.WriteString(str)
-				buffer.WriteRune('"')
-				str = buffer.String()
+	if err != nil || len(list) <= 0 {
+		return conio.CONTINUE
+	}
+	commonStr := getCommmon(list)
+	needQuote := strings.ContainsRune(str, '"')
+	if !needQuote {
+		for _, node := range list {
+			if strings.ContainsRune(node, ' ') {
+				needQuote = true
+				break
 			}
-			this.ReplaceAndRepaint(wordStart,str)
-		} else {
-			return KeyFuncCompletionList(this)
 		}
 	}
+	if needQuote {
+		var buffer bytes.Buffer
+		buffer.WriteRune('"')
+		buffer.WriteString(commonStr)
+		if len(list) <= 1 {
+			buffer.WriteRune('"')
+		}
+		commonStr = buffer.String()
+	}
+	if len(list) == 1 && ! strings.HasSuffix(commonStr,"/") && ! strings.HasSuffix(commonStr,"/\"") {
+		commonStr += " "
+	}
+	if slashToBackSlash {
+		commonStr = strings.Replace(commonStr, "/", "\\", -1)
+	}
+	if compareWithoutQuote(str, commonStr) {
+		return KeyFuncCompletionList(this)
+	}
+	this.ReplaceAndRepaint(wordStart, commonStr)
+	conio.PutRep('\a', 1)
 	return conio.CONTINUE
 }
