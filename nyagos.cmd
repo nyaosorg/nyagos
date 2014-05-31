d68879d054e82c3a90f42a88308684036dd85f73
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-05-31 23:03:45 +0900

    起動バッチ : . EXEは起動バッチと同じ場所にあれば実行できるようにした。

--- nyagos.cmd
+++ nyagos.cmd
@@ -2,8 +2,8 @@
 rem --- batchfile for test ---
 set PROMPT=$e[34;40;1m$L$P$G$_$$ $e[37;1m
 set OPTION=-a "ls=ls -oF"
-if exist nyagos.exe (
-    nyagos.exe %OPTION%
+if exist %~dp0nyagos.exe (
+    %~dp0nyagos.exe %OPTION%
 ) else (
     go run nyagos.go %OPTION%
 )
