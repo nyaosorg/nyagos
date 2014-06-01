526206bc4b1e857ada8bcd50ed6b9e1ce51b323b
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-06-02 00:53:05 +0900

    alias で $*,$0,$1,$2,$3 などのパラメータを使えるようにした。

--- alias/alias.go
+++ alias/alias.go
@@ -2,30 +2,54 @@ package alias
 
 import "bytes"
 import "os/exec"
+import "regexp"
+import "strconv"
 import "strings"
 
-import "../interpreter"
 import "../builtincmd"
+import "../interpreter"
 
 var Table = map[string]string{}
+var paramMatch = regexp.MustCompile("\\$(\\*|[0-9]+)")
 
 func Hook(cmd *exec.Cmd, IsBackground bool) (interpreter.WhatToDoAfterCmd, error) {
 	baseStr, ok := Table[strings.ToLower(cmd.Args[0])]
 	if !ok {
 		return interpreter.THROUGH, nil
 	}
-	var buffer bytes.Buffer
-	buffer.WriteString(baseStr)
-	for _, arg := range cmd.Args[1:] {
-		buffer.WriteRune(' ')
-		buffer.WriteString(arg)
+	isReplaced := false
+	cmdline := paramMatch.ReplaceAllStringFunc(baseStr, func(s string) string {
+		if s == "$*" {
+			isReplaced = true
+			return strings.Join(cmd.Args[1:], " ")
+		}
+		i, err := strconv.ParseInt(s[1:], 10, 0)
+		if err == nil {
+			isReplaced = true
+			if 0 <= i && int(i) < len(cmd.Args) {
+				return cmd.Args[i]
+			}
+		}
+		return s
+	})
+
+	if !isReplaced {
+		var buffer bytes.Buffer
+		buffer.WriteString(baseStr)
+
+		for _, arg := range cmd.Args[1:] {
+			buffer.WriteRune(' ')
+			buffer.WriteString(arg)
+		}
+
+		cmdline = buffer.String()
 	}
 	var stdio interpreter.Stdio
 	stdio.Stdin = cmd.Stdin
 	stdio.Stdout = cmd.Stdout
 	stdio.Stderr = cmd.Stderr
 	return interpreter.Interpret(
-		buffer.String(),
+		cmdline,
 		builtincmd.Exec,
 		&stdio)
 }
