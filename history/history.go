13cf248e9c16639233a4a0c09431496453dea70c
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-06-15 17:36:53 +0900

    ヒストリ置換の修飾子(!*,!^,!$,!:n)

--- history/history.go
+++ history/history.go
@@ -55,6 +55,14 @@ func Replace(line string) (string, bool) {
 			continue
 		}
 		ch, _, _ = reader.ReadRune()
+		if n := strings.IndexRune("^$:*", ch); n >= 0 {
+			reader.UnreadRune()
+			if len(histories) > 0 {
+				insertHisotry(&buffer, reader, histories[len(histories)-1])
+				isReplaced = true
+			}
+			continue
+		}
 		if ch == '!' {
 			if len(histories) > 0 {
 				insertHisotry(&buffer, reader, histories[len(histories)-1])
@@ -62,7 +70,7 @@ func Replace(line string) (string, bool) {
 				continue
 			} else {
 				buffer.WriteRune('!')
-				break
+				continue
 			}
 		}
 		if n := strings.IndexRune("0123456789", ch); n >= 0 {
@@ -119,6 +127,81 @@ func Replace(line string) (string, bool) {
 	return buffer.String(), isReplaced
 }
 
-func insertHisotry(buffer *bytes.Buffer, reader *strings.Reader, base string) {
-	buffer.WriteString(base)
+func splitQ(s string) []string {
+	args := make([]string, 0)
+	reader := strings.NewReader(s)
+	for reader.Len() > 0 {
+		var buffer bytes.Buffer
+		for {
+			if reader.Len() <= 0 {
+				return args
+			}
+			ch, _, _ := reader.ReadRune()
+			if ch != ' ' {
+				reader.UnreadRune()
+				break
+			}
+		}
+		quote := false
+		for reader.Len() > 0 {
+			ch, _, _ := reader.ReadRune()
+			if ch == '"' {
+				quote = !quote
+			}
+			if ch == ' ' && !quote {
+				break
+			}
+			buffer.WriteRune(ch)
+		}
+		s := buffer.String()
+		if s != "" {
+			args = append(args, s)
+		}
+	}
+	return args
+}
+
+func insertHisotry(buffer *bytes.Buffer, reader *strings.Reader, history1 string) {
+	ch, siz, _ := reader.ReadRune()
+	if siz > 0 && ch == '^' {
+		args := splitQ(history1)
+		if len(args) >= 2 {
+			buffer.WriteString(args[1])
+		}
+	} else if siz > 0 && ch == '$' {
+		args := splitQ(history1)
+		if len(args) >= 2 {
+			buffer.WriteString(args[len(args)-1])
+		}
+	} else if siz > 0 && ch == '*' {
+		args := splitQ(history1)
+		if len(args) >= 2 {
+			buffer.WriteString(strings.Join(args[1:], " "))
+		}
+	} else if siz > 0 && ch == ':' {
+		args := splitQ(history1)
+		n := 0
+		count := 0
+		for reader.Len() > 0 {
+			ch, _, _ = reader.ReadRune()
+			index := strings.IndexRune("0123456789", ch)
+			if index >= 0 {
+				n = n*10 + index
+				count++
+			} else {
+				reader.UnreadRune()
+				break
+			}
+		}
+		if count <= 0 {
+			buffer.WriteRune(':')
+		} else if n < len(args) {
+			buffer.WriteString(args[n])
+		}
+	} else {
+		if siz > 0 {
+			reader.UnreadRune()
+		}
+		buffer.WriteString(history1)
+	}
 }
