b251db2e4c8b7116e4ecf5f7737f47b74928618d
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-06-25 12:56:38 +0900

    nyagos.rc 導入に伴い、nyagos.cmd 削除

--- nyagos.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem --- batchfile for test ---
-set PROMPT=$e[34;40;1m$L$P$G$_$$ $e[37;1m
-set OPTION=-a "ls=ls -oF" %1 %2 %3 %4 %5 %6 %7 %8 %9
-if exist %~dp0nyagos.exe (
-    %~dp0nyagos.exe %OPTION%
-) else (
-    go run nyagos.go %OPTION%
-)
