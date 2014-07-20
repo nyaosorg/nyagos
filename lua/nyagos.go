2432e4605f1b291e5df65bf3e4a142aef0499972
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-07-21 02:21:06 +0900

    Lua に exec を追加 && %HOME%\.nyagos を読むように nyagos.lua を設定

--- lua/nyagos.go
+++ lua/nyagos.go
@@ -1,27 +1,42 @@
 package lua
 
+import "fmt"
 import "os"
 import "strings"
 
 import "../alias/table"
+import "../interpreter"
+import "../option"
 
-func LuaAlias(L *Lua) int {
+func alias(L *Lua) int {
 	name := L.ToString(1)
 	value := L.ToString(2)
 	aliasTable.Table[strings.ToLower(name)] = value
 	return 0
 }
 
-func LuaSetEnv(L *Lua) int {
+func setEnv(L *Lua) int {
 	name := L.ToString(1)
 	value := L.ToString(2)
 	os.Setenv(name, value)
 	return 0
 }
 
+func exec(L *Lua) int {
+	statement := L.ToString(1)
+	_, err := interpreter.Interpret(statement, option.CommandHooks, nil)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	return 0
+}
+
 func SetFunctions(this *Lua) {
-	this.PushGoFunction(LuaAlias)
+	this.PushGoFunction(alias)
 	this.SetGlobal("alias")
-	this.PushGoFunction(LuaSetEnv)
+	this.PushGoFunction(setEnv)
 	this.SetGlobal("setenv")
+	this.PushGoFunction(exec)
+	this.SetGlobal("exec")
 }
--- nyagos.lua
+++ nyagos.lua
@@ -1,16 +1,33 @@
 print "Nihongo Yet Another GOing Shell (c) 2014 K.Hayama"
-setenv("PROMPT","$e[36;40;1m$L$P$G$_$$ $e[37;1m")
-local home = os.getenv("USERPROFILE") or os.getenv("HOME")
+
+function set(equation)
+    local pos=string.find(equation,"=",1,true)
+    local left=string.sub(equation,1,pos-1)
+    local right=string.sub(equation,pos+1)
+    if pos and string.sub(left,-1) == "+" then
+        left = string.sub(left,1,-2)
+        local original=os.getenv(left)
+        if string.find(right,original) then
+            right = right .. ";" .. original
+        else
+            right = original
+        end
+    end
+    right = string.gsub(right,"%%(%w+)%%",function(w)
+        return os.getenv(w)
+    end)
+    setenv(left,right)
+end
+
+set "PROMPT=$e[36;40;1m$L$P$G$_$$ $e[37;1m"
+alias("ls","ls -oF")
+local home = os.getenv("HOME") or os.getenv("USERPROFILE")
 if home then
     local rcfname = home .. [[\.nyagos]]
     fd = io.open(rcfname)
     if fd then
         fd:close()
-        loadfile(fcfname)
+        loadfile(rcfname)()
     end
 end
-
-function addpath(path1)
-    setenv(path1..";"..os.getenv("PATH"))
-end
-
+system "cd"
