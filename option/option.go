c5f16a2aacf0813045f6a2b0ae408a46e2fce272
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-07-22 04:03:42 +0900

    -a オプションを削除

--- option/option.go
+++ option/option.go
@@ -2,10 +2,8 @@ package option
 
 import "io"
 import "os/exec"
-import "strings"
 
 import "../alias"
-import . "../alias/table"
 import "../commands"
 import "../interpreter"
 
@@ -28,17 +26,6 @@ func Parse(getArg func() (string, bool)) {
 		}
 		for _, o := range arg[1:] {
 			switch o {
-			case 'a':
-				if equation, equationOk := getArg(); equationOk {
-					equationArray := strings.SplitN(equation, "=", 2)
-					if len(equationArray) >= 2 {
-						Table[strings.ToLower(equationArray[0])] =
-							equationArray[1]
-					} else {
-						delete(Table, strings.ToLower(
-							equationArray[0]))
-					}
-				}
 			case 'c', 'k':
 				if fname, fnameOk := getArg(); fnameOk {
 					interpreter.Interpret(fname, CommandHooks, nil)
