f99c5828ffa4e8cef220ab00bb6398c6c77f131b
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-08-15 00:41:28 +0900

    Makefile -> make.cmd

---
+++ make.cmd
@@ -0,0 +1,21 @@
+@setlocal
+@set PROMPT=$G$S
+@if not "%1" == "" goto %1
+
+:build
+        if not exist nyagos.syso windres --output-format=coff -o nyagos.syso nyagos.rc
+	for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -ldflags "-X main.version [SNAPSHOT-%DATE:/=%-%%V]"
+        goto end
+
+:fmt
+	for /R . %%I IN (*.go) do go fmt %%I
+        goto end
+
+:clean
+	for %%I in (nyagos.exe nyagos.syso) do if exist %%I del %%I
+        goto end
+
+:snapshot
+	zip -9 nyagos-%DATE:/=%.zip nyagos.exe lua52.dll nyagos.lua nyagos_ja.mkd readme.mkd .nyagos
+
+:end
