cf5eb1b8e2443a0dbbb1dec36d76c0297408c1f2
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-08-17 20:21:39 +0900

    With `make.cmd get`, go get github-packages

--- make.cmd
+++ make.cmd
@@ -4,18 +4,25 @@
 
 :build
         if not exist nyagos.syso windres --output-format=coff -o nyagos.syso nyagos.rc
-	for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -ldflags "-X main.stamp %DATE% -X main.commit %%V"
+        for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -ldflags "-X main.stamp %DATE% -X main.commit %%V"
         goto end
 
 :fmt
-	for /R . %%I IN (*.go) do go fmt %%I
+        for /R . %%I IN (*.go) do go fmt %%I
         goto end
 
 :clean
-	for %%I in (nyagos.exe nyagos.syso) do if exist %%I del %%I
+        for %%I in (nyagos.exe nyagos.syso) do if exist %%I del %%I
+        goto end
+
+:get
+        go get github.com/mattn/go-runewidth
+        go get github.com/shiena/ansicolor 
+        go get github.com/atotto/clipboard       
         goto end
 
 :snapshot
-	zip -9 nyagos-%DATE:/=%.zip nyagos.exe lua52.dll nyagos.lua nyagos_ja.mkd readme.mkd .nyagos
+        zip -9 nyagos-%DATE:/=%.zip nyagos.exe lua52.dll nyagos.lua nyagos_ja.mkd readme.mkd .nyagos
+        goto end
 
 :end
