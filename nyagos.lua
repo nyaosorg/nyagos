99c8008f641ee918b1a3302aa78565f1a14fca5f
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-09-15 19:18:00 +0900

    逆クォートの中に & や < , > があっても誤認識しないようにした。

--- nyagos.lua
+++ nyagos.lua
@@ -139,7 +139,11 @@ nyagos.filter = function(cmdline)
     cmdline = string.gsub(cmdline,'`([^`]*)`',function(m)
         local r = nyagos.eval(m)
         if r then
-            return nyagos.atou(r)
+            r = nyagos.atou(r)
+            r = string.gsub(r,'[|&<>!]',function(m)
+                return string.format('%%u+%04X%%',string.byte(m,1,1))
+            end)
+            return r
         else
             return ""
         end
