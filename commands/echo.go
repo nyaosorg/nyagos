9cb52aaf8a80b4fa20dd143d8ccafbfb56896aa9
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-10-01 14:11:05 +0900

    内蔵 echo コマンドがエスケープシーケンスに対応していなかった不具合を修正

--- commands/echo.go
+++ commands/echo.go
@@ -4,9 +4,11 @@ import "fmt"
 import "os/exec"
 import "strings"
 
+import "github.com/shiena/ansicolor"
+
 import "../interpreter"
 
 func cmd_echo(cmd *exec.Cmd) (interpreter.NextT, error) {
-	fmt.Fprintln(cmd.Stdout, strings.Join(cmd.Args[1:], " "))
+	fmt.Fprintln(ansicolor.NewAnsiColorWriter(cmd.Stdout), strings.Join(cmd.Args[1:], " "))
 	return interpreter.CONTINUE, nil
 }
