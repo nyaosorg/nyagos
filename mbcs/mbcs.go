7e540c432d5e58c973031d192882a18e6bf56879
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-03 14:16:42 +0900

    mbcs/mbcs.go does not depend on cgo

--- mbcs/mbcs.go
+++ mbcs/mbcs.go
@@ -1,44 +1,33 @@
 package mbcs
 
-/*
-#include <windows.h>
-*/
-import "C"
 import "syscall"
 import "unsafe"
-import "fmt"
 
-func lastError(name string) error {
-	switch C.GetLastError() {
-	case C.ERROR_INSUFFICIENT_BUFFER:
-		return fmt.Errorf("%s: ERROR_INSUFFICIENT_BUFFER", name)
-	case C.ERROR_INVALID_FLAGS:
-		return fmt.Errorf("%s: ERROR_INVALID_FLAGS", name)
-	case C.ERROR_INVALID_PARAMETER:
-		return fmt.Errorf("%s: ERROR_INVALID_PARAMETER", name)
-	case C.ERROR_NO_UNICODE_TRANSLATION:
-		return fmt.Errorf("%s: ERROR_NO_UNICODE_TRANSLATION", name)
-	default:
-		return fmt.Errorf("%s: Unknown error", name)
-	}
-}
+var kernel32 = syscall.NewLazyDLL("kernel32")
+var multiByteToWideChar = kernel32.NewProc("MultiByteToWideChar")
+var wideCharToMultiByte = kernel32.NewProc("WideCharToMultiByte")
+
+const CP_THREAD_ACP = 3
 
 func UtoA(utf8 string) ([]byte, error) {
 	utf16, err := syscall.UTF16FromString(utf8)
 	if err != nil {
 		return nil, err
 	}
-	size := C.WideCharToMultiByte(C.CP_THREAD_ACP, 0,
-		(*C.WCHAR)(&utf16[0]), C.int(len(utf16)), nil, 0, nil, nil)
+	size, _, _ := wideCharToMultiByte.Call(CP_THREAD_ACP, 0,
+		uintptr(unsafe.Pointer(&utf16[0])),
+		uintptr(len(utf16)),
+		uintptr(0), 0, uintptr(0), uintptr(0))
 	if size <= 0 {
-		return nil, lastError("WideCharToMultiByte")
+		return nil, syscall.GetLastError()
 	}
 	mbcs := make([]byte, size)
-	rc := C.WideCharToMultiByte(C.CP_THREAD_ACP, 0,
-		(*C.WCHAR)(&utf16[0]), C.int(len(utf16)),
-		(*C.CHAR)(unsafe.Pointer(&mbcs[0])), size, nil, nil)
+	rc, _, _ := wideCharToMultiByte.Call(CP_THREAD_ACP, 0,
+		uintptr(unsafe.Pointer(&utf16[0])),
+		uintptr(len(utf16)),
+		uintptr(unsafe.Pointer(&mbcs[0])), size, uintptr(0), uintptr(0))
 	if rc == 0 {
-		return nil, lastError("WideCharToMultiByte")
+		return nil, syscall.GetLastError()
 	}
 	return mbcs, nil
 }
@@ -47,17 +36,19 @@ func AtoU(mbcs []byte) (string, error) {
 	if mbcs == nil || len(mbcs) <= 0 {
 		return "", nil
 	}
-	size := C.MultiByteToWideChar(C.CP_THREAD_ACP, 0,
-		(*C.CHAR)(unsafe.Pointer(&mbcs[0])), C.int(len(mbcs)), nil, 0)
+	size, _, _ := multiByteToWideChar.Call(CP_THREAD_ACP, 0,
+		uintptr(unsafe.Pointer(&mbcs[0])),
+		uintptr(len(mbcs)),
+		uintptr(0), 0)
 	if size <= 0 {
-		return "", lastError("MultiByteToWideChar")
+		return "", syscall.GetLastError()
 	}
 	utf16 := make([]uint16, size)
-	rc := C.MultiByteToWideChar(C.CP_THREAD_ACP, 0,
-		(*C.CHAR)(unsafe.Pointer(&mbcs[0])), C.int(len(mbcs)),
-		(*C.WCHAR)(&utf16[0]), size)
+	rc, _, _ := multiByteToWideChar.Call(CP_THREAD_ACP, 0,
+		uintptr(unsafe.Pointer(&mbcs[0])), uintptr(len(mbcs)),
+		uintptr(unsafe.Pointer(&utf16[0])), size)
 	if rc == 0 {
-		return "", lastError("MultiByteToWideChar")
+		return "", syscall.GetLastError()
 	}
 	return syscall.UTF16ToString(utf16), nil
 }
