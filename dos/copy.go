151c4a1e055458202c063611033f0537db6feb35
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-14 02:10:41 +0900

    add dos.Move()

--- dos/copy.go
+++ dos/copy.go
@@ -4,6 +4,7 @@ import "syscall"
 import "unsafe"
 
 var copyFile = kernel32.NewProc("CopyFileW")
+var moveFile = kernel32.NewProc("MoveFileW")
 
 func Copy(src string, dst string, override bool) error {
 	cSrc, cSrcErr := syscall.UTF16FromString(src)
@@ -20,9 +21,29 @@ func Copy(src string, dst string, override bool) error {
 	} else {
 		override_ = 0
 	}
-	rc, _, err := copyFile.Call(uintptr(unsafe.Pointer(&cSrc[0])),
+	rc, _, err := copyFile.Call(
+		uintptr(unsafe.Pointer(&cSrc[0])),
 		uintptr(unsafe.Pointer(&cDst[0])),
 		override_)
+	if rc != 0 {
+		return nil
+	} else {
+		return err
+	}
+}
+
+func Move(src, dst string) error {
+	cSrc, cSrcErr := syscall.UTF16FromString(src)
+	if cSrcErr != nil {
+		return cSrcErr
+	}
+	cDst, cDstErr := syscall.UTF16FromString(dst)
+	if cDstErr != nil {
+		return cDstErr
+	}
+	rc, _, err := moveFile.Call(
+		uintptr(unsafe.Pointer(&cSrc[0])),
+		uintptr(unsafe.Pointer(&cDst[0])))
 	if rc != 0 {
 		return nil
 	} else {
