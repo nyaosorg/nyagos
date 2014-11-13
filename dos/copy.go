3327fff897b08dcff5c02ded73e3eace9675d9da
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-14 01:56:20 +0900

    add dos.Copy()

---
+++ dos/copy.go
@@ -0,0 +1,31 @@
+package dos
+
+import "syscall"
+import "unsafe"
+
+var copyFile = kernel32.NewProc("CopyFileW")
+
+func Copy(src string, dst string, override bool) error {
+	cSrc, cSrcErr := syscall.UTF16FromString(src)
+	if cSrcErr != nil {
+		return cSrcErr
+	}
+	cDst, cDstErr := syscall.UTF16FromString(dst)
+	if cDstErr != nil {
+		return cDstErr
+	}
+	var override_ uintptr
+	if override {
+		override_ = 1
+	} else {
+		override_ = 0
+	}
+	rc, _, err := copyFile.Call(uintptr(unsafe.Pointer(&cSrc[0])),
+		uintptr(unsafe.Pointer(&cDst[0])),
+		override_)
+	if rc != 0 {
+		return nil
+	} else {
+		return err
+	}
+}
