1e7e811f96ae0643f9a32c0b64ba2c130cee915c
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-18 22:50:45 +0900

    'rmdir /s' does not remove files under junctions

--- dos/truncate.go
+++ dos/truncate.go
@@ -6,31 +6,38 @@ import "os"
 import "syscall"
 
 func Truncate(folder string, out io.Writer) error {
-	fd, fdErr := os.Open(folder)
-	if fdErr != nil {
-		return fdErr
+	attr, attrErr := GetFileAttributes(folder)
+	if attrErr != nil {
+		return fmt.Errorf("%s: %s", folder, attrErr)
 	}
-	fi, fiErr := fd.Readdir(-1)
-	fd.Close()
-	if fiErr != nil {
-		return fiErr
-	}
-	for _, f := range fi {
-		if f.Name() == "." || f.Name() == ".." {
-			continue
+	if (attr & FILE_ATTRIBUTE_REPARSE_POINT) == 0 {
+		// Only not junction, delete files under folder.
+		fd, fdErr := os.Open(folder)
+		if fdErr != nil {
+			return fdErr
 		}
-		fullpath := Join(folder, f.Name())
-		var err error
-		if f.IsDir() {
-			fmt.Fprintf(out, "%s\\\n", fullpath)
-			err = Truncate(fullpath, out)
-		} else {
-			fmt.Fprintln(out, fullpath)
-			SetFileAttributes(fullpath, FILE_ATTRIBUTE_NORMAL)
-			err = syscall.Unlink(fullpath)
+		fi, fiErr := fd.Readdir(-1)
+		fd.Close()
+		if fiErr != nil {
+			return fiErr
 		}
-		if err != nil {
-			return fmt.Errorf("%s: %s", fullpath, err.Error())
+		for _, f := range fi {
+			if f.Name() == "." || f.Name() == ".." {
+				continue
+			}
+			fullpath := Join(folder, f.Name())
+			var err error
+			if f.IsDir() {
+				fmt.Fprintf(out, "%s\\\n", fullpath)
+				err = Truncate(fullpath, out)
+			} else {
+				fmt.Fprintln(out, fullpath)
+				SetFileAttributes(fullpath, FILE_ATTRIBUTE_NORMAL)
+				err = syscall.Unlink(fullpath)
+			}
+			if err != nil {
+				return fmt.Errorf("%s: %s", fullpath, err.Error())
+			}
 		}
 	}
 	if err := syscall.Rmdir(folder); err != nil {
