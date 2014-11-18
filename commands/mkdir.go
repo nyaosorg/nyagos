ba7ebd4e48af63fe0b1125323d6ba5ca85f48c6a
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-18 16:59:20 +0900

    rmdir support /s option(=deltree), but can not remove system,hidden or readonly files.

--- commands/mkdir.go
+++ commands/mkdir.go
@@ -1,24 +1,62 @@
 package commands
 
+import "fmt"
+import "os"
 import "syscall"
 
+import "../conio"
 import "../interpreter"
 
 func cmd_mkdir(cmd *interpreter.Interpreter) (interpreter.NextT, error) {
-	for i := 1; i < len(cmd.Args); i++ {
-		err := syscall.Mkdir(cmd.Args[i], 0777)
+	for _, arg1 := range cmd.Args[1:] {
+		err := syscall.Mkdir(arg1, 0777)
 		if err != nil {
-			return interpreter.CONTINUE, err
+			fmt.Fprintf(cmd.Stderr, "%s: %s\n", arg1, err)
 		}
 	}
 	return interpreter.CONTINUE, nil
 }
 
 func cmd_rmdir(cmd *interpreter.Interpreter) (interpreter.NextT, error) {
-	for i, n := 1, len(cmd.Args); i < n; i++ {
-		err := syscall.Rmdir(cmd.Args[i])
+	s_option := false
+	message := "%s: Rmdir Are you sure? [Yes/No/Quit] "
+	for _, arg1 := range cmd.Args[1:] {
+		if arg1 == "/s" {
+			s_option = true
+			message = "%s : Delete Tree. Are you sure? [Yes/No/Quit] "
+			continue
+		}
+		fi, err := os.Stat(arg1)
 		if err != nil {
-			return interpreter.CONTINUE, err
+			fmt.Fprintf(cmd.Stderr, "%s: %s\n", arg1, err.Error())
+			continue
+		}
+		if !fi.IsDir() {
+			fmt.Fprintf(cmd.Stderr, "%s: not directory\n", arg1)
+			continue
+		}
+		fmt.Fprintf(cmd.Stderr, message, arg1)
+		ch := conio.GetCh()
+		fmt.Fprintf(cmd.Stderr, "%c ", ch)
+		switch ch {
+		case 'y', 'Y':
+
+		case 'q', 'Q':
+			fmt.Fprintln(cmd.Stderr, "-> canceled all")
+			return interpreter.CONTINUE, nil
+		default:
+			fmt.Fprintln(cmd.Stderr, "-> canceled")
+			continue
+		}
+		if s_option {
+			err = os.RemoveAll(arg1)
+		} else {
+			err = syscall.Rmdir(arg1)
+		}
+		if err != nil {
+			fmt.Fprintf(cmd.Stderr, "-> %s\n", err.Error())
+		} else {
+			fmt.Fprintln(cmd.Stderr, "-> done.")
 		}
 	}
 	return interpreter.CONTINUE, nil
