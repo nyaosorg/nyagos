ad975f21838f1bce2a5b67a792d30c9bb9f0040a
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-11-22 13:52:12 +0900

    Append support keys.

--- conio/readline/constants.go
+++ conio/readline/constants.go
@@ -1,44 +1,74 @@
 package readline
 
 const (
-	K_BACKSPACE = "BACKSPACE"
-	K_CTRL      = "CTRL"
-	K_CTRL_A    = "C_A"
-	K_CTRL_B    = "C_B"
-	K_CTRL_C    = "C_C"
-	K_CTRL_D    = "C_D"
-	K_CTRL_E    = "C_E"
-	K_CTRL_F    = "C_F"
-	K_CTRL_G    = "C_G"
-	K_CTRL_H    = "C_H"
-	K_CTRL_I    = "C_I"
-	K_CTRL_J    = "C_J"
-	K_CTRL_K    = "C_K"
-	K_CTRL_L    = "C_L"
-	K_CTRL_M    = "C_M"
-	K_CTRL_N    = "C_N"
-	K_CTRL_O    = "C_O"
-	K_CTRL_P    = "C_P"
-	K_CTRL_Q    = "C_Q"
-	K_CTRL_R    = "C_R"
-	K_CTRL_S    = "C_S"
-	K_CTRL_T    = "C_T"
-	K_CTRL_U    = "C_U"
-	K_CTRL_V    = "C_V"
-	K_CTRL_W    = "C_W"
-	K_CTRL_X    = "C_X"
-	K_CTRL_Y    = "C_Y"
-	K_CTRL_Z    = "C_Z"
-	K_DELETE    = "DEL"
-	K_DOWN      = "DOWN"
-	K_END       = "END"
-	K_ENTER     = "ENTER"
-	K_ESCAPE    = "ESCAPE"
-	K_HOME      = "HOME"
-	K_LEFT      = "LEFT"
-	K_RIGHT     = "RIGHT"
-	K_SHIFT     = "SHIFT"
-	K_UP        = "UP"
+	K_BACKSPACE  = "BACKSPACE"
+	K_CAPSLOCK   = "CAPSLOCK"
+	K_CLEAR      = "CLEAR"
+	K_CTRL       = "CTRL"
+	K_CTRL_A     = "C_A"
+	K_CTRL_B     = "C_B"
+	K_CTRL_BREAK = "C_BREAK"
+	K_CTRL_C     = "C_C"
+	K_CTRL_D     = "C_D"
+	K_CTRL_E     = "C_E"
+	K_CTRL_F     = "C_F"
+	K_CTRL_G     = "C_G"
+	K_CTRL_H     = "C_H"
+	K_CTRL_I     = "C_I"
+	K_CTRL_J     = "C_J"
+	K_CTRL_K     = "C_K"
+	K_CTRL_L     = "C_L"
+	K_CTRL_M     = "C_M"
+	K_CTRL_N     = "C_N"
+	K_CTRL_O     = "C_O"
+	K_CTRL_P     = "C_P"
+	K_CTRL_Q     = "C_Q"
+	K_CTRL_R     = "C_R"
+	K_CTRL_S     = "C_S"
+	K_CTRL_T     = "C_T"
+	K_CTRL_U     = "C_U"
+	K_CTRL_V     = "C_V"
+	K_CTRL_W     = "C_W"
+	K_CTRL_X     = "C_X"
+	K_CTRL_Y     = "C_Y"
+	K_CTRL_Z     = "C_Z"
+	K_DELETE     = "DEL"
+	K_DOWN       = "DOWN"
+	K_END        = "END"
+	K_ENTER      = "ENTER"
+	K_ESCAPE     = "ESCAPE"
+	K_F1         = "F1"
+	K_F10        = "F10"
+	K_F11        = "F11"
+	K_F12        = "F12"
+	K_F13        = "F13"
+	K_F14        = "F14"
+	K_F15        = "F15"
+	K_F16        = "F16"
+	K_F17        = "F17"
+	K_F18        = "F18"
+	K_F19        = "F19"
+	K_F2         = "F2"
+	K_F20        = "F20"
+	K_F21        = "F21"
+	K_F22        = "F22"
+	K_F23        = "F23"
+	K_F24        = "F24"
+	K_F3         = "F3"
+	K_F4         = "F4"
+	K_F5         = "F5"
+	K_F6         = "F6"
+	K_F7         = "F7"
+	K_F8         = "F8"
+	K_F9         = "F9"
+	K_HOME       = "HOME"
+	K_LEFT       = "LEFT"
+	K_PAGEDOWN   = "PAGEDOWN"
+	K_PAGEUP     = "PAGEUP"
+	K_PAUSE      = "PAUSE"
+	K_RIGHT      = "RIGHT"
+	K_SHIFT      = "SHIFT"
+	K_UP         = "UP"
 )
 
 const (
@@ -97,15 +127,45 @@ var name2char = map[string]rune{
 // http://msdn.microsoft.com/ja-jp/library/windows/desktop/dd375731(v=vs.85).aspx
 
 var name2scan = map[string]uint16{
-	K_CTRL:   0x11,
-	K_DELETE: 0x2E,
-	K_DOWN:   0x28,
-	K_END:    0x23,
-	K_HOME:   0x24,
-	K_LEFT:   0x25,
-	K_RIGHT:  0x27,
-	K_SHIFT:  0x10,
-	K_UP:     0x26,
+	K_CAPSLOCK:   0x14,
+	K_CLEAR:      0x0C,
+	K_CTRL:       0x11,
+	K_CTRL_BREAK: 0x03,
+	K_DELETE:     0x2E,
+	K_DOWN:       0x28,
+	K_END:        0x23,
+	K_F10:        0x79,
+	K_F11:        0x7A,
+	K_F12:        0x7B,
+	K_F13:        0x7C,
+	K_F14:        0x7D,
+	K_F15:        0x7E,
+	K_F16:        0x7F,
+	K_F17:        0x80,
+	K_F18:        0x81,
+	K_F19:        0x82,
+	K_F1:         0x70,
+	K_F20:        0x83,
+	K_F21:        0x84,
+	K_F22:        0x85,
+	K_F23:        0x86,
+	K_F24:        0x87,
+	K_F2:         0x71,
+	K_F3:         0x72,
+	K_F4:         0x73,
+	K_F5:         0x74,
+	K_F6:         0x75,
+	K_F7:         0x76,
+	K_F8:         0x77,
+	K_F9:         0x78,
+	K_HOME:       0x24,
+	K_LEFT:       0x25,
+	K_PAGEDOWN:   0x22,
+	K_PAGEUP:     0x21,
+	K_PAUSE:      0x13,
+	K_RIGHT:      0x27,
+	K_SHIFT:      0x10,
+	K_UP:         0x26,
 }
 
 var name2func = map[string]KeyFuncT{
