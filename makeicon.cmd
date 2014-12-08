39165809308b3f536f8234fd48d05aaa5f23754f
HAYAMA_Kaoru <iyahaya@nifty.com>
2014-12-08 13:01:28 +0900

    makeicon.cmd: Even run on develop-directory, make icon linked to installdir

--- makeicon.cmd
+++ makeicon.cmd
@@ -1,2 +1,7 @@
-@set "PROMPT=$G"
-for /F %%I in ('cscript /nologo specialfolders.vbs desktop') do cscript /nologo lnk.vbs "%~dp0\nyagos.exe" "%%I"
+@echo off
+if exist version.cmd call version.cmd
+if "%INSTALLDIR%" == "" set "INSTALLDIR=%~dp0"
+if not exist "%INSTALLDIR%\nyagos.exe" set "INSTALLDIR=%~dp0"
+if not "%2" == "" set "INSTALLDIR=%2"
+
+for /F %%I in ('cscript /nologo specialfolders.vbs desktop') do cscript /nologo lnk.vbs "%INSTALLDIR%\nyagos.exe" "%%I"
