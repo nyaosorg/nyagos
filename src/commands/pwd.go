e37d4989ea5833f559c84ad338ec18b013876abb
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-01-22 01:19:27 +0900

    Implemented: pwd -N (N:digit)
    * print working directory N-times changed ago

--- src/commands/pwd.go
+++ src/commands/pwd.go
@@ -2,13 +2,25 @@ package commands
 
 import (
 	"fmt"
+	"strconv"
 
 	"../dos"
-	"../interpreter"
+	. "../interpreter"
 )
 
-func cmd_pwd(cmd *interpreter.Interpreter) (interpreter.NextT, error) {
-	wd, _ := dos.Getwd()
-	fmt.Fprintln(cmd.Stdout, wd)
-	return interpreter.CONTINUE, nil
+func cmd_pwd(cmd *Interpreter) (NextT, error) {
+	if len(cmd.Args) >= 2 {
+		if i, err := strconv.ParseInt(cmd.Args[1], 10, 0); err == nil && i < 0 {
+			i += int64(len(cd_history))
+			if i < 0 {
+				return CONTINUE, fmt.Errorf("pwd %s: too old history", cmd.Args[1])
+			}
+			fmt.Fprintln(cmd.Stdout, cd_history[i])
+			return CONTINUE, nil
+		}
+	} else {
+		wd, _ := dos.Getwd()
+		fmt.Fprintln(cmd.Stdout, wd)
+	}
+	return CONTINUE, nil
 }
