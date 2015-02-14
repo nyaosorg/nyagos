8b6d64341d8c8146df4219c5144a82d08b108917
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-02-14 13:05:49 +0900

    Add built-in command pushd/popd/dirs

---
+++ commands/pushd.go
@@ -0,0 +1,57 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+
+	"../dos"
+	. "../interpreter"
+)
+
+var dirstack = make([]string, 0, 20)
+
+func cmd_dirs(cmd *Interpreter) (NextT, error) {
+	wd, _ := dos.Getwd()
+	fmt.Fprint(cmd.Stdout, wd)
+	for i := len(dirstack) - 1; i >= 0; i-- {
+		fmt.Fprint(cmd.Stdout, " ", dirstack[i])
+	}
+	fmt.Fprintln(cmd.Stdout)
+	return CONTINUE, nil
+}
+
+func cmd_popd(cmd *Interpreter) (NextT, error) {
+	if len(dirstack) <= 0 {
+		return CONTINUE, errors.New("popd: directory stack empty.")
+	}
+	err := dos.Chdir(dirstack[len(dirstack)-1])
+	if err != nil {
+		return CONTINUE, err
+	}
+	dirstack = dirstack[:len(dirstack)-1]
+	return cmd_dirs(cmd)
+}
+
+func cmd_pushd(cmd *Interpreter) (NextT, error) {
+	wd, err := dos.Getwd()
+	if err != nil {
+		return CONTINUE, err
+	}
+	if len(cmd.Args) >= 2 {
+		dirstack = append(dirstack, wd)
+		err := dos.Chdir(cmd.Args[1])
+		if err != nil {
+			return CONTINUE, err
+		}
+	} else {
+		if len(dirstack) <= 0 {
+			return CONTINUE, errors.New("pushd: directory stack empty.")
+		}
+		err := dos.Chdir(dirstack[len(dirstack)-1])
+		if err != nil {
+			return CONTINUE, err
+		}
+		dirstack[len(dirstack)-1] = wd
+	}
+	return cmd_dirs(cmd)
+}
