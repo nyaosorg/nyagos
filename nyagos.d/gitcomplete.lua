d66206ebfee9d543f405d55d8880508fc3937c09
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-03-08 00:36:56 +0900

    gitcomplete.lua: off when git does not exist

--- nyagos.d/gitcomplete.lua
+++ nyagos.d/gitcomplete.lua
@@ -10,34 +10,35 @@ if githelp then
         end
     end
     githelp:close()
-
-    nyagos.bindkey("C_I",function(this)
-        if this:firstword() ~= "git" then
-            this:call("COMPLETE")
-            return
-        end
-        local lastword,lastword_at = this:lastword()
-        if lastword_at ~= 5 then
-            this:call("COMPLETE")
-            return
-        end
-        local list={}
-        local lastword_len = string.len(lastword)
-        for i=1,#gitcmds do
-            if string.sub(gitcmds[i],1,lastword_len) == lastword then
-                list[#list+1] = gitcmds[i]
+    if #gitcmds >= 1 then
+        nyagos.bindkey("C_I",function(this)
+            if this:firstword() ~= "git" then
+                this:call("COMPLETE")
+                return
             end
-        end
-        local commonprefix = nyagos.commonprefix(list)
-        if string.len(commonprefix) > lastword_len then
-            local result = string.sub(commonprefix,lastword_len+1)
-            if #list == 1 then
-                result = result .. " "
+            local lastword,lastword_at = this:lastword()
+            if lastword_at ~= 5 then
+                this:call("COMPLETE")
+                return
             end
-            return result
-        elseif #list >= 2 then
-            this:boxprint(list)
-        end
-        return nil
-    end)
+            local list={}
+            local lastword_len = string.len(lastword)
+            for i=1,#gitcmds do
+                if string.sub(gitcmds[i],1,lastword_len) == lastword then
+                    list[#list+1] = gitcmds[i]
+                end
+            end
+            local commonprefix = nyagos.commonprefix(list)
+            if string.len(commonprefix) > lastword_len then
+                local result = string.sub(commonprefix,lastword_len+1)
+                if #list == 1 then
+                    result = result .. " "
+                end
+                return result
+            elseif #list >= 2 then
+                this:boxprint(list)
+            end
+            return nil
+        end)
+    end
 end
