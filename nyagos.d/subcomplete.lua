79d569b9cd4a691336e3e6b72a19a4416ff65a26
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-03-08 01:00:54 +0900

    subcomplete.lua: modify for support not git command

--- nyagos.d/subcomplete.lua
+++ nyagos.d/subcomplete.lua
@@ -1,7 +1,8 @@
+local maincmds = {}
+
 local githelp=io.popen("git help -a","r")
-local gitcmds=nil
 if githelp then
-    gitcmds={}
+    local gitcmds={}
     for line in githelp:lines() do
         if string.match(line,"^  %S") then
             for word in string.gmatch(line,"%S+") do
@@ -10,35 +11,39 @@ if githelp then
         end
     end
     githelp:close()
-    if #gitcmds >= 1 then
-        nyagos.bindkey("C_I",function(this)
-            if this:firstword() ~= "git" then
-                this:call("COMPLETE")
-                return
-            end
-            local lastword,lastword_at = this:lastword()
-            if lastword_at ~= 5 then
-                this:call("COMPLETE")
-                return
-            end
-            local list={}
-            local lastword_len = string.len(lastword)
-            for i=1,#gitcmds do
-                if string.sub(gitcmds[i],1,lastword_len) == lastword then
-                    list[#list+1] = gitcmds[i]
-                end
-            end
-            local commonprefix = nyagos.commonprefix(list)
-            if string.len(commonprefix) > lastword_len then
-                local result = string.sub(commonprefix,lastword_len+1)
-                if #list == 1 then
-                    result = result .. " "
-                end
-                return result
-            elseif #list >= 2 then
-                this:boxprint(list)
-            end
-            return nil
-        end)
+    if next(gitcmds) then
+        maincmds["git"] = gitcmds
     end
+end
+
+if next(maincmds) then
+    nyagos.bindkey("C_I",function(this)
+        local firstword = this:firstword()
+        for maincmd1,subcmds in pairs(maincmds) do
+            if maincmd1 == firstword then
+                local lastword,lastword_at = this:lastword()
+                if lastword_at == string.len(maincmd1)+2 then
+                    local list={}
+                    local lastword_len = string.len(lastword)
+                    for i=1,#subcmds do
+                        if string.sub(subcmds[i],1,lastword_len) == lastword then
+                            list[#list+1] = subcmds[i]
+                        end
+                    end
+                    local commonprefix = nyagos.commonprefix(list)
+                    if string.len(commonprefix) > lastword_len then
+                        local result = string.sub(commonprefix,lastword_len+1)
+                        if #list == 1 then
+                            result = result .. " "
+                        end
+                        return result
+                    elseif #list >= 2 then
+                        this:boxprint(list)
+                    end
+                    return nil
+                end
+            end
+        end
+        return this:call("COMPLETE")
+    end)
 end
