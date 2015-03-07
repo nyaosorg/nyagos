6be598ec5417090421025201521350644fce5cd9
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-03-07 21:11:18 +0900

    Let 'which' search alias-table

--- commands/which.go
+++ commands/which.go
@@ -3,13 +3,19 @@ package commands
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 
+	"../alias"
 	"../dos"
 	. "../interpreter"
 )
 
 func cmd_which(cmd *Interpreter) (NextT, error) {
 	for _, name := range cmd.Args[1:] {
+		if a, ok := alias.Table[strings.ToLower(name)]; ok {
+			fmt.Fprintf(cmd.Stdout, "aliased to %s\n", a.String())
+			continue
+		}
 		path, err := exec.LookPath(name)
 		if err != nil {
 			return CONTINUE, err
