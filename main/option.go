d3d06864af77cc844419b418694ec84558be4b3b
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-04-13 18:52:11 +0900

    nyagos -e 'SCRIPTCODE' can refer arg[].

--- main/option.go
+++ main/option.go
@@ -9,6 +9,17 @@ import (
 	"../lua"
 )
 
+func setLuaArg(L *lua.Lua, arg0 string) {
+	L.NewTable()
+	L.PushString(arg0)
+	L.RawSetI(-2, 0)
+	for i, arg1 := range flag.Args() {
+		L.PushString(arg1)
+		L.RawSetI(-2, lua.Integer(i+1))
+	}
+	L.SetGlobal("arg")
+}
+
 func optionParse(L *lua.Lua) bool {
 	optionK := flag.String("k", "", "like `cmd /k`")
 	optionC := flag.String("c", "", "like `cmd /c`")
@@ -27,14 +38,7 @@ func optionParse(L *lua.Lua) bool {
 		result = false
 	}
 	if *optionF != "" {
-		L.NewTable()
-		L.PushString(*optionF)
-		L.RawSetI(-2, 0)
-		for i, arg1 := range flag.Args() {
-			L.PushString(arg1)
-			L.RawSetI(-2, lua.Integer(i+1))
-		}
-		L.SetGlobal("arg")
+		setLuaArg(L, *optionF)
 		err := L.Source(*optionF)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -46,6 +50,7 @@ func optionParse(L *lua.Lua) bool {
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		} else {
+			setLuaArg(L, *optionE)
 			L.Call(0, 0)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
