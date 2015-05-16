7a103c09399d19c7e4822b42b9839ab71f4ed8b4
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-05-16 11:37:54 +0900

    cdlnk.lua - Support: cd SHORTCUT.LNK

---
+++ nyagos.d/cdlnk.lua
@@ -0,0 +1,38 @@
+local wshObj = nyagos.ole.create_object_utf8("WScript.Shell")
+
+local function getfolder(arg1)
+    local shortcut1 = wshObj:CreateShortcut(arg1)
+    if not shortcut1 then
+        return arg1
+    end
+    local path = shortcut1.TargetPath
+    if nyagos.fsObj:FolderExists(path) then
+        return path
+    end
+    path = shortcut1.WorkingDirectory
+    if nyagos.fsObj:FolderExists(path) then
+        return path
+    end
+    path = nyagos.fsObj:GetParentFolderName(shortcut1.TargetPath)
+    if nyagos.fsObj:FolderExists(path) then
+        return path
+    end
+    return arg1
+end
+
+nyagos.alias("cd",function(args)
+    for i=1,#args do
+        local arg1 = args[i]
+        if string.match(arg1,"%.[lL][nN][kK]$") then
+            arg1 = getfolder(arg1)
+        end
+        if string.find(arg1," ") then
+            arg1 = '"'..arg1..'"'
+        end
+        args[i] = arg1
+    end
+    table.insert(args,1,"__cd__")
+    local cmdline=table.concat(args," ")
+    --print(cmdline)
+    nyagos.exec(cmdline)
+end)
