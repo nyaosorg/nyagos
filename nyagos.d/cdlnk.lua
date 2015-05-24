71a670560a07ff1a1d52073ef88945fbee4bf200
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-05-24 14:59:56 +0900

    No NYOLE.DLL, No cdlnk.lua and trash.lua

--- nyagos.d/cdlnk.lua
+++ nyagos.d/cdlnk.lua
@@ -1,40 +1,49 @@
-nyagos.ole = nyagos.ole or require("nyole")
-local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
-local wshObj = nyagos.ole.create_object_utf8("WScript.Shell")
-
-local function getfolder(arg1)
-    local shortcut1 = wshObj:CreateShortcut(arg1)
-    if not shortcut1 then
-        return arg1
+if not nyagos.ole then
+    local status
+    status, nyagos.ole = pcall(require,"nyole")
+    if not status then
+        nyagos.ole = nil
     end
-    local path = shortcut1.TargetPath
-    if fsObj:FolderExists(path) then
-        return path
-    end
-    path = shortcut1.WorkingDirectory
-    if fsObj:FolderExists(path) then
-        return path
-    end
-    path = fsObj:GetParentFolderName(shortcut1.TargetPath)
-    if nyagos.fsObj:FolderExists(path) then
-        return path
-    end
-    return arg1
 end
 
-local org_cd = nyagos.alias.cd
-nyagos.alias.cd=function(args)
-    for i=1,#args do
-        local arg1 = args[i]
-        if string.match(arg1,"%.[lL][nN][kK]$") then
-            arg1 = getfolder(arg1)
+if nyagos.ole then
+    local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
+    local wshObj = nyagos.ole.create_object_utf8("WScript.Shell")
+
+    local function getfolder(arg1)
+        local shortcut1 = wshObj:CreateShortcut(arg1)
+        if not shortcut1 then
+            return arg1
         end
-        args[i] = arg1
+        local path = shortcut1.TargetPath
+        if fsObj:FolderExists(path) then
+            return path
+        end
+        path = shortcut1.WorkingDirectory
+        if fsObj:FolderExists(path) then
+            return path
+        end
+        path = fsObj:GetParentFolderName(shortcut1.TargetPath)
+        if nyagos.fsObj:FolderExists(path) then
+            return path
+        end
+        return arg1
     end
-    if org_cd then
-        org_cd(args)
-    else
-        args[0] = "__cd__"
-        nyagos.exec(args)
+
+    local org_cd = nyagos.alias.cd
+    nyagos.alias.cd=function(args)
+        for i=1,#args do
+            local arg1 = args[i]
+            if string.match(arg1,"%.[lL][nN][kK]$") then
+                arg1 = getfolder(arg1)
+            end
+            args[i] = arg1
+        end
+        if org_cd then
+            org_cd(args)
+        else
+            args[0] = "__cd__"
+            nyagos.exec(args)
+        end
     end
 end
--- nyagos.d/trash.lua
+++ nyagos.d/trash.lua
@@ -1,14 +1,22 @@
-nyagos.ole = nyagos.ole or require("nyole")
-local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
-local shellApp = nyagos.ole.create_object_utf8("Shell.Application")
-local trashBox = shellApp:NameSpace(math.tointeger(10))
-if trashBox.MoveHere then
-    nyagos.alias.trash = function(args)
-        args = nyagos.glob(table.unpack(args))
-        for i=1,#args do
-            trashBox:MoveHere(fsObj:GetAbsolutePathName(args[i]))
-        end
+if not nyagos.ole then
+    local status
+    status,nyagos.ole = pcall(require,"nyole")
+    if not status then
+        nyagos.ole = nil
     end
-else
-    nyagos.writerr("Warning: trash.lua requires nyaole.dll 0.0.0.5 or later\n")
+end
+if nyagos.ole then
+    local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
+    local shellApp = nyagos.ole.create_object_utf8("Shell.Application")
+    local trashBox = shellApp:NameSpace(math.tointeger(10))
+    if trashBox.MoveHere then
+        nyagos.alias.trash = function(args)
+            args = nyagos.glob(table.unpack(args))
+            for i=1,#args do
+                trashBox:MoveHere(fsObj:GetAbsolutePathName(args[i]))
+            end
+        end
+    else
+        nyagos.writerr("Warning: trash.lua requires nyaole.dll 0.0.0.5 or later\n")
+    end
 end
