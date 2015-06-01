fcedbbc4de87770fd03e3fdc074e36c0785a6e69
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-06-02 08:14:13 +0900

    Let ezoe.lua's cd use not nyole.dll but nyagos.stat()

--- catalog.d/ezoe.lua
+++ catalog.d/ezoe.lua
@@ -3,28 +3,21 @@ nyagos.on_command_not_found = function(args)
     return true
 end
 
-if not nyagos.ole then
-    local status
-    status, nyagos.ole = pcall(require,"nyole")
-    if not status then
-        nyagos.ole = nil
-    end
-end
-
-if nyagos.ole then
-    local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
-    local cd = nyagos.alias.cd
-    nyagos.alias.cd = function(args)
-        local success=true
-        for i=1,#args do
-            local dir=args[i]
-            if dir:sub(1,1) ~= "-" and not dir:match("%.[lL][nN][kK]$") then
-                if not fsObj:FolderExists(dir) then
-                    nyagos.writerr(dir..": ディレクトリではない。\n")
-                    return
-                end
+local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
+local cd = nyagos.alias.cd
+nyagos.alias.cd = function(args)
+    local success=true
+    for i=1,#args do
+        local dir=args[i]
+        if dir:sub(1,1) ~= "-" and not dir:match("%.[lL][nN][kK]$") then
+            local stat = nyagos.stat(dir)
+            if not stat or not stat.isdir  then
+                nyagos.writerr(dir..": ディレクトリではない。\n")
+                success = false
             end
         end
+    end
+    if success then
         if cd then
             cd(args)
         else
