23d89250cd2941d817a3f3aaffb4ae303af8f416
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-06-01 20:03:03 +0900

    Fixed ezoe.lua did not work when the parameter is not a folder but a file.

--- catalog.d/ezoe.lua
+++ catalog.d/ezoe.lua
@@ -3,23 +3,28 @@ nyagos.on_command_not_found = function(args)
     return true
 end
 
-local cd = nyagos.alias.cd
-nyagos.alias.cd = function(args)
-    local success=true
-    for i=1,#args do
-        local dir=args[i]
-        if dir:sub(1,1) ~= "-" and not dir:match("%.[lL][nN][kK]$") then
-            local nul=nyagos.pathjoin(dir,"nul")
-            local fd=io.open(nul)
-            if fd then
-                fd:close()
-            else
-                nyagos.writerr(dir..": ディレクトリではない。\n")
-                success = false
+if not nyagos.ole then
+    local status
+    status, nyagos.ole = pcall(require,"nyole")
+    if not status then
+        nyagos.ole = nil
+    end
+end
+
+if nyagos.ole then
+    local fsObj = nyagos.ole.create_object_utf8("Scripting.FileSystemObject")
+    local cd = nyagos.alias.cd
+    nyagos.alias.cd = function(args)
+        local success=true
+        for i=1,#args do
+            local dir=args[i]
+            if dir:sub(1,1) ~= "-" and not dir:match("%.[lL][nN][kK]$") then
+                if not fsObj:FolderExists(dir) then
+                    nyagos.writerr(dir..": ディレクトリではない。\n")
+                    return
+                end
             end
         end
-    end
-    if success then
         if cd then
             cd(args)
         else
@@ -28,5 +33,4 @@ nyagos.alias.cd = function(args)
         end
     end
 end
-
 -- vim:set fenc=utf8 --
