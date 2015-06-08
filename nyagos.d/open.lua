119a5b069b9aaba108c4eef7ff817e952eb2a2f9
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-06-08 12:12:14 +0900

    Fixed open.lua did not print error when wildcard did not match anyfiles.

--- nyagos.d/open.lua
+++ nyagos.d/open.lua
@@ -4,19 +4,29 @@ nyagos.alias("open",function(args)
         local list=nyagos.glob(args[i])
         if list and #list >= 1 then
             for i=1,#list do
-                assert(nyagos.shellexecute("open",list[i]))
+                local fd = io.open(list[i])
+                if fd then
+                    fd:close()
+                    assert(nyagos.shellexecute("open",list[i]))
+                else
+                    nyagos.writerr(list[i]..": not found.\n")
+                end
             end
         else
-            if nyagos.access(args[i],0) then
+            local fd = io.open(args[i])
+            if fd then
+                fd:close()
                 assert(nyagos.shellexecute("open",args[i]))
             else
-                print(args[i] .. ": can not get status")
+                print(args[i] .. ": not found.\n")
             end
         end
         count = count +1
     end
     if count <= 0 then
-        if nyagos.access(".\\open.cmd",0) then
+        local fd = io.open("open.cmd")
+        if fd then
+            fd:close()
             nyagos.exec("open.cmd")
         else
             assert(nyagos.shellexecute("open","."))
