ff437e91a4d28f09d6cc9b15a72064527724f910
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-06-13 19:00:57 +0900

    dos.GetFileAttributesFromFileInfo can read *FindFiles

--- dos/fileattr.go
+++ dos/fileattr.go
@@ -6,7 +6,13 @@ import (
 )
 
 func GetFileAttributesFromFileInfo(status os.FileInfo) uint32 {
-	return status.Sys().(*syscall.Win32FileAttributeData).FileAttributes
+	if it, ok := status.Sys().(*syscall.Win32FileAttributeData); ok {
+		return it.FileAttributes
+	} else if it, ok := status.(*FindFiles); ok {
+		return it.data1.FileAttributes
+	} else {
+		panic("Can not get fileatttribute")
+	}
 }
 
 func GetFileAttributes(path string) (uint32, error) {
