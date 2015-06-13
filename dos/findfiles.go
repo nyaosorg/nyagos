853a094ad346fcec4af79890761197cda4bdd1d2
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-06-13 18:59:27 +0900

    PowerUp FindFiles for ls

--- dos/findfiles.go
+++ dos/findfiles.go
@@ -1,10 +1,18 @@
 package dos
 
-import "syscall"
+import (
+	"os"
+	"syscall"
+	"time"
+)
 
 type FindFiles struct {
 	handle syscall.Handle
 	data1  syscall.Win32finddata
+}
+
+func (this *FindFiles) Clone() *FindFiles {
+	return &FindFiles{this.handle, this.data1}
 }
 
 func FindFirst(pattern string) (*FindFiles, error) {
@@ -24,6 +32,29 @@ func (this *FindFiles) Name() string {
 	return syscall.UTF16ToString(this.data1.FileName[:])
 }
 
+func (this *FindFiles) Size() int64 {
+	return int64((this.data1.FileSizeHigh << 32) | this.data1.FileSizeLow)
+}
+
+func (this *FindFiles) ModTime() time.Time {
+	return time.Unix(0, this.data1.LastWriteTime.Nanoseconds())
+}
+
+func (this *FindFiles) Mode() os.FileMode {
+	m := os.FileMode(0444)
+	if this.IsDir() {
+		m |= 0111 | os.ModeDir
+	}
+	if !this.IsReadOnly() {
+		m |= 0222
+	}
+	return m
+}
+
+func (this *FindFiles) Sys() interface{} {
+	return this.data1
+}
+
 func (this *FindFiles) FindNext() error {
 	return syscall.FindNextFile(this.handle, &this.data1)
 }
@@ -36,12 +67,28 @@ func (this *FindFiles) Attribute() uint32 {
 	return this.data1.FileAttributes
 }
 
+func (this *FindFiles) IsReparsePoint() bool {
+	return (this.Attribute() & FILE_ATTRIBUTE_REPARSE_POINT) != 0
+}
+
+func (this *FindFiles) IsReadOnly() bool {
+	return (this.Attribute() & syscall.FILE_ATTRIBUTE_READONLY) != 0
+}
+
+func (this *FindFiles) IsExecutable() bool {
+	return IsExecutableSuffix(this.Name())
+}
+
 func (this *FindFiles) IsDir() bool {
 	return (this.Attribute() & syscall.FILE_ATTRIBUTE_DIRECTORY) != 0
 }
 
 func (this *FindFiles) IsHidden() bool {
 	return (this.Attribute() & syscall.FILE_ATTRIBUTE_HIDDEN) != 0
+}
+
+func (this *FindFiles) IsSystem() bool {
+	return (this.Attribute() & syscall.FILE_ATTRIBUTE_SYSTEM) != 0
 }
 
 func DirName(path string) string {
@@ -56,5 +103,21 @@ func DirName(path string) string {
 		return path[0:(lastroot + 1)]
 	} else {
 		return ""
+	}
+}
+
+func ForFiles(pattern string, callback func(*FindFiles) bool) error {
+	this, err := FindFirst(pattern)
+	if err != nil {
+		return err
+	}
+	defer this.Close()
+	for {
+		if !callback(this.Clone()) {
+			return nil
+		}
+		if err := this.FindNext(); err != nil {
+			return nil
+		}
 	}
 }
