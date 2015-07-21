4074b4951f9be1c13579d22ad705c20435756383
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-07-21 15:47:39 +0900

    Fixed panic on nyagos.stat(nil)

--- main/lua_cmd.go
+++ main/lua_cmd.go
@@ -417,7 +417,7 @@ func cmdStat(L lua.Lua) int {
 	}
 	var stat os.FileInfo
 	var path_ string
-	if len(path) >= 0 && path[len(path)-1] == '\\' {
+	if len(path) > 0 && path[len(path)-1] == '\\' {
 		path_ = dos.Join(path, ".")
 	} else {
 		path_ = path
