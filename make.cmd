a1de19845345adeedca5ccee2d310f986d8e394b
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-09-13 03:13:07 +0900

    Fixed make.cmd errored relative-path from main/

--- make.cmd
+++ make.cmd
@@ -18,8 +18,8 @@ goto build
         for /F %%I IN ('dir /s /b /aa *.go') do go fmt "%%I" & attrib -A "%%I"
         pushd "%~dp0main"
         for %%I in (windres.exe) do if not "%%~$PATH:I" == "" cscript //nologo make_rc.js | windres.exe --output-format=coff -o nyagos.syso
-        for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -o "%~dp0nyagos.exe" -ldflags "-X main.stamp=%DATE% -X main.commit=%%V %X_VERSION%"
         popd
+        for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -o nyagos.exe -ldflags "-X main.stamp=%DATE% -X main.commit=%%V %X_VERSION%" .\main
         goto end
 
 :fmt
