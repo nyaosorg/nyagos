c92a28006bec44245c7060a3a0adb2fb4e37dced
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-09-13 09:01:08 +0900

    Change script to make .syso from JScript To Powershell

---
+++ main/makesyso.ps1
@@ -0,0 +1,55 @@
+Set-PSDebug -strict
+
+# If windres.exe is not found, quit immediately.
+try{
+    Get-Command windres -ErrorAction Stop | %{}
+}catch{
+    Write-Host "makesyso.ps1: windres.exe not found"
+    exit
+}
+
+$text = @"
+#include <winver.h>
+
+GOPHER ICON NYAGOS.ICO
+"@
+
+$version = $env:version
+if ($version ){
+    Write-Host "makesyso.ps1: set icon and version $version"
+    $vcomma = ($version -replace "[\._]",",")
+    $text = $text + @"
+
+VS_VERSION_INFO    VERSIONINFO
+FILEVERSION        $vcomma
+PRODUCTVERSION     $vcomma
+FILEFLAGSMASK      VS_FFI_FILEFLAGSMASK
+FILEFLAGS          0x00000000L
+FILEOS             VOS__WINDOWS32
+FILETYPE           VFT_APP
+FILESUBTYPE        VFT2_UNKNOWN
+BEGIN
+    BLOCK "VarFileInfo"
+    BEGIN
+        VALUE "Translation", 0x0411, 0x04E4
+    END
+    BLOCK "StringFileInfo"
+    BEGIN
+        BLOCK "041104E4"
+        BEGIN
+            VALUE "CompanyName",      "NYAOS.ORG"
+            VALUE "FileDescription",  "Extended Commandline Shell"
+            VALUE "FileVersion",      "$version\0"
+            VALUE "LegalCopyright",   "Copyright (C) 2014-2015 HAYAMA_Kaoru\0"
+            VALUE "OriginalFilename", "NYAGOS.EXE\0"
+            VALUE "ProductName",      "Nihongo Yet Another GOing Shell\0"
+            VALUE "ProductVersion",   "$version\0"
+        END
+    END
+END
+"@
+}else{
+    Write-Host "makesyso.ps1: set icon."
+}
+
+$text | windres.exe --output-format=coff -o nyagos.syso
--- make.cmd
+++ make.cmd
@@ -17,7 +17,7 @@ goto build
 :build
         for /F %%I IN ('dir /s /b /aa *.go') do go fmt "%%I" & attrib -A "%%I"
         pushd "%~dp0main"
-        for %%I in (windres.exe) do if not "%%~$PATH:I" == "" cscript //nologo make_rc.js | windres.exe --output-format=coff -o nyagos.syso
+        powershell -ExecutionPolicy RemoteSigned -File makesyso.ps1
         popd
         for /F %%V in ('git log -1 --pretty^=format:%%H') do go build -o nyagos.exe -ldflags "-X main.stamp=%DATE% -X main.commit=%%V %X_VERSION%" .\main
         goto end
