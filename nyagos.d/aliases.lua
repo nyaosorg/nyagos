ff5abb493cc0416f9e73411bedc68b2a93388e70
HAYAMA_Kaoru <iyahaya@nifty.com>
2015-12-05 19:32:26 +0900

    nyagos.d/aliases.lua: Add lua_f (for #84)

--- nyagos.d/aliases.lua
+++ nyagos.d/aliases.lua
@@ -1,3 +1,7 @@
 nyagos.alias.ls='ls -oF $*'
 nyagos.alias.lua_e=function(args) assert(load(args[1]))() end
+nyagos.alias.lua_f=function(args)
+    local path=table.remove(args,1)
+    assert(loadfile(path))(args)
+end
 nyagos.alias["for"]='%COMSPEC% /c "@set PROMPT=$G & @for $*"'
