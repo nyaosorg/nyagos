8eb28fd656ce11caf9b3970b38b63d6dfa44a8fe
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-03-20 22:51:03 +0900

    peco.lua := peco_complete.lua + peco_history.lua

--- nyagos.d/catalog/peco.lua
+++ nyagos.d/catalog/peco.lua
@@ -18,3 +18,18 @@ nyagos.bindkey("C-o",function(this)
     return result
 end)
 
+nyagos.bindkey("C_R", function(this)
+    local path = nyagos.pathjoin(nyagos.env.appdata,'NYAOS_ORG\\nyagos.history')
+    local result = nyagos.eval('peco < '..path)
+    this:call("CLEAR_SCREEN")
+    return result
+end)
+
+nyagos.bindkey("M_H" , function(this)
+    local result = nyagos.eval('cd --history | peco')
+    this:call("CLEAR_SCREEN")
+    if string.find(result,' ') then
+        result = '"'..result..'"'
+    end
+    return result
+end)
