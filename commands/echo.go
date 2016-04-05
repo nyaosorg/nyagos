bb8bd3950619342211491ef62fbc23d1446333c6
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-05 16:08:13 +0900

    Fixed: built-in echo used LF (not CRLF) #124

--- commands/echo.go
+++ commands/echo.go
@@ -2,12 +2,20 @@ package commands
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/mattn/go-isatty"
 
 	. "../interpreter"
 )
 
 func cmd_echo(cmd *Interpreter) (ErrorLevel, error) {
-	fmt.Fprintln(cmd.Stdout, strings.Join(cmd.Args[1:], " "))
+	fmt.Fprint(cmd.Stdout, strings.Join(cmd.Args[1:], " "))
+	if f, f_ok := cmd.Stdout.(*os.File); f_ok && isatty.IsTerminal(f.Fd()) {
+		fmt.Fprint(cmd.Stdout, "\n")
+	} else {
+		fmt.Fprint(cmd.Stdout, "\r\n")
+	}
 	return NOERROR, nil
 }
