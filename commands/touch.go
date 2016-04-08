14db2f35bdcf8eaf8159cf3e74572b8cc311338e
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-08 14:00:54 +0900

    Support: touch [[CC]YY]MMDDmmhh[.ss]

--- commands/touch.go
+++ commands/touch.go
@@ -3,10 +3,23 @@ package commands
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 
 	. "../interpreter"
 )
+
+var timePattern = regexp.MustCompile(
+	"^(?:(\\d\\d)?(\\d\\d))?(\\d\\d)(\\d\\d)(?:(\\d\\d)(\\d\\d))(?:\\.(\\d\\d))?$")
+
+func atoiOr(s string, orelse int) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		return orelse
+	}
+	return val
+}
 
 func cmd_touch(this *Interpreter) (ErrorLevel, error) {
 	errcnt := 0
@@ -16,6 +29,23 @@ func cmd_touch(this *Interpreter) (ErrorLevel, error) {
 			fmt.Fprintf(this.Stderr,
 				"%s: built-in touch: Not implemented.\n",
 				arg1)
+		} else if m := timePattern.FindStringSubmatch(arg1); m != nil {
+			yy := atoiOr(m[2], stamp.Year()%100)
+			cc, cc_err := strconv.Atoi(m[1])
+			if cc_err != nil {
+				if yy <= 68 {
+					cc = 20
+				} else {
+					cc = 19
+				}
+			}
+			year := yy + cc*100
+			month, _ := strconv.Atoi(m[3])
+			mday, _ := strconv.Atoi(m[4])
+			hour := atoiOr(m[5], 0)
+			min := atoiOr(m[6], 0)
+			sec := atoiOr(m[7], 0)
+			stamp = time.Date(year, time.Month(month), mday, hour, min, sec, 0, time.Local)
 		} else {
 			fd, err := os.OpenFile(arg1, os.O_APPEND, 0666)
 			if err != nil && os.IsNotExist(err) {
