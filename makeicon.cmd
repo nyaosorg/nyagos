b8ba3add7c695248929270145d002570223cab6f
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-10 11:35:53 +0900

    Use PowerShell instead of JScrpit to know where is desktop

--- makeicon.cmd
+++ makeicon.cmd
@@ -4,4 +4,5 @@ if "%INSTALLDIR%" == "" set "INSTALLDIR=%~dp0"
 if not exist "%INSTALLDIR%\nyagos.exe" set "INSTALLDIR=%~dp0"
 if not "%2" == "" set "INSTALLDIR=%2"
 
-for /F %%I in ('cscript /nologo specialfolders.js desktop') do echo Desktop=%%I & cscript //nologo lnk.js "%INSTALLDIR%\nyagos.exe" "%%I\Nihongo Yet Another GOing Shell.lnk" "WorkingDirectory=%USERPROFILE%" "HotKey=CTRL+ALT+N" "Description=Nihongo Yet Another GOing Shell"
+for /F "usebackq" %%I in (`powershell "[Environment]::GetFolderPath('Desktop')"`) do set "DESKTOP=%%I"
+cscript //nologo lnk.js "%INSTALLDIR%\nyagos.exe" "%DESKTOP%\Nihongo Yet Another GOing Shell.lnk" "WorkingDirectory=%USERPROFILE%" "HotKey=CTRL+ALT+N" "Description=Nihongo Yet Another GOing Shell"
--- specialfolders.js
+++
@@ -1,4 +0,0 @@
-var objShell = new ActiveXObject("WScript.Shell");
-for(var i=0 ; i < WScript.Arguments.length ; i++ ){
-    WScript.Echo( objShell.SpecialFolders(WScript.Arguments.Item(i) ) )
-}
