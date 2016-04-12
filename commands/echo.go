767314f5b2823bd4e2e7ae0a563ef974f0ae239b
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-12 20:51:54 +0900

    Remove unnecessary cast of cmd_echo

--- commands/echo.go
+++ commands/echo.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/mattn/go-isatty"
@@ -12,7 +11,7 @@ import (
 
 func cmd_echo(cmd *Interpreter) (ErrorLevel, error) {
 	fmt.Fprint(cmd.Stdout, strings.Join(cmd.Args[1:], " "))
-	if f, f_ok := cmd.Stdout.(*os.File); f_ok && isatty.IsTerminal(f.Fd()) {
+	if isatty.IsTerminal(cmd.Stdio[1].Fd()) {
 		fmt.Fprint(cmd.Stdout, "\n")
 	} else {
 		fmt.Fprint(cmd.Stdout, "\r\n")
