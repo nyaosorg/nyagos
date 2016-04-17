4c7df0cddd3d7aabcf51fcd2592217db1141fced
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-17 13:55:34 +0900

    Replace makeconst.lua to makeconst.ps1

--- conio/makeconst.cmd
+++ conio/makeconst.cmd
@@ -1,15 +1,12 @@
-setlocal
-for %%I in (gcc.exe) do set "MINGW=%%~dp$PATH:I.."
-lua %~dp0..\makeconst.lua conio ^
-	CTRL_CLOSE_EVENT ^
-	CTRL_LOGOFF_EVENT ^
-	CTRL_SHUTDOWN_EVENT ^
+powershell -ExecutionPolicy RemoteSigned -File  "%~dp0..\makeconst.ps1" ^
+        conio ^
+        CTRL_CLOSE_EVENT ^
+        CTRL_LOGOFF_EVENT ^
+        CTRL_SHUTDOWN_EVENT ^
         CTRL_C_EVENT ^
         ENABLE_ECHO_INPUT ^
         ENABLE_PROCESSED_INPUT ^
-	STD_INPUT_HANDLE ^
-	STD_OUTPUT_HANDLE ^
-	KEY_EVENT ^
-	"%MINGW%\include" > const.go
+        STD_INPUT_HANDLE ^
+        STD_OUTPUT_HANDLE ^
+        KEY_EVENT > const.go
 go fmt const.go
-endlocal
--- dos/makeconst.cmd
+++ dos/makeconst.cmd
@@ -1,10 +1,10 @@
-lua %~dp0..\makeconst.lua dos ^
+powershell -ExecutionPolicy RemoteSigned -File "%~dp0..\makeconst.ps1" ^
+        dos ^
         FILE_ATTRIBUTE_NORMAL ^
-	FILE_ATTRIBUTE_REPARSE_POINT ^
-	FILE_ATTRIBUTE_HIDDEN ^
-	CP_THREAD_ACP ^
-	MOVEFILE_REPLACE_EXISTING ^
-	MOVEFILE_COPY_ALLOWED ^
-	MOVEFILE_WRITE_THROUGH ^
-	C:\MingW\include > const.go
+        FILE_ATTRIBUTE_REPARSE_POINT ^
+        FILE_ATTRIBUTE_HIDDEN ^
+        CP_THREAD_ACP ^
+        MOVEFILE_REPLACE_EXISTING ^
+        MOVEFILE_COPY_ALLOWED ^
+        MOVEFILE_WRITE_THROUGH > const.go
 go fmt const.go
