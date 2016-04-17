3faced78b790151a050620fa89d02ccc0ed31f96
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-04-17 23:01:52 +0900

    Remake {conio,dos,lua}/const.go with new makeconst.{cmd,c}

--- lua/const.go
+++ lua/const.go
@@ -1,7 +1,6 @@
 package lua
 
 const LUA_REGISTRYINDEX = -1001000
-
 const LUA_TBOOLEAN = 1
 const LUA_TFUNCTION = 6
 const LUA_TLIGHTUSERDATA = 2
@@ -11,9 +10,7 @@ const LUA_TSTRING = 4
 const LUA_TTABLE = 5
 const LUA_TTHREAD = 8
 const LUA_TUSERDATA = 7
-
 const LUA_FILEHANDLE = "FILE*"
-
 const LUA_OK = 0
 const LUA_ERRSYNTAX = 3
 const LUA_ERRMEM = 4
