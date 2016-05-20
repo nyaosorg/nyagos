289864bdea3d28677b556c58fb6662db4098b11c
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-05-20 15:12:13 +0900

    trash.lua: replace OLE-Call new style.

--- nyagos.d/trash.lua
+++ nyagos.d/trash.lua
@@ -6,11 +6,11 @@ nyagos.alias.trash = function(args)
     end
     local fsObj = nyagos.create_object("Scripting.FileSystemObject")
     local shellApp = nyagos.create_object("Shell.Application")
-    local trashBox = shellApp:_call("NameSpace",math.tointeger(10))
+    local trashBox = shellApp:NameSpace(math.tointeger(10))
     args = nyagos.glob(table.unpack(args))
     for i=1,#args do
-        if fsObj:_call("FileExists",args[i]) or fsObj:_call("FolderExists",args[i]) then
-            trashBox:_call("MoveHere",fsObj:_call("GetAbsolutePathName",args[i]))
+        if fsObj:FileExists(args[i]) or fsObj:FolderExists(args[i]) then
+            trashBox:MoveHere(fsObj:GetAbsolutePathName(args[i]))
         else
             nyagos.writerr(args[i]..": such a file or directory not found.\n")
         end
