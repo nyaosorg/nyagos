d393cab331f360895c32369458d580d0a2b21cc0
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-06-09 08:10:12 +0900

    conio/const.go is created by go-importconst

--- conio/makeconst.cmd
+++ conio/makeconst.cmd
@@ -1,4 +1,9 @@
-pushd "%~dp0"
-gcc makeconst\makeconst.c && a > const.go && go fmt const.go
-if exist a.exe del a.exe
-popd
+go-importconst conio ^
+    d(CTRL_CLOSE_EVENT) ^
+    d(CTRL_LOGOFF_EVENT) ^
+    d(CTRL_SHUTDOWN_EVENT) ^
+    d(CTRL_C_EVENT) ^
+    d(ENABLE_ECHO_INPUT) ^
+    d(ENABLE_PROCESSED_INPUT) ^
+    u32x(STD_INPUT_HANDLE) ^
+    u32x(STD_OUTPUT_HANDLE) ^
