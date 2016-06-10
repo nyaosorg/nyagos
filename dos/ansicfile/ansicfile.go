5c801796c12ed2f4ed53366c44ef39ea371c5232
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-06-11 08:34:05 +0900

    dos/ansicfile.FilePtr satisfies as io.Reader

--- dos/ansicfile/ansicfile.go
+++ dos/ansicfile/ansicfile.go
@@ -2,6 +2,7 @@ package ansicfile
 
 import (
 	"errors"
+	"io"
 	"syscall"
 	"unsafe"
 )
@@ -10,6 +11,8 @@ var msvcrt = syscall.NewLazyDLL("msvcrt")
 var wfopen = msvcrt.NewProc("_wfopen")
 var fclose = msvcrt.NewProc("fclose")
 var fwrite = msvcrt.NewProc("fwrite")
+var fread = msvcrt.NewProc("fread")
+var feof = msvcrt.NewProc("feof")
 
 type FilePtr uintptr
 
@@ -45,5 +48,30 @@ func (fp FilePtr) Write(p []byte) (int, error) {
 		return n, err
 	} else {
 		return n, errors.New("ansicfile.FilePtr.Write error")
+	}
+}
+
+func (fp FilePtr) Eof() bool {
+	rc, _, _ := feof.Call(uintptr(fp))
+	return rc != 0
+}
+
+func (fp FilePtr) Read(p []byte) (int, error) {
+	if fp.Eof() {
+		return 0, io.EOF
+	}
+	rc, _, err := fread.Call(uintptr(unsafe.Pointer(&p[0])),
+		1, uintptr(len(p)), uintptr(fp))
+	n := int(rc)
+	if n == len(p) {
+		return n, nil
+	} else if err != nil {
+		return n, err
+	} else {
+		if fp.Eof() {
+			return n, io.EOF
+		} else {
+			return n, errors.New("ansicfile.FilePtr.Read error")
+		}
 	}
 }
--- dos/ansicfile/ansicfile_test.go
+++ dos/ansicfile/ansicfile_test.go
@@ -5,16 +5,41 @@ import (
 	"testing"
 )
 
+const sample_data = "12345678910"
+
 func TestOpen(t *testing.T) {
 	fp, err := Open("あ", "w")
 	if err == nil {
 		w := bufio.NewWriter(fp)
-		w.WriteString("12345678910")
+		for i := 0; i < 5; i++ {
+			w.WriteString(sample_data)
+			w.WriteString("\n")
+		}
 		w.Flush()
 		fp.Close()
 	} else {
-		t.Fatalf("NG: Open(\"あ\") Failed by %s", err.Error())
+		t.Fatalf("NG: Write Open(\"あ\") Failed by %s", err.Error())
 	}
+	fp, err = Open("あ", "r")
+	if err == nil {
+		r := bufio.NewScanner(fp)
+		count := 0
+		for r.Scan() {
+			print("read: ", r.Text(), "\n")
+			if r.Text() != sample_data {
+				t.Fatalf("NG: Read data and Write data not equal('%s' != '%s') ",
+					r.Text(), sample_data)
+			}
+			count++
+		}
+		if count != 5 {
+			t.Fatalf("NG: data lack\n")
+		}
+		fp.Close()
+	} else {
+		t.Fatalf("NG: Read Open(\"あ\") Failed by %s", err.Error())
+	}
+
 	fp, err = Open("*", "w")
 	if err == nil {
 		t.Fatalf("NG: Open(\"*\") should failed")
