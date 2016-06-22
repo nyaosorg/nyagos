79fcd1171ffa5fd9cc1819650b4cbe7a1bf4d409
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-06-22 23:13:00 +0900

    package dos: move magic numbers from <windows.h> to "const.go"

--- dos/makeconst.cmd
+++ dos/makeconst.cmd
@@ -6,5 +6,21 @@ go-importconst dos ^
     d(MOVEFILE_REPLACE_EXISTING) ^
     d(MOVEFILE_COPY_ALLOWED) ^
     d(MOVEFILE_WRITE_THROUGH) ^
+    d(SW_HIDE) ^
+    d(SW_MAXIMIZE) ^
+    d(SW_MINIMIZE) ^
+    d(SW_RESTORE) ^
+    d(SW_SHOW) ^
+    d(SW_SHOWDEFAULT) ^
+    d(SW_SHOWMAXIMIZED) ^
+    d(SW_SHOWMINIMIZED) ^
+    d(SW_SHOWMINNOACTIVE) ^
+    d(SW_SHOWNA) ^
+    d(SW_SHOWNOACTIVATE) ^
+    d(SW_SHOWNORMAL) ^
+    up(COINIT_APARTMENTTHREADED) ^
+    up(COINIT_MULTITHREADED) ^
+    up(COINIT_DISABLE_OLE1DDE) ^
+    up(COINIT_SPEED_OVER_MEMORY)
 @echo off
 rem go-importconst -> https://github.com/zetamatta/go-importconst
--- dos/shellexecute.go
+++ dos/shellexecute.go
@@ -10,21 +10,6 @@ var shell32 = syscall.NewLazyDLL("shell32")
 var shellExecute = shell32.NewProc("ShellExecuteW")
 
 const (
-	SW_HIDE            = 0
-	SW_MAXIMIZE        = 3
-	SW_MINIMIZE        = 6
-	SW_RESTORE         = 9
-	SW_SHOW            = 5
-	SW_SHOWDEFAULT     = 1
-	SW_SHOWMAXIMIZED   = 3
-	SW_SHOWMINIMIZED   = 2
-	SW_SHOWMINNOACTIVE = 7
-	SW_SHOWNA          = 8
-	SW_SHOWNOACTIVATE  = 4
-	SW_SHOWNORMAL      = 1
-)
-
-const (
 	EDIT       = "edit"
 	EXPLORE    = "explore"
 	OPEN       = "open"
