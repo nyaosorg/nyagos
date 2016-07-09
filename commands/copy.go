df932e34384d278cb8c2de2d883bc123c6553b2e
HAYAMA_Kaoru <iyahaya@nifty.com>
2016-07-10 00:06:26 +0900

    Optimize copy,move

--- commands/copy.go
+++ commands/copy.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -12,22 +11,27 @@ import (
 	"../dos"
 )
 
+type copymove_t struct {
+	*exec.Cmd
+	Action  func(src, dst string) error
+	IsDirOk bool
+}
+
 func cmd_copy(cmd *exec.Cmd) (int, error) {
-	return cmd_xxxx(cmd.Args,
-		cmd.Stderr,
-		func(src, dst string) error {
+	return copymove_t{
+		Cmd: cmd,
+		Action: func(src, dst string) error {
 			return dos.Copy(src, dst, false)
 		},
-		false)
+	}.Run()
 }
 
 func cmd_move(cmd *exec.Cmd) (int, error) {
-	return cmd_xxxx(cmd.Args,
-		cmd.Stderr,
-		func(src, dst string) error {
-			return dos.Move(src, dst)
-		},
-		true)
+	return copymove_t{
+		Cmd:     cmd,
+		Action:  dos.Move,
+		IsDirOk: true,
+	}.Run()
 }
 
 func cmd_ln(cmd *exec.Cmd) (int, error) {
@@ -35,65 +39,56 @@ func cmd_ln(cmd *exec.Cmd) (int, error) {
 		args := make([]string, 0, len(cmd.Args)-1)
 		args = append(args, cmd.Args[0])
 		args = append(args, cmd.Args[2:]...)
-		return cmd_xxxx(
-			args,
-			cmd.Stderr,
-			func(src, dst string) error {
-				return os.Symlink(src, dst)
-			},
-			true)
+		return copymove_t{
+			Cmd:     cmd,
+			Action:  os.Symlink,
+			IsDirOk: true,
+		}.Run()
 	} else {
-		return cmd_xxxx(
-			cmd.Args,
-			cmd.Stderr,
-			func(src, dst string) error {
-				return os.Link(src, dst)
-			},
-			false)
+		return copymove_t{
+			Cmd:    cmd,
+			Action: os.Link,
+		}.Run()
 	}
 }
 
-func cmd_xxxx(args []string,
-	out io.Writer,
-	action func(src, dst string) error,
-	isDirOk bool) (int, error) {
-	if len(args) <= 2 {
-		fmt.Fprintf(out,
+func (this copymove_t) Run() (int, error) {
+	if len(this.Args) <= 2 {
+		fmt.Fprintf(this.Stderr,
 			"Usage: %s [/y] SOURCE-FILENAME DESITINATE-FILENAME\n"+
 				"       %s [/y] FILENAMES... DESINATE-DIRECTORY\n",
-			args[0], args[0])
+			this.Args[0], this.Args[0])
 		return 0, nil
 	}
-	fi, err := os.Stat(args[len(args)-1])
+	fi, err := os.Stat(this.Args[len(this.Args)-1])
 	isDir := err == nil && fi.Mode().IsDir()
 	all := false
-	for i, n := 1, len(args)-1; i < n; i++ {
-		if args[i] == "/y" {
+	for i, src := range this.Args[1 : len(this.Args)-1] {
+		if src == "/y" {
 			all = true
 			continue
 		}
-		src := args[i]
-		dst := args[n]
+		dst := this.Args[len(this.Args)-1]
 		if isDir {
 			dst = filepath.Join(dst, filepath.Base(src))
 		}
-		if !isDirOk {
+		if !this.IsDirOk {
 			fi, err := os.Stat(src)
 			if err == nil && fi.Mode().IsDir() {
-				fmt.Fprintf(out, "%s is directory and passed.\n", src)
+				fmt.Fprintf(this.Stderr, "%s is directory and passed.\n", src)
 				continue
 			}
 		}
 
-		fmt.Fprintf(out, "%s -> %s\n", src, dst)
+		fmt.Fprintf(this.Stderr, "%s -> %s\n", src, dst)
 		if !all {
 			fi, err := os.Stat(dst)
 			if fi != nil && err == nil {
-				fmt.Fprintf(out,
+				fmt.Fprintf(this.Stderr,
 					"%s: override? [Yes/No/All/Quit] ",
 					dst)
 				ch := getch.Rune()
-				fmt.Fprintf(out, "%c\n", ch)
+				fmt.Fprintf(this.Stderr, "%c\n", ch)
 				switch ch {
 				case 'y', 'Y':
 
@@ -106,18 +101,15 @@ func cmd_xxxx(args []string,
 				}
 			}
 		}
-		err := action(src, dst)
+		err := this.Action(src, dst)
 		if err != nil {
-			if i == n-1 {
+			if i == len(this.Args)-1 {
 				return 1, err
 			}
-			fmt.Fprintf(out, "%s\nContinue? [Yes/No] ", err.Error())
+			fmt.Fprintf(this.Stderr, "%s\nContinue? [Yes/No] ", err.Error())
 			ch := getch.Rune()
-			fmt.Fprintf(out, "%c\n", ch)
-			switch ch {
-			case 'y', 'Y':
-
-			default:
+			fmt.Fprintf(this.Stderr, "%c\n", ch)
+			if ch != 'y' && ch != 'Y' {
 				return 0, nil
 			}
 		}
