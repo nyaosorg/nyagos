ee047a058d60a8a1ed29ee55ae49987d6cceada4
Kaoru HAYAMA <iyahaya@nifty.com>
2016-09-19 10:45:12 +0900

    Load all-lua-script with nyagos.exe itself not by nyagos.lua

--- main/loadscr.go
+++ main/loadscr.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"../dos"
 	"../lua"
@@ -15,11 +16,45 @@ func loadScripts(L lua.Lua) {
 		fmt.Fprintln(os.Stderr, exeNameErr)
 	}
 	exeFolder := filepath.Dir(exeName)
+
+	// for compatibility
 	nyagos_lua := filepath.Join(exeFolder, "nyagos.lua")
 	if _, err := os.Stat(nyagos_lua); err == nil {
 		err := L.Source(nyagos_lua)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
+		}
+		return
+	}
+
+	nyagos_d := filepath.Join(exeFolder, "nyagos.d")
+	nyagos_d_fd, nyagos_d_err := os.Open(nyagos_d)
+	if nyagos_d_err == nil {
+		defer nyagos_d_fd.Close()
+		names, err := nyagos_d_fd.Readdirnames(-1)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		} else {
+			for _, name1 := range names {
+				if strings.HasSuffix(strings.ToLower(name1), ".lua") {
+					path1 := filepath.Join(nyagos_d, name1)
+					err1 := L.Source(path1)
+					if err1 != nil {
+						fmt.Fprintf(os.Stderr, "%s: %s\n", name1, err1.Error())
+					}
+				}
+			}
+		}
+	}
+	home := os.Getenv("HOME")
+	if home == "" {
+		home = os.Getenv("USERPROFILE")
+	}
+	dot_nyagos := filepath.Join(home, ".nyagos")
+	if _, err := os.Stat(dot_nyagos); err == nil {
+		err1 := L.Source(dot_nyagos)
+		if err1 != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", dot_nyagos, err1.Error())
 		}
 	}
 }
---
+++ nyagos.d/use.lua
@@ -0,0 +1,16 @@
+function use(name)
+    local catalog_d = string.gsub(nyagos.exe,"%.[eE][xX][eE]$",".d\\catalog")
+    name = string.gsub(name,"%.lua$","") .. ".lua"
+    local fname = nyagos.pathjoin(catalog_d,name)
+    local chank,err=nyagos.loadfile(fname)
+    if err then
+        print(err)
+    elseif chank then
+        local ok,err=pcall(chank)
+        if not ok then
+            print(fname .. ": " ..err)
+        end
+    else
+        print(fname .. ":fail to load")
+    end
+end
