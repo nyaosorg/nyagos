5ed83622a4496eb0630bda1bb6a5006b1c3254d5
Kaoru HAYAMA <iyahaya@nifty.com>
2016-09-25 02:34:46 +0900

    rewrite `source` command with bufio

--- commands/source.go
+++ commands/source.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -15,51 +14,93 @@ import (
 	"../dos"
 )
 
-func cmd_source(cmd *exec.Cmd) (int, error) {
-	args := cmd.Args
-	verbose := false
-	if len(args) >= 2 && args[1] == "-v" {
-		verbose = true
-		args = args[1:]
+func load_envfile(fname string, verbose io.Writer) error {
+	fp, err := os.Open(fname)
+	if err != nil {
+		return err
 	}
-	if len(cmd.Args) < 2 {
-		return 255, nil
-	}
-	tempDir := os.TempDir()
-	pid := os.Getpid()
-	batchPath := filepath.Join(tempDir, fmt.Sprintf("nyagos-%d.cmd", pid))
-	envTxtPath := filepath.Join(tempDir, fmt.Sprintf("nyagos-%d.tmp", pid))
-	pwdTxtPath := filepath.Join(tempDir, fmt.Sprintf("nyagos_%d.tmp", pid))
+	defer fp.Close()
 
+	scan := bufio.NewScanner(fp)
+	for scan.Scan() {
+		line, err := mbcs.AtoU(scan.Bytes())
+		if err != nil {
+			return err
+		}
+		line = strings.TrimSpace(line)
+		eqlPos := strings.Index(line, "=")
+		if eqlPos > 0 {
+			left := line[:eqlPos]
+			right := line[eqlPos+1:]
+			if left != "ERRORLEVEL_" {
+				if verbose != nil {
+					fmt.Fprintf(verbose, "%s=%s\n", left, right)
+				}
+				os.Setenv(left, right)
+			}
+		}
+	}
+	if err := scan.Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func load_pwdfile(fname string, verbose io.Writer) error {
+	fp, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+	scan := bufio.NewScanner(fp)
+	if !scan.Scan() {
+		return fmt.Errorf("Could not load the new current directory from %s", fname)
+	}
+	if err := scan.Err(); err != nil {
+		return err
+	}
+	line, err := mbcs.AtoU(scan.Bytes())
+	if err != nil {
+		return err
+	}
+	line = strings.TrimSpace(line)
+	if verbose != nil {
+		fmt.Fprintf(verbose, "cd \"%s\"\n", line)
+	}
+	os.Chdir(line)
+	return nil
+}
+
+func call_batch(batch string, args []string, env string, pwd string, verbose io.Writer) (int, error) {
 	params := []string{
 		os.Getenv("COMSPEC"),
 		"/C",
-		batchPath,
+		batch,
 	}
-	batchFd, batchFd_err := os.Create(batchPath)
-	if batchFd_err != nil {
-		return -1, batchFd_err
+	fd, err := os.Create(batch)
+	if err != nil {
+		return 1, err
 	}
-	var batchWriter io.Writer
-	if verbose {
-		batchWriter = io.MultiWriter(batchFd, cmd.Stdout)
+	var writer *bufio.Writer
+	if verbose != nil {
+		writer = bufio.NewWriter(io.MultiWriter(fd, verbose))
 	} else {
-		batchWriter = batchFd
+		writer = bufio.NewWriter(fd)
 	}
-	fmt.Fprint(batchWriter, "@call")
-	for _, v := range args[1:] {
+	fmt.Fprint(writer, "@call")
+	for _, v := range args {
 		if strings.ContainsRune(v, ' ') {
-			fmt.Fprintf(batchWriter, " \"%s\"", v)
+			fmt.Fprintf(writer, " \"%s\"", v)
 		} else {
-			fmt.Fprintf(batchWriter, " %s", v)
+			fmt.Fprintf(writer, " %s", v)
 		}
 	}
-	fmt.Fprintf(batchWriter, "\n@set \"ERRORLEVEL_=%%ERRORLEVEL%%\"\n")
-	fmt.Fprintf(batchWriter, "@set > \"%s\"\n", envTxtPath)
-	fmt.Fprintf(batchWriter, "@cd > \"%s\"\n", pwdTxtPath)
-	fmt.Fprintf(batchWriter, "@exit /b \"%%ERRORLEVEL_%%\"\n")
-	batchFd.Close()
-	defer os.Remove(batchPath)
+	fmt.Fprintf(writer, "\n@set \"ERRORLEVEL_=%%ERRORLEVEL%%\"\n")
+	fmt.Fprintf(writer, "@set > \"%s\"\n", env)
+	fmt.Fprintf(writer, "@cd > \"%s\"\n", pwd)
+	fmt.Fprintf(writer, "@exit /b \"%%ERRORLEVEL_%%\"\n")
+	writer.Flush()
+	fd.Close()
 
 	cmd2 := exec.Cmd{Path: params[0], Args: params}
 	if err := cmd2.Run(); err != nil {
@@ -69,60 +110,52 @@ func cmd_source(cmd *exec.Cmd) (int, error) {
 	if !errorlevelOk {
 		errorlevel = 255
 	}
-	defer os.Remove(envTxtPath)
-	defer os.Remove(pwdTxtPath)
+	return errorlevel, nil
+}
 
-	fp, err := os.Open(envTxtPath)
+func cmd_source(cmd *exec.Cmd) (int, error) {
+	var verbose io.Writer
+	args := make([]string, 0, len(cmd.Args))
+	debug := false
+	for _, arg1 := range cmd.Args[1:] {
+		switch arg1 {
+		case "-v":
+			verbose = cmd.Stderr
+		case "-d":
+			debug = true
+		default:
+			args = append(args, arg1)
+		}
+	}
+	if len(cmd.Args) <= 0 {
+		return 255, nil
+	}
+
+	tempDir := os.TempDir()
+	pid := os.Getpid()
+	batch := filepath.Join(tempDir, fmt.Sprintf("nyagos-%d.cmd", pid))
+	env := filepath.Join(tempDir, fmt.Sprintf("nyagos-%d.tmp", pid))
+	pwd := filepath.Join(tempDir, fmt.Sprintf("nyagos_%d.tmp", pid))
+
+	errorlevel, err := call_batch(batch, args, env, pwd, verbose)
+
+	if !debug {
+		defer os.Remove(env)
+		defer os.Remove(pwd)
+		defer os.Remove(batch)
+	}
+
 	if err != nil {
+		return errorlevel, err
+	}
+
+	if err := load_envfile(env, verbose); err != nil {
 		return 1, err
 	}
-	defer fp.Close()
 
-	br := bufio.NewReader(fp)
-	for {
-		lineB, readErr := br.ReadBytes(byte('\n'))
-		if readErr != nil {
-			if readErr != io.EOF {
-				fmt.Fprintf(cmd.Stderr, "%s: %s (environment-readline error)\n", envTxtPath, readErr.Error())
-			}
-			break
-		}
-		line, atouErr := mbcs.AtoU(lineB)
-		if atouErr != nil {
-			fmt.Fprintf(cmd.Stderr, "%s: %s(environment-ansi-to-unicode error)\n", envTxtPath, atouErr.Error())
-			continue
-		}
-		line = strings.TrimSpace(line)
-		eqlPos := strings.Index(line, "=")
-		if eqlPos > 0 {
-			left := line[:eqlPos]
-			right := line[eqlPos+1:]
-			if left != "ERRORLEVEL_" {
-				if verbose {
-					fmt.Fprintf(cmd.Stdout, "%s=%s\n", left, right)
-				}
-				os.Setenv(left, right)
-			}
-		}
+	if err := load_pwdfile(pwd, verbose); err != nil {
+		return 1, err
 	}
 
-	fp2, err2 := os.Open(pwdTxtPath)
-	if err2 != nil {
-		return 1, err2
-	}
-	defer fp2.Close()
-	br2 := bufio.NewReader(fp2)
-	lineB, lineErr := br2.ReadBytes(byte('\n'))
-	if lineErr != nil {
-		return 1, errors.New("source : could not get current-directory")
-	}
-	line, err := mbcs.AtoU(lineB)
-	if err == nil {
-		line = strings.TrimSpace(line)
-		if verbose {
-			fmt.Fprintf(cmd.Stdout, "cd \"%s\"\n", line)
-		}
-		os.Chdir(line)
-	}
 	return errorlevel, nil
 }
