8befd0a002abba0d83ca1ed7a0dffdb71c86fb46
Kaoru HAYAMA <iyahaya@nifty.com>
2016-11-19 07:16:05 +0900

    open.lua: prompt if more than one parameters are given.

--- nyagos.d/open.lua
+++ nyagos.d/open.lua
@@ -5,10 +5,25 @@ nyagos.alias.open = function(args)
         if list and #list >= 1 then
             for j=1,#list do
                 if nyagos.access(list[j],0) then
+                    if #list >= 2 then
+                        nyagos.write("open: "..list[j].." ? [y/n/q] ")
+                        local ch = nyagos.getkey()
+                        ch = string.lower(utf8.char(ch))
+                        if string.match(ch,"^%g+$") then
+                            nyagos.write(ch)
+                        end
+                        nyagos.write("\n")
+                        if ch == 'q' then
+                            break
+                        elseif ch ~= 'y' then
+                            goto continue
+                        end
+                    end
                     assert(nyagos.shellexecute("open",list[j]))
                 else
                     print(args[i] .. ": can not get status")
                 end
+                ::continue::
             end
         else
             if nyagos.access(args[i],0) then
