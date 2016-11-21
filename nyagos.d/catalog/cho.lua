3de303e933a0090effba9950e12a860f51ad8a11
Kaoru HAYAMA <iyahaya@nifty.com>
2016-11-22 00:12:40 +0900

    add nyagos.d/catalog/cho.lua (close #171)

---
+++ nyagos.d/catalog/cho.lua
@@ -0,0 +1,53 @@
+nyagos.alias.dump_temp_out = function()
+    for _,val in ipairs(share.dump_temp_out) do
+        nyagos.write(val,"\n")
+    end
+end
+
+nyagos.bindkey("C-o",function(this)
+    local word,pos = this:lastword()
+    word = string.gsub(word,'"','')
+    share.dump_temp_out = nyagos.glob(word.."*")
+    nyagos.write("\n")
+    local result=nyagos.eval('dump_temp_out | cho')
+    this:call("REPAINT_ON_NEWLINE")
+    if string.find(result," ",1,true) then
+        result = '"'..result..'"'
+    end
+    assert( this:replacefrom(pos,result) )
+end)
+
+nyagos.alias.__dump_history = function()
+    local uniq={}
+    for i=nyagos.gethistory()-1,1,-1 do
+        local line = nyagos.gethistory(i)
+        if line ~= "" and not uniq[line] then
+            nyagos.write(line,"\n")
+            uniq[line] = true
+        end
+    end
+end
+
+nyagos.bindkey("C_R", function(this)
+    nyagos.write("\n")
+    local result = nyagos.eval('__dump_history | cho')
+    this:call("REPAINT_ON_NEWLINE")
+    return result
+end)
+
+nyagos.bindkey("M_H" , function(this)
+    nyagos.write("\n")
+    local result = nyagos.eval('cd --history | cho')
+    this:call("REPAINT_ON_NEWLINE")
+    if string.find(result,' ') then
+        result = '"'..result..'"'
+    end
+    return result
+end)
+
+nyagos.bindkey("M_G" , function(this)
+    nyagos.write("\n")
+    local result = nyagos.eval('git log --pretty="format:%h %s" | cho')
+    this:call("REPAINT_ON_NEWLINE")
+    return string.match(result,"^%S+") or ""
+end)
