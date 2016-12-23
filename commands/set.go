e7bfb456fc71d30f1c8d05ae29afa284a3e8a82b
Kaoru HAYAMA <iyahaya@nifty.com>
2016-12-23 19:08:57 +0900

    Support: set VAR+=VALUE, VAR^=VALUE

--- commands/set.go
+++ commands/set.go
@@ -1,12 +1,35 @@
 package commands
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
+
+func shrink(values ...string) string {
+	hash := make(map[string]bool)
+
+	var buffer bytes.Buffer
+	for _, value := range values {
+		for _, val1 := range strings.Split(value, ";") {
+			val1 = strings.TrimSpace(val1)
+			if len(val1) > 0 {
+				VAL1 := strings.ToUpper(val1)
+				if _, ok := hash[VAL1]; !ok {
+					hash[VAL1] = true
+					if buffer.Len() > 0 {
+						buffer.WriteString(";")
+					}
+					buffer.WriteString(val1)
+				}
+			}
+		}
+	}
+	return buffer.String()
+}
 
 func cmd_set(ctx context.Context, cmd *exec.Cmd) (int, error) {
 	if len(cmd.Args) <= 1 {
@@ -18,13 +41,24 @@ func cmd_set(ctx context.Context, cmd *exec.Cmd) (int, error) {
 	for _, arg := range cmd.Args[1:] {
 		eqlPos := strings.Index(arg, "=")
 		if eqlPos < 0 {
+			// set NAME
 			fmt.Fprintf(cmd.Stdout, "%s=%s\n", arg, os.Getenv(arg))
+		} else if eqlPos >= 3 && arg[eqlPos-1] == '+' {
+			// set NAME+=VALUE
+			right := arg[eqlPos+1:]
+			left := arg[:eqlPos-1]
+			os.Setenv(left, shrink(os.Getenv(left), right))
+		} else if eqlPos >= 3 && arg[eqlPos-1] == '^' {
+			// set NAME^=VALUE
+			right := arg[eqlPos+1:]
+			left := arg[:eqlPos-1]
+			os.Setenv(left, shrink(right, os.Getenv(left)))
+		} else if eqlPos+1 < len(arg) {
+			// set NAME=VALUE
+			os.Setenv(arg[:eqlPos], arg[eqlPos+1:])
 		} else {
-			if eqlPos+1 < len(arg) {
-				os.Setenv(arg[:eqlPos], arg[eqlPos+1:])
-			} else {
-				os.Unsetenv(arg[:eqlPos])
-			}
+			// set NAME=
+			os.Unsetenv(arg[:eqlPos])
 		}
 	}
 	return 0, nil
