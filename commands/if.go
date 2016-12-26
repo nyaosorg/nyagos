e89ef75cf4c8bf91d7d7ac4eaa2df5f04749e263
Kaoru HAYAMA <iyahaya@nifty.com>
2016-12-27 00:09:40 +0900

    if: add /I option

--- commands/if.go
+++ commands/if.go
@@ -17,6 +17,14 @@ func cmd_if(ctx context.Context, cmd *exec.Cmd) (int, error) {
 	not := false
 	start := 1
 
+	option := map[string]bool{}
+
+	for len(args) >= 2 && strings.HasPrefix(args[1], "/") {
+		option[strings.ToLower(args[1])] = true
+		args = args[1:]
+		start++
+	}
+
 	if len(args) >= 2 && strings.EqualFold(args[1], "not") {
 		not = true
 		args = args[1:]
@@ -24,7 +32,11 @@ func cmd_if(ctx context.Context, cmd *exec.Cmd) (int, error) {
 	}
 	status := false
 	if len(args) >= 4 && args[2] == "==" {
-		status = (args[1] == args[3])
+		if _, ok := option["/i"]; ok {
+			status = strings.EqualFold(args[1], args[3])
+		} else {
+			status = (args[1] == args[3])
+		}
 		args = args[4:]
 		start += 3
 	} else if len(args) >= 3 && strings.EqualFold(args[1], "exist") {
