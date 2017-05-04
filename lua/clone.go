bf03e270de5ef7dfeb5ab9bae199018ca58daef2
Kaoru HAYAMA <iyahaya@nifty.com>
2017-05-04 22:55:36 +0900

    Internal. Add lua.Lua.Clone() and its test code

---
+++ lua/clone.go
@@ -0,0 +1,43 @@
+package lua
+
+func (L Lua) CloneTo(newL Lua) error {
+	L.GetGlobal("_G")
+	defer L.Pop(1)
+	err := L.ForInDo(-1, func(src Lua) error {
+		if !src.IsString(-2) {
+			return nil
+		}
+		key, err := src.ToString(-2)
+		if err != nil {
+			return err
+		}
+		println("KEY=", key)
+
+		// If new instance has already the member, pass it.
+		newL.GetGlobal(key)
+		defer newL.Pop(1)
+		if !newL.IsNil(-1) {
+			return nil
+		}
+		println("not found and copy")
+		val, err := src.ToPushable(-1)
+		if err != nil {
+			return err
+		}
+		println("push to new instance")
+		val.Push(newL)
+		newL.SetGlobal(key)
+		return nil
+	})
+	return err
+}
+
+func (L Lua) Clone() (Lua, error) {
+	newL, err := New()
+	if err != nil {
+		return Lua(0), err
+	}
+	newL.OpenLibs()
+	err = L.CloneTo(newL)
+	return newL, err
+}
---
+++ lua/t/clone.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	".."
+)
+
+func main() {
+	L, err := lua.New()
+	if err != nil {
+		println(err)
+		return
+	}
+	defer L.Close()
+	L.OpenLibs()
+
+	L.PushString("ahaha")
+	L.SetGlobal("hogehoge")
+
+	L.NewTable()
+	L.PushInteger(1)
+	L.SetField(-2, "foo")
+	L.SetGlobal("sample_table")
+
+	println("Setup first instance: done")
+
+	N, err := L.Clone()
+	if err != nil {
+		println(err)
+		return
+	}
+	println("Cloning done")
+	defer N.Close()
+	N.GetGlobal("hogehoge")
+	val, err := N.ToString(-1)
+	if err != nil {
+		println(err)
+		return
+	}
+	L.Pop(1)
+	if val == "ahaha" {
+		println("test-1: ok")
+	} else {
+		println("test-1: ng: val=", val)
+	}
+
+	N.GetGlobal("sample_table")
+	N.GetField(-1, "foo")
+	val2, err := N.ToInteger(-1)
+	L.Pop(2)
+	if val2 == 1 {
+		println("test-2: ok")
+	} else {
+		println("test-2: ng")
+	}
+}
