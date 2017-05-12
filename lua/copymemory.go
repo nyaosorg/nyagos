634843d3a27afa9d0fb2f73691b80268c1bf0e7c
Kaoru HAYAMA <iyahaya@nifty.com>
2017-05-13 01:33:49 +0900

    Fix #216: failed to load kernel32.RtlCopyMemory on 386-build

--- lua/copymemory.go
+++ lua/copymemory.go
@@ -2,15 +2,13 @@ package lua
 
 import "syscall"
 
-var kernel32 = syscall.NewLazyDLL("kernel32")
-var procCopyMemory = kernel32.NewProc("RtlCopyMemory")
+var msvcrt = syscall.NewLazyDLL("msvcrt")
+var procMemcpy = msvcrt.NewProc("memcpy")
+var procStrlen = msvcrt.NewProc("strlen")
 
 func copyMemory(dst uintptr, src uintptr, length uintptr) {
-	procCopyMemory.Call(dst, src, length)
+	procMemcpy.Call(dst, src, length)
 }
-
-var msvcrt = syscall.NewLazyDLL("msvcrt")
-var procStrlen = msvcrt.NewProc("strlen")
 
 func strLen(p uintptr) uintptr {
 	rc, _, _ := procStrlen.Call(p)
