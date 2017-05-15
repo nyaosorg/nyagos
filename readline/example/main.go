e5c688b601fd0422afc33fadbf2b45a8de1f27be
HAYAMA_Kaoru <iyahaya@nifty.com>
2017-05-15 19:26:46 +0900

    Internal. readline/example: Add code to exit with Ctrl-P/N

--- readline/example/main.go
+++ readline/example/main.go
@@ -14,7 +14,22 @@ func main() {
 		Default: "AHAHA",
 		Cursor:  3,
 	}
+
+	enter_status := 0
+	readline.BindKeyClosure(readline.K_CTRL_P,
+		 func(r *readline.Buffer) readline.Result {
+			enter_status = -1
+			return readline.ENTER
+		})
+
+	readline.BindKeyClosure(readline.K_CTRL_N,
+		func(r *readline.Buffer) readline.Result {
+			enter_status = +1
+			return readline.ENTER
+		})
+
 	text, err := readline1.ReadLine(context.Background())
+	fmt.Printf("ENTER_STATUS=%d\n", enter_status)
 	if err != nil {
 		fmt.Printf("ERR=%s\n", err.Error())
 	} else {
