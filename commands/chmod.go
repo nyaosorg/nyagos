dd97e945ac2f19a9166e0824254ffed6d4b0cf33
Kaoru HAYAMA <iyahaya@nifty.com>
2017-06-01 07:15:49 +0900

    Impl chmod [augo][+-=][rwx]

--- commands/chmod.go
+++ commands/chmod.go
@@ -5,28 +5,95 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/zetamatta/nyagos/shell"
 )
 
+var rxOOO = regexp.MustCompile("^[0-7][0-7][0-7]$")
+
+func getOct(s string) (int, error) {
+	val := 0
+	for _, r := range s {
+		n := strings.IndexRune("01234567", r)
+		if n < 0 {
+			return 0, fmt.Errorf("%s: invalid permission str", s)
+		}
+		val = val*8 + n
+	}
+	return val, nil
+}
+
+var rxEqu = regexp.MustCompile(`^([aogu]+)([\-\+\=])([rwx]+)$`)
+
 func cmd_chmod_(args []string) error {
 	if len(args) < 2 {
 		return errors.New("Usage: chmod ooo (files...)")
 	}
-	permission := 0
-	if len(args[0]) != 3 {
-		return fmt.Errorf("%s: invalid permission str", args[0])
-	}
-	for _, r := range args[0] {
-		n := strings.IndexRune("01234567", r)
-		if n < 0 {
+	var f func(string) error
+	if rxOOO.MatchString(args[0]) {
+		val, err := getOct(args[0])
+		if err != nil {
 			return fmt.Errorf("%s: invalid permission str", args[0])
 		}
-		permission = permission*8 + n
+		f = func(fname string) error {
+			return os.Chmod(fname, os.FileMode(val))
+		}
+	} else if m := rxEqu.FindStringSubmatch(args[0]); m != nil {
+		var basebits os.FileMode
+		for _, r := range m[3] {
+			switch r {
+			case 'r':
+				basebits |= 4
+			case 'w':
+				basebits |= 2
+			case 'x':
+				basebits |= 1
+			}
+		}
+		var bits os.FileMode
+		for _, r := range m[1] {
+			switch r {
+			case 'u':
+				bits |= (basebits << 6)
+			case 'g':
+				bits |= (basebits << 3)
+			case 'o':
+				bits |= basebits
+			case 'a':
+				bits = (basebits << 6) | (basebits << 3) | basebits
+			}
+		}
+		switch m[2] {
+		case "+":
+			f = func(fname string) error {
+				stat, err := os.Stat(fname)
+				if err != nil {
+					return err
+				}
+				mod := stat.Mode()
+				return os.Chmod(fname, mod|bits)
+			}
+		case "-":
+			f = func(fname string) error {
+				stat, err := os.Stat(fname)
+				if err != nil {
+					return err
+				}
+				mod := stat.Mode()
+				return os.Chmod(fname, mod&^bits)
+			}
+		case "=":
+			f = func(fname string) error {
+				return os.Chmod(fname, bits)
+			}
+		}
+	} else {
+		return fmt.Errorf("%s: invalid permission str", args[0])
 	}
 	for _, fname := range args[1:] {
-		if err := os.Chmod(fname, os.FileMode(permission)); err != nil {
+		if err := f(fname); err != nil {
 			return fmt.Errorf("%s: %s", fname, err.Error())
 		}
 	}
