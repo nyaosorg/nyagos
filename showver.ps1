17d94b61f2668f1b95357b73b6904b01595eed01
HAYAMA_Kaoru <iyahaya@nifty.com>
2017-07-03 16:20:32 +0900

    showver.ps1 : add error check

--- showver.ps1
+++ showver.ps1
@@ -1,15 +1,21 @@
-$v = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($Args[0]) 
-if( $v ){
-    Write-Output(("FileVersion:    {0} ({1},{2},{3},{4})" -f 
-        $v.FileVersion,
-        $v.FileMajorPart,
-        $v.FileMinorPart,
-        $v.FileBuildPart,
-        $v.FilePrivatePart))
-    Write-Output(("ProductVersion: {0} ({1},{2},{3},{4})" -f 
-        $v.ProductVersion,
-        $v.ProductMajorPart,
-        $v.ProductMinorPart,
-        $v.ProductBuildPart,
-        $v.ProductPrivatePart))
+foreach($fname in $args ){
+    if( Test-Path $fname ){
+        $v = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($fname)
+        if( $v ){
+            Write-Host(("FileVersion:    {0} ({1},{2},{3},{4})" -f
+                $v.FileVersion,
+                $v.FileMajorPart,
+                $v.FileMinorPart,
+                $v.FileBuildPart,
+                $v.FilePrivatePart))
+            Write-Host(("ProductVersion: {0} ({1},{2},{3},{4})" -f
+                $v.ProductVersion,
+                $v.ProductMajorPart,
+                $v.ProductMinorPart,
+                $v.ProductBuildPart,
+                $v.ProductPrivatePart))
+        }
+    }else{
+        Write-Host(("{0}: not found" -f $fname))
+    }
 }
