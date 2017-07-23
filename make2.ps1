4b1c4af8df39ce7c2e8d5da0422e7b368c48d8e9
Kaoru HAYAMA <iyahaya@nifty.com>
2017-07-23 15:08:18 +0900

    make2.ps2 bundles showver.ps1

--- make2.ps1
+++ make2.ps1
@@ -127,10 +127,35 @@ function Go-VersionInfo($version) {
     goversioninfo.exe -icon mains\nyagos.ico -o nyagos.syso Misc\version.json
 }
 
-function Make-Bindata {
-    Get-ChildItem .\nyagos.d -Recurse |
-    
+function Show-Version($fname) {
+    if( Test-Path $fname ){
+        Write-Host $fname
+        $v = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($fname)
+        if( $v ){
+            Write-Host(("  FileVersion:    `"{0}`" ({1},{2},{3},{4})" -f
+                $v.FileVersion,
+                $v.FileMajorPart,
+                $v.FileMinorPart,
+                $v.FileBuildPart,
+                $v.FilePrivatePart))
+            Write-Host(("  ProductVersion: `"{0}`" ({1},{2},{3},{4})" -f
+                $v.ProductVersion,
+                $v.ProductMajorPart,
+                $v.ProductMinorPart,
+                $v.ProductBuildPart,
+                $v.ProductPrivatePart))
+        }
+        $data = [System.IO.File]::ReadAllBytes($fname)
+        $md5 = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
+        $bs = $md5.ComputeHash($data)
+        Write-Host( ("  md5sum: {0}" -f [System.BitConverter]::ToString($bs).ToLower().Replace("-","")))
+    }else{
+        Write-Host(("{0}: not found" -f $fname))
+    }
+}
 
+function Make-Bindata {
+    # Get-ChildItem .\nyagos.d -Recurse |
 }
 
 function Do-Build {
@@ -139,6 +164,9 @@ function Do-Build {
 
 
 switch( $args[0] ){
+    "status" {
+        Show-Version(".\nyagos.exe")
+    }
     "" {
         Write-Host("goarch=" + (Get-GoArch))
         Write-Host("version=" + (Get-Version))
