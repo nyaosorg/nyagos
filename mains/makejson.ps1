bdd4d3a7d46aef450f3c2fbee49dd656d82184c4
Kaoru HAYAMA <iyahaya@nifty.com>
2017-07-23 14:49:59 +0900

    Refactor the script to make JSON for version

--- mains/makejson.ps1
+++ mains/makejson.ps1
@@ -1,63 +1,50 @@
 Set-PSDebug -strict
 
-$text = @"
-{
-	"FixedFileInfo":
-	{
-		"FileVersion": {
-			"Major": %VER1%,
-			"Minor": %VER2%,
-			"Patch": %VER3%,
-			"Build": %VER4%
-		},
-		"ProductVersion": {
-			"Major": %VER1%,
-			"Minor": %VER2%,
-			"Patch": %VER3%,
-			"Build": %VER4%
-		},
-		"FileFlagsMask": "3f",
-		"FileFlags ": "00",
-		"FileOS": "040004",
-		"FileType": "01",
-		"FileSubType": "00"
-	},
-	"StringFileInfo":
-	{
-		"Comments": "",
-		"CompanyName": "NYAOS.ORG",
-		"FileDescription": "Extended Commandline Shell",
-		"FileVersion": "%VERSION%",
-		"InternalName": "",
-		"LegalCopyright": "Copyright (C) 2014-2017 HAYAMA_Kaoru",
-		"LegalTrademarks": "",
-		"OriginalFilename": "NYAGOS.EXE",
-		"PrivateBuild": "",
-		"ProductName": "Nihongo Yet Another GOing Shell",
-		"ProductVersion": "%VERSION%",
-		"SpecialBuild": ""
-	},
-	"VarFileInfo":
-	{
-		"Translation": {
-			"LangID": "0411",
-			"CharsetID": "04E4"
-		}
-	}
-}
-"@
-
-$version = $env:version 
-if( $version -like "*.*.*_*" ){
+function Make-JSON($version){
+    $version = [string]$version
+    $json = @{
+        FixedFileInfo = @{
+            FileFlagsMask="3f";
+            FileFlags="00";
+            FileOS="040004";
+            FileType="01";
+            FileSubType="00";
+        };
+        StringFileInfo = @{
+            Comments="";
+            CompanyName="NYAOS.ORG";
+            FileDescription="Extended Commandline Shell";
+            InternalName="";
+            LegalCopyright="Copyright (C) 2014-2017 HAYAMA_Kaoru";
+            LegalTrademarks="";
+            OriginalFilename="NYAGOS.EXE";
+            PrivateBuild="";
+            ProductName="Nihongo Yet Another GOing Shell";
+            SpecialBuild="";
+        };
+        VarFileInfo = @{
+            Translation=@{
+                LangID="0411";
+                CharsetID="04E4";
+            }
+        }
+    }
     $v = $version -split "[\._]"
+    if( $v.Length -ne 4 ){
+        $v = @(0,0,0,0)
+    }
+    $private:versionSet = @{
+        Major = [int]$v[0];
+        Minor = [int]$v[1];
+        Patch = [int]$v[2];
+        Build = [int]$v[3];
+    }
+    $json["FixedFileInfo"]["FileVersion"] = $versionSet
+    $json["FixedFileInfo"]["ProductVersion"] = $versionSet
+    $json["StringFileInfo"]["FileVersion"] = $version
+    $json["StringFileInfo"]["ProductVersion"] = $version
 
-    $text = $text -replace "%VER1%",$v[0]
-    $text = $text -replace "%VER2%",$v[1]
-    $text = $text -replace "%VER3%",$v[2]
-    $text = $text -replace "%VER4%",$v[3]
-    $text = $text -replace "%VERSION%",$version
-}else{
-    $text = $text -replace "%VER[1-4]%","0"
-    $text = $text -replace "%VERSION%","0.0.0_0"
+    $json | ConvertTo-Json
 }
-Write-Host($text)
+
+Make-JSON( [string]$env:version )
