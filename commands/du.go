81191a66e3f5bd495e1979aed10568956c686733
Kaoru HAYAMA <iyahaya@nifty.com>
2017-07-29 18:59:57 +0900

    du: count disk size by 4096 bytes

--- commands/du.go
+++ commands/du.go
@@ -9,7 +9,7 @@ import (
 	"github.com/zetamatta/nyagos/shell"
 )
 
-func du_(path string, output func(string, int64)) (int64, error) {
+func du_(path string, output func(string, int64), blocksize int64) (int64, error) {
 	fd, err := os.Open(path)
 	if err != nil {
 		return 0, err
@@ -24,15 +24,16 @@ func du_(path string, output func(string, int64)) (int64, error) {
 	for _, fileInfo1 := range fileInfos {
 		if fileInfo1.IsDir() {
 			dirs = append(dirs, fileInfo1.Name())
+		} else {
+			diskuse += ((fileInfo1.Size() + blocksize - 1) / blocksize) * blocksize
 		}
-		diskuse += fileInfo1.Size()
 	}
 	if err := fd.Close(); err != nil {
 		return diskuse, err
 	}
 	for _, dir1 := range dirs {
 		fullpath := filepath.Join(path, dir1)
-		diskuse1, err := du_(fullpath, output)
+		diskuse1, err := du_(fullpath, output, blocksize)
 		if err != nil {
 			return diskuse, err
 		}
@@ -44,16 +45,16 @@ func du_(path string, output func(string, int64)) (int64, error) {
 
 func cmd_du(_ context.Context, cmd *shell.Cmd) (int, error) {
 	output := func(name string, size int64) {
-		fmt.Fprintf(cmd.Stdout, "%d\t%s\n", size/1000, name)
+		fmt.Fprintf(cmd.Stdout, "%d\t%s\n", size/1024, name)
 	}
 	if len(cmd.Args) <= 2 {
-		_, err := du_(".", output)
+		_, err := du_(".", output, 4096)
 		if err != nil {
 			return 1, err
 		}
 	}
 	for _, arg1 := range cmd.Args[1:] {
-		_, err := du_(arg1, output)
+		_, err := du_(arg1, output, 4096)
 		if err != nil {
 			return 1, err
 		}
