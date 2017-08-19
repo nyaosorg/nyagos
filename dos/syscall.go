18ec43ca24e705ee095f09a5f62cc33224aef6ba
Kaoru HAYAMA <iyahaya@nifty.com>
2017-08-19 13:15:10 +0900

    go generate dos.GetLogicalDrives()

--- dos/syscall.go
+++ dos/syscall.go
@@ -5,3 +5,4 @@ package dos
 //sys copyFile(src string,dst string,isFailIfExist bool)(n uint32,err error) = kernel32.CopyFileW
 //sys moveFileEx(src string,dst string,flag uintptr)(n uint32,err error) = kernel32.MoveFileExW
 //sys getDiskFreeSpaceEx(rootPathName string,free *uint64,total *uint64,totalFree *uint64)(n uint32,err error) = kernel32.GetDiskFreeSpaceExW
+//sys GetLogicalDrives()(n uint32,err error) = kernel32.GetLogicalDrives
