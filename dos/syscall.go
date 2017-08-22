7364b3a3fbbb0f29a2c670688e091e24ff67ce3d
Kaoru HAYAMA <iyahaya@nifty.com>
2017-08-22 21:39:59 +0900

    Rewrite go:generate line with macro $GOFILE

--- dos/syscall.go
+++ dos/syscall.go
@@ -1,6 +1,6 @@
 package dos
 
-//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall.go syscall.go
+//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output z$GOFILE $GOFILE
 
 //sys copyFile(src string,dst string,isFailIfExist bool)(n uint32,err error) = kernel32.CopyFileW
 //sys moveFileEx(src string,dst string,flag uintptr)(n uint32,err error) = kernel32.MoveFileExW
