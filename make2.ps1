a01b0acfdae8bdcacdd8ddd673f6bff9a7cce98e
Kaoru HAYAMA <iyahaya@nifty.com>
2017-09-23 07:24:59 +0900

    make2.ps1: Support make2.ps1 release...

--- make2.ps1
+++ make2.ps1
@@ -2,22 +2,14 @@ Set-PSDebug -strict
 
 Function Get-GoArch{
     if( Test-Path "goarch.txt" ){
-        (Get-Content "goarch.txt")
+        Get-Content "goarch.txt"
     }else{
-        ( go version | %{ $_.Split()[-1].Split("/")[-1] })
+        go version | %{ $_.Split()[-1].Split("/")[-1] }
     }
 }
 
-Function Get-Version{
-    Get-Content Misc\version.txt
-}
-
-Function Get-Commit{
-    git describe --tags
-}
-
 Function EachGoDir{
-    Get-ChildItem . -Recurse | 
+    Get-ChildItem . -Recurse |
     ?{ $_.Extension -eq '.go' } |
     %{ [System.IO.Path]::GetDirectoryName($_.FullName)} |
     Sort-Object |
@@ -36,109 +28,76 @@ Function Get-Imports {
     Get-Unique
 }
 
+function Is-Modified($path){
+    ([System.IO.File]::GetAttributes($path) -band
+        [System.IO.FileAttributes]::Archive) -eq
+        [System.IO.FileAttributes]::Archive
+}
+
 function Get-Modified {
     Get-ChildItem . -Recurse -Name |
     ?{ $_ -like "*.go" } |
-    ?{ 
-        $private:attr = [System.IO.File]::GetAttributes($_)
-        ($attr -band [System.IO.FileAttributes]::Archive) -eq
-            [System.IO.FileAttributes]::Archive
-    }
+    ?{ Is-Modified($_) }
 }
 
 function Set-NoModified($path) {
-    $private:attr = [System.IO.File]::GetAttributes($path)
+    $attr = [System.IO.File]::GetAttributes($path)
     [System.IO.File]::SetAttributes($path,
         $attr -band -bnot [System.IO.FileAttributes]::Archive)
 }
 
 function Go-Fmt{
-    Get-Modified | %{ go fmt -n $_ ; Set-NoModified($_) }
+    Get-Modified | %{
+        Write-Host "go fmt $_"
+        go fmt -n $_
+        Set-NoModified($_)
+    }
 }
 
 function Get-Go1stPath {
-    if( $env:gopath -ne "" ){
+    if( $env:gopath -ne $null -and $env:gopath -ne "" ){
         $gopath = $env:gopath
     }else{
-        $gopath = Join-Path($env:userprofile,"go")
+        $gopath = (Join-Path $env:userprofile "go")
     }
     $gopath.Split(";")[0]
 }
 
-function Make-JSON($version){
-    $version = [string]$version
-    $json = @{
-        FixedFileInfo = @{
-            FileFlagsMask="3f";
-            FileFlags="00";
-            FileOS="040004";
-            FileType="01";
-            FileSubType="00";
-        };
-        StringFileInfo = @{
-            Comments= "";
-            CompanyName= "NYAOS.ORG";
-            FileDescription= "Extended Commandline Shell";
-            InternalName= "";
-            LegalCopyright= "Copyright (C) 2014-2017 HAYAMA_Kaoru";
-            LegalTrademarks= "";
-            OriginalFilename= "NYAGOS.EXE";
-            PrivateBuild= "";
-            ProductName= "Nihongo Yet Another GOing Shell";
-            SpecialBuild= "";
-        };
-        VarFileInfo = @{
-            Translation=@{
-                LangID= "0411";
-                CharsetID= "04E4";
-            }
-        }
-    }
-    if( $version -like "*.*.*_*" ){
-        $v = $version -split "[\._]"
-    }else{
-        $v = @(0,0,0,0)
-    }
-    $private:versionSet = @{
-        Major = $v[0];
-        Minor = $v[1];
-        Patch = $v[2];
-        Build = $v[3];
-    }
-    $json["FixedFileInfo"]["FileVersion"] = $versionSet
-    $json["FixedFileInfo"]["ProductVersion"] = $versionSet
-    $json["StringFileInfo"]["FileVersion"] = $version
-    $json["StringFileInfo"]["ProductVersion"] = $version
-
-    $json | ConvertTo-Json
-}
-
 function Go-VersionInfo($version) {
-    $cwd = Get-Location
-    if( -not (Test-Path("goversioninfo.exe")) ){
-        go get "github.com/josephspurrier/goversioninfo"
-        $gopath1 = Get-Go1stPath
-        pushd (Join-Path $gopath1 "src\github.com\josephspurrier\goversioninfo\cmd\goversioninfo")
-        go build
-        copy goversioninfo.exe $cwd
-        popd
+    Download-Exe "github.com/josephspurrier/goversioninfo" "goversioninfo.exe"
+    if( -not ($version -match "^\d+[\._]\d+[\._]\d+[\._]\d+$") ){
+        $version = "0.0.0_0"
     }
-    Make-JSON( $version ) | Set-Content -encoding UTF8 Misc\version.json
-    goversioninfo.exe -icon mains\nyagos.ico -o nyagos.syso Misc\version.json
+    $v = $version.Split("[\._]")
+
+    .\goversioninfo.exe `
+        "-file-version=$version" `
+        "-product-version=$version" `
+        "-icon=mains\nyagos.ico" `
+        ("-ver-major=" + $v[0]) `
+        ("-ver-minor=" + $v[1]) `
+        ("-ver-build=" + $v[2]) `
+        ("-ver-patch=" + $v[3]) `
+        ("-product-ver-major=" + $v[0]) `
+        ("-product-ver-minor=" + $v[1]) `
+        ("-product-ver-build=" + $v[2]) `
+        ("-product-ver-patch=" + $v[3]) `
+        "-o" nyagos.syso `
+        versioninfo.json
 }
 
 function Show-Version($fname) {
     if( Test-Path $fname ){
-        Write-Host $fname
+        Write-Output $fname
         $v = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($fname)
         if( $v ){
-            Write-Host(("  FileVersion:    `"{0}`" ({1},{2},{3},{4})" -f
+            Write-Output(("  FileVersion:    `"{0}`" ({1},{2},{3},{4})" -f
                 $v.FileVersion,
                 $v.FileMajorPart,
                 $v.FileMinorPart,
                 $v.FileBuildPart,
                 $v.FilePrivatePart))
-            Write-Host(("  ProductVersion: `"{0}`" ({1},{2},{3},{4})" -f
+            Write-Output(("  ProductVersion: `"{0}`" ({1},{2},{3},{4})" -f
                 $v.ProductVersion,
                 $v.ProductMajorPart,
                 $v.ProductMinorPart,
@@ -148,40 +107,78 @@ function Show-Version($fname) {
         $data = [System.IO.File]::ReadAllBytes($fname)
         $md5 = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
         $bs = $md5.ComputeHash($data)
-        Write-Host( ("  md5sum: {0}" -f [System.BitConverter]::ToString($bs).ToLower().Replace("-","")))
+        Write-Output ("  md5sum: {0}" -f [System.BitConverter]::ToString($bs).ToLower().Replace("-",""))
     }else{
-        Write-Host(("{0}: not found" -f $fname))
+        Write-Output ("{0}: not found" -f $fname)
     }
 }
 
-function Make-Bindata {
-    # Get-ChildItem .\nyagos.d -Recurse |
+function Download-Exe($url,$exename){
+    if( Test-Path $exename ){
+        return
+    }
+    go get $url
+    Set-Location (Join-Path (Get-Go1stPath) "src" $url)
+    go build
+    Copy-Item go-bindata.exe $cwd
+    Set-Location $cwd
 }
 
-function Do-Build {
+function Build($version,$tags) {
+    Go-Fmt
+    Go-VersionInfo $version
 
+    Get-ChildItem ".\nyagos.d" -Recurse |
+    ?{ Is-Modified($_.FullName) } |
+    %{
+        Set-NoModified($_.FullName)
+        if( Test-Path "mains\bindata.go" ){
+            Remove-Item "mains\bindata.go"
+        }
+    }
+
+    if( -not (Test-Path "mains\bindata.go") ){
+        Download-Exe "github.com/jteeuwen/go-bindata" "go-bindata.exe"
+        .\go-bindata.exe -pkg "mains" -o "mains\bindata.go" "nyagos.d/..."
+    }
+
+    $ldflags = (git log -1 --date=short --pretty=format:"-X main.stamp=%ad -X main.commit=%H")
+    go build "-o" nyagos.exe -ldflags "$ldflags -X main.version=$version" $tags
 }
-
 
 switch( $args[0] ){
-    "status" {
-        Show-Version(".\nyagos.exe")
-    }
     "" {
-        Write-Host("goarch=" + (Get-GoArch))
-        Write-Host("version=" + (Get-Version))
-        Write-Host("commit=" + (Get-Commit))
-        EachGoDir | %{ Write-Host $_ } 
-        Get-Imports | %{ Write-Host $_ }
+        Build (git describe --tags) ""
+    }
+    "debug" {
+        Build "" "-tags=debug"
+    }
+    "release" {
+        Build (Get-Content Misc\version.txt) ""
+    }
+    "status" {
+        Show-Version ".\nyagos.exe"
+    }
+    "vet" {
+    }
+    "clean" {
+    }
+    "sweep" {
+    }
+    "bindata" {
+    }
+    "goversioninfo" {
+    }
+    "const" {
+    }
+    "package" {
+    }
+    "install" {
     }
     "get" {
-        Get-Imports | %{ Write-Host $_ ; go get -u $_ }
+        Get-Imports | %{ Write-Output $_ ; go get -u $_ }
     }
     "fmt" {
         Go-Fmt
-    }
-    "t" {
-        Go-VersionInfo
-        #Make-JSON("1.2.3_4")
     }
 }
