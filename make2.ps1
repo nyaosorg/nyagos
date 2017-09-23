86ba981d92c35aba9205782caff7da6ccd01a887
Kaoru HAYAMA <iyahaya@nifty.com>
2017-09-23 11:11:59 +0900

    make2.ps1: Support clean , go-get ...

--- make2.ps1
+++ make2.ps1
@@ -1,4 +1,5 @@
 Set-PSDebug -strict
+$VerbosePreference = "Continue"
 
 Function Get-GoArch{
     if( Test-Path "goarch.txt" ){
@@ -48,9 +49,22 @@ function Set-NoModified($path) {
 
 function Go-Fmt{
     Get-Modified | %{
-        Write-Host "go fmt $_"
+        Write-Verbose -Message "$ go fmt $_"
         go fmt -n $_
         Set-NoModified($_)
+    }
+    Get-ChildItem . -Recurse | ?{ $_.Name -eq "syscall.go" } | %{
+        $dir = (Split-Path $_.FullName -Parent)
+        $dst = (Join-Path $dir "zsyscall.go")
+        if( -not (Test-Path $dst) ){
+            Write-Verbose -Message ( `
+                "Found {0} but not found $dst" `
+                -f $_.Fullname,$dst )
+            pushd $dir
+            Write-Verbose -Message ("$ go generate on " + $dir)
+            go generate
+            popd
+        }
     }
 }
 
@@ -64,7 +78,7 @@ function Get-Go1stPath {
 }
 
 function Go-VersionInfo($version) {
-    Download-Exe "github.com/josephspurrier/goversioninfo" "goversioninfo.exe"
+    Download-Exe "github.com/josephspurrier/goversioninfo" "goversioninfo.exe" "cmd\goversioninfo"
     if( -not ($version -match "^\d+[\._]\d+[\._]\d+[\._]\d+$") ){
         $version = "0.0.0_0"
     }
@@ -109,40 +123,56 @@ function Show-Version($fname) {
         $bs = $md5.ComputeHash($data)
         Write-Output ("  md5sum: {0}" -f [System.BitConverter]::ToString($bs).ToLower().Replace("-",""))
     }else{
-        Write-Output ("{0}: not found" -f $fname)
+        Write-Error ("{0} not found" -f $fname)
     }
 }
 
-function Download-Exe($url,$exename){
+function Download-Exe($url,$exename,$builddir){
     if( Test-Path $exename ){
+        Write-Verbose -Message ("Found {0}" -f $exename)
         return
     }
+    Write-Verbose -Message ("{0} not found." -f $exename)
+    Write-Verbose -Message ("$ go get " + $url)
     go get $url
-    Set-Location (Join-Path (Get-Go1stPath) "src" $url)
+    $workdir = (Join-Path (Join-Path (Get-Go1stPath) "src") $url)
+    if( $builddir -ne $null -and $builddir -ne "" ){
+        $workdir = (Join-Path $workdir $builddir)
+    }
+    $cwd = (Get-Location)
+    Set-Location $workdir
+    Write-Verbose -Message ("$ go build {0} on {1}" -f $exename,$workdir)
     go build
-    Copy-Item go-bindata.exe $cwd
+    Write-Verbose -message ("$ copy {0} {1}" -f $exename,$cwd)
+    Copy-Item $exename $cwd
     Set-Location $cwd
 }
 
 function Build($version,$tags) {
+    Write-Verbose -Message ("Build as version='{0}' tags='{1}'" -f $version,$tags)
     Go-Fmt
+
     Go-VersionInfo $version
 
     Get-ChildItem ".\nyagos.d" -Recurse |
     ?{ Is-Modified($_.FullName) } |
     %{
+        Write-Verbose -Message ("found {0} modified" -f $_.FullName)
         Set-NoModified($_.FullName)
         if( Test-Path "mains\bindata.go" ){
+            Write-Verbose -Message "$ del mains\bindata.go"
             Remove-Item "mains\bindata.go"
         }
     }
 
     if( -not (Test-Path "mains\bindata.go") ){
-        Download-Exe "github.com/jteeuwen/go-bindata" "go-bindata.exe"
+        Download-Exe "github.com/jteeuwen/go-bindata" "go-bindata.exe" "go-bindata"
+        Write-Verbose -Message "$ go-bindata.exe"
         .\go-bindata.exe -pkg "mains" -o "mains\bindata.go" "nyagos.d/..."
     }
 
     $ldflags = (git log -1 --date=short --pretty=format:"-X main.stamp=%ad -X main.commit=%H")
+    Write-Verbose -Message "$ go build"
     go build "-o" nyagos.exe -ldflags "$ldflags -X main.version=$version" $tags
 }
 
@@ -162,6 +192,33 @@ switch( $args[0] ){
     "vet" {
     }
     "clean" {
+        foreach( $p in @(`
+            "nyagos.exe",`
+            "nyagos.syso",`
+            "version.now",`
+            "mains\bindata.go",`
+            "goversioninfo.exe",`
+            "go-bindata.exe" ) )
+        {
+            if( Test-Path $p ){
+                Write-Verbose -Message ("Remove " + $p)
+                Remove-Item $p
+            }
+        }
+        Write-Verbose "Search zsyscall.go"
+        Get-ChildItem "." -Recurse |
+        ?{ $_.Name -eq "zsyscall.go" } |
+        %{
+            Write-Verbose -Message ("Remove " + $_.FullName)
+            Remove-Item $_.FullName
+        }
+
+        EachGoDir | %{
+            Write-Verbose -Message ("$ go clean on " + $_)
+            pushd $_
+            go clean
+            popd
+        }
     }
     "sweep" {
     }
