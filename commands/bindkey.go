d18e0d9fc02234c3a97f57149719f0d34283fe65
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-10-09 14:21:15 +0900

    Fix crash in cmd_bindkey.

--- commands/bindkey.go
+++ commands/bindkey.go
@@ -12,6 +12,7 @@ func cmd_bindkey(ctx context.Context, cmd *shell.Cmd) (int, error) {
 	if len(cmd.Args) < 3 {
 		fmt.Fprintf(cmd.Stderr, "%[1]s: Usage %[1]s KEYNAME FUNCNAME\n",
 			cmd.Args[0])
+		return 0, nil
 	}
 	err := readline.BindKeySymbol(cmd.Args[1], cmd.Args[2])
 	if err != nil {
