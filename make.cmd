7b40201b7ed956b7642381fa950caa72de967d3c
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-04-02 11:13:11 +0900

    make.cmd supports `make nolua 386` to make a 32bit-executable without Lua

--- make.cmd
+++ make.cmd
@@ -397,7 +397,12 @@ switch( $args[0] ){
         Build (git describe --tags) ""
     }
     "nolua" {
+        $private:save = $env:GOARCH
+        if( $args[1] ){
+            $env:GOARCH = $args[1]
+        }
         Build (git describe --tags) "-tags=nolua"
+        $env:GOARCH = $save
     }
     "386"{
         $private:save = $env:GOARCH
