346451c58853f220b645aa5df266f6f5d08050f2
Kaoru HAYAMA <iyahaya@nifty.com>
2018-04-14 11:57:30 +0900

    go fmt ls_test.go

--- commands/ls/ls_test.go
+++ commands/ls/ls_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	err := Main(context.Background(),[]string{"."}, os.Stdout, os.Stderr)
+	err := Main(context.Background(), []string{"."}, os.Stdout, os.Stderr)
 	if err != nil {
 		t.Fatalf("ls .: %s", err.Error())
 	}
-	err = Main(context.Background(),[]string{"-l", "ls_test.go"}, os.Stdout, os.Stderr)
+	err = Main(context.Background(), []string{"-l", "ls_test.go"}, os.Stdout, os.Stderr)
 	if err != nil {
 		t.Fatalf("ls *: %s", err.Error())
 	}
