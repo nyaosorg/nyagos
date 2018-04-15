f7bd6aa88bafa7f3032cdbfa2876a837327031e5
Kaoru HAYAMA <iyahaya@nifty.com>
2018-04-16 00:36:19 +0900

    issue #298: Ctrl-C can terminate the `type` command.

--- commands/type.go
+++ commands/type.go
@@ -12,9 +12,17 @@ import (
 	"github.com/zetamatta/go-mbcs"
 )
 
-func cat(r io.Reader, w io.Writer) {
+func cat(ctx context.Context, r io.Reader, w io.Writer) bool {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
+		if done := ctx.Done(); done != nil {
+			select {
+			case <-done:
+				return false
+			default:
+
+			}
+		}
 		line := scanner.Bytes()
 		var text string
 		if utf8.Valid(line) {
@@ -29,11 +37,12 @@ func cat(r io.Reader, w io.Writer) {
 		text = strings.Replace(text, "\xEF\xBB\xBF", "", 1)
 		fmt.Fprintln(w, text)
 	}
+	return true
 }
 
 func cmdType(ctx context.Context, cmd Param) (int, error) {
 	if len(cmd.Args()) <= 1 {
-		cat(cmd.In(), cmd.Out())
+		cat(ctx, cmd.In(), cmd.Out())
 	} else {
 		for _, arg1 := range cmd.Args()[1:] {
 			r, err := os.Open(arg1)
@@ -49,8 +58,11 @@ func cmdType(ctx context.Context, cmd Param) (int, error) {
 				r.Close()
 				return 3, fmt.Errorf("%s: Permission denied", arg1)
 			}
-			cat(r, cmd.Out())
+			cont := cat(ctx, r, cmd.Out())
 			r.Close()
+			if !cont {
+				return 0, nil
+			}
 		}
 	}
 	return 0, nil
