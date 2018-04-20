db1dd7cac98409a67f0cedc8513c9f6c1fe48aff
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-04-20 11:13:19 +0900

    Add constatnts on lua-package compatible version with GopherLuaAdd

---
+++ lua/const2.go
@@ -0,0 +1,20 @@
+package lua
+
+// constant-name compatible with GopherLua
+
+const LTBool = LUA_TBOOLEAN
+const LTFunction = LUA_TFUNCTION
+const LTNil = LUA_TNIL
+const LTNumber = LUA_TNUMBER
+const LTString = LUA_TSTRING
+const LTTable = LUA_TTABLE
+const LTThread = LUA_TTHREAD
+const LTUserData = LUA_TUSERDATA
+
+// const LUA_REGISTRYINDEX = -1001000
+// const LUA_TLIGHTUSERDATA = 2
+// const LUA_FILEHANDLE = "FILE*"
+// const LUA_OK = 0
+// const LUA_ERRSYNTAX = 3
+// const LUA_ERRMEM = 4
+// const LUA_ERRGCMM = 5
