d42f5e7f6071e9cb52efbfb3cef51cb7671bfc48
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-04-25 12:07:42 +0900

    Split mains/prompt.go from mains/nyagos.go

--- mains/nyagos.go
+++ mains/nyagos.go
@@ -20,28 +20,6 @@ import (
 
 var noLuaEngineErr = errors.New("no lua engine")
 
-var prompt_hook lua.Object = lua.TGoFunction(lua2cmd(functions.Prompt))
-
-func printPrompt(ctx context.Context, sh *shell.Shell, L Lua) (int, error) {
-	L.Push(prompt_hook)
-
-	if !L.IsFunction(-1) {
-		L.Pop(1)
-		return 0, nil
-	}
-	L.PushString(os.Getenv("PROMPT"))
-	if err := callCSL(ctx, sh, L, 1, 1); err != nil {
-		return 0, err
-	}
-	length, lengthErr := L.ToInteger(-1)
-	L.Pop(1)
-	if lengthErr == nil {
-		return length, nil
-	} else {
-		return 0, fmt.Errorf("nyagos.prompt: return-value(length) is invalid: %s", lengthErr.Error())
-	}
-}
-
 type luaWrapper struct {
 	Lua
 }
