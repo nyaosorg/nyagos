ed4ab6029113a2cdf5d36f98714045e6c6626ba0
Kaoru HAYAMA <iyahaya@nifty.com>
2018-05-04 17:39:15 +0900

    Implemented test for alias function's return value

---
+++ t/tst_alias_result.lua
@@ -0,0 +1,37 @@
+--- test for alias function's return value ---
+--- Do `lua_f 20180504.lua`
+
+nyagos.alias.tst20180504 = function(args)
+    return { "echo","ahaha" }
+end
+
+local result = nyagos.eval("tst20180504")
+if result == "ahaha" then
+    print("OK",result)
+else
+    print("NG:",result)
+end
+
+nyagos.alias.tst20180504b = function(args)
+    return "echo ihihi"
+end
+
+result = nyagos.eval("tst20180504b")
+if result == "ihihi" then
+    print("OK:",result)
+else
+    print("NG:",result)
+end
+
+nyagos.alias.tst20180504c = function(args)
+    return 3
+end
+
+nyagos.exec("tst20180504c")
+result = nyagos.env.errorlevel 
+if result == "3" then
+    print("OK",result)
+else
+    print("NG",result)
+end
+
