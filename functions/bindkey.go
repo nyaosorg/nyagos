7e984351f1be008cf109773455f2679350d2f929
Kaoru HAYAMA <iyahaya@nifty.com>
2018-06-20 23:38:30 +0900

    Fix functions/bindkey.go for golint

--- functions/bindkey.go
+++ functions/bindkey.go
@@ -5,6 +5,7 @@ import (
 	"github.com/zetamatta/nyagos/readline"
 )
 
+// CmdGetBindKey is the getter for nyagos.key table.
 func CmdGetBindKey(args []any_t) []any_t {
 	if len(args) < 1 {
 		return []any_t{nil, "too few arguments"}
@@ -13,7 +14,6 @@ func CmdGetBindKey(args []any_t) []any_t {
 	fnc := readline.GetBindKey(key)
 	if fnc != nil {
 		return []any_t{fmt.Sprint(fnc)}
-	} else {
-		return []any_t{nil}
 	}
+	return []any_t{nil}
 }
