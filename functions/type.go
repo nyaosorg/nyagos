3b013df22336fe8555bfbb19b221f74f5456d63a
Kaoru HAYAMA <iyahaya@nifty.com>
2018-06-21 07:08:12 +0900

    Fix type.go for golint

--- functions/type.go
+++ functions/type.go
@@ -2,6 +2,7 @@ package functions
 
 import "io"
 
+// Param is the parameter type for nyagos.xxxxxx which uses stdin/stdout/stderr/colored-console.
 type Param struct {
 	Args []interface{}
 	In   io.Reader
