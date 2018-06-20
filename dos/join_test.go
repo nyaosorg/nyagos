3e87cb2ef29e954787524b8060c1eea59b1454e4
Kaoru HAYAMA <iyahaya@nifty.com>
2018-06-21 06:39:20 +0900

    fix join_test.go for golint

--- dos/join_test.go
+++ dos/join_test.go
@@ -5,19 +5,19 @@ import (
 	"testing"
 )
 
-func testJoin_(a, b string) {
+func _testJoin(a, b string) {
 	fmt.Printf("'%s'+'%s' -> '%s'\n", a, b, Join(a, b))
 }
 
 func TestJoin(t *testing.T) {
-	testJoin_(`foo`, `bar`)
-	testJoin_(`foo`, `\bar`)
-	testJoin_(`c:`, `bar`)
-	testJoin_(`foo/`, `bar`)
-	testJoin_(`foo\`, `bar`)
-	testJoin_(`foo`, `c:bar`)
-	testJoin_(`foo`, `c:\bar`)
-	testJoin_(`c:foo`, `\bar`)
-	testJoin_(`c:foo`, `\\host\path\to`)
-	testJoin_(`\\host\path\to`, `c:foo`)
+	_testJoin(`foo`, `bar`)
+	_testJoin(`foo`, `\bar`)
+	_testJoin(`c:`, `bar`)
+	_testJoin(`foo/`, `bar`)
+	_testJoin(`foo\`, `bar`)
+	_testJoin(`foo`, `c:bar`)
+	_testJoin(`foo`, `c:\bar`)
+	_testJoin(`c:foo`, `\bar`)
+	_testJoin(`c:foo`, `\\host\path\to`)
+	_testJoin(`\\host\path\to`, `c:foo`)
 }
