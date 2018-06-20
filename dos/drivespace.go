9e08557c1e090ce0ecb4eee1ada075b290693333
Kaoru HAYAMA <iyahaya@nifty.com>
2018-06-21 06:46:24 +0900

    Fix drivespace.go for golint

--- dos/drivespace.go
+++ dos/drivespace.go
@@ -1,5 +1,7 @@
 package dos
 
+// GetDiskFreeSpace retunrs disk information.
+//   rootPathName - string like "C:"
 func GetDiskFreeSpace(rootPathName string) (free uint64, total uint64, totalFree uint64, err error) {
 	rc, err1 := getDiskFreeSpaceEx(rootPathName, &free, &total, &totalFree)
 	if rc == 0 {
