2d449965fdfb41c3195f97b8fc1ad4c23403334d
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-06-20 17:59:23 +0900

    Fix mains/bit32lua.go for golint

--- mains/bit32lua.go
+++ mains/bit32lua.go
@@ -5,7 +5,7 @@ import (
 	"github.com/zetamatta/nyagos/functions"
 )
 
-func SetupBit32Table(L *lua.LState) {
+func setupBit32Table(L *lua.LState) {
 	table := L.NewTable()
 	L.SetField(table, "band", L.NewFunction(lua2cmd(functions.CmdBitAnd)))
 	L.SetField(table, "bor", L.NewFunction(lua2cmd(functions.CmdBitOr)))
