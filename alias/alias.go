4e9fe09866cf2f484683d805c6205952b1c42201
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-06-27 14:18:57 +0900

    Fix for golint: alias/*.go

--- alias/alias.go
+++ alias/alias.go
@@ -19,47 +19,48 @@ type callableT interface {
 	Call(ctx context.Context, cmd *shell.Cmd) (int, error)
 }
 
-type AliasFunc struct {
+// Func is the type for string-type alias. It has a Call method
+type Func struct {
 	BaseStr string
 }
 
-func New(baseStr string) *AliasFunc {
-	return &AliasFunc{baseStr}
+// New is the constructor for Func
+func New(baseStr string) *Func {
+	return &Func{baseStr}
 }
 
-func (this *AliasFunc) String() string {
-	return this.BaseStr
+// String is the method to support fmt.Stringer
+func (f *Func) String() string {
+	return f.BaseStr
 }
 
-func (this *AliasFunc) Call(ctx context.Context, cmd *shell.Cmd) (next int, err error) {
+// Call is the method to support callableT and it calls the alias-function.
+func (f *Func) Call(ctx context.Context, cmd *shell.Cmd) (next int, err error) {
 	isReplaced := false
 	if dbg {
-		print("AliasFunc.Call('", cmd.Arg(0), "')\n")
+		print("Func.Call('", cmd.Arg(0), "')\n")
 	}
-	cmdline := paramMatch.ReplaceAllStringFunc(this.BaseStr, func(s string) string {
+	cmdline := paramMatch.ReplaceAllStringFunc(f.BaseStr, func(s string) string {
 		if s == "$~*" {
 			isReplaced = true
 			if cmd.Args() != nil && len(cmd.Args()) >= 2 {
 				return strings.Join(cmd.Args()[1:], " ")
-			} else {
-				return ""
 			}
+			return ""
 		} else if s == "$*" {
 			isReplaced = true
 			if cmd.Args() != nil && len(cmd.Args()) >= 2 {
 				return strings.Join(cmd.RawArgs()[1:], " ")
-			} else {
-				return ""
 			}
+			return ""
 		} else if len(s) >= 3 && s[0] == '$' && s[1] == '~' && unicode.IsDigit(rune(s[2])) {
 			i, err := strconv.ParseInt(s[2:], 10, 0)
 			if err == nil {
 				isReplaced = true
 				if 0 <= i && cmd.Args() != nil && int(i) < len(cmd.Args()) {
 					return cmd.Arg(int(i))
-				} else {
-					return ""
 				}
+				return ""
 			}
 		}
 		i, err := strconv.ParseInt(s[1:], 10, 0)
@@ -67,16 +68,15 @@ func (this *AliasFunc) Call(ctx context.Context, cmd *shell.Cmd) (next int, err 
 			isReplaced = true
 			if 0 <= i && cmd.Args() != nil && int(i) < len(cmd.Args()) {
 				return cmd.RawArg(int(i))
-			} else {
-				return ""
 			}
+			return ""
 		}
 		return s
 	})
 
 	if !isReplaced {
 		var buffer strings.Builder
-		buffer.WriteString(this.BaseStr)
+		buffer.WriteString(f.BaseStr)
 		for _, s := range cmd.RawArgs()[1:] {
 			fmt.Fprintf(&buffer, " %s", s)
 		}
@@ -89,9 +89,11 @@ func (this *AliasFunc) Call(ctx context.Context, cmd *shell.Cmd) (next int, err 
 	return
 }
 
+// Table is the ALL ALIAS table !
 var Table = map[string]callableT{}
 var paramMatch = regexp.MustCompile(`\$(\~)?(\*|[0-9]+)`)
 
+// AllNames returns all-alias names for completion
 func AllNames() []completion.Element {
 	names := make([]completion.Element, 0, len(Table))
 	for name1 := range Table {
@@ -118,6 +120,7 @@ func hook(ctx context.Context, cmd *shell.Cmd) (int, bool, error) {
 	return next, true, err
 }
 
+// Init is the package initializer which inserts hook-function into shell package.
 func Init() {
 	nextHook = shell.SetHook(hook)
 }
