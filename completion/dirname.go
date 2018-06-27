6c83d83ec04601fd30d90ec099b03a2a1a8e70b1
HAYAMA_Kaoru <iyahaya@nifty.com>
2018-06-27 14:30:56 +0900

    Fix for golint: completion/dirname.go, hook.go

--- completion/dirname.go
+++ completion/dirname.go
@@ -2,12 +2,11 @@ package completion
 
 import "strings"
 
-// Get dirname from path, but do not clean up path.
+// DirName returns a directory-part from path, but do not clean up path.
 func DirName(path string) string {
 	lastroot := strings.LastIndexAny(path, `\/:`)
 	if lastroot >= 0 {
 		return path[0:(lastroot + 1)]
-	} else {
-		return ""
 	}
+	return ""
 }
--- completion/hook.go
+++ completion/hook.go
@@ -5,15 +5,17 @@ import (
 	"github.com/zetamatta/nyagos/readline"
 )
 
-var command_listupper = []func() []Element{
+var commandListUpper = []func() []Element{
 	func() []Element { return listUpAllExecutableOnEnv("PATH") },
 	func() []Element { return listUpAllExecutableOnEnv("NYAGOSPATH") },
 }
 
+// AppendCommandLister is the function to append the environment variable name at seeing on command-name completion.
 func AppendCommandLister(f func() []Element) {
-	command_listupper = append(command_listupper, f)
+	commandListUpper = append(commandListUpper, f)
 }
 
+// HookToList is the slice for Completion-Hook functions for users.
 var HookToList = []func(context.Context, *readline.Buffer, *List) (*List, error){}
 
 func init() {
