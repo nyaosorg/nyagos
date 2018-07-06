9053780ac57bbeb7a4b241e9180272399d27a39b
Kaoru HAYAMA <iyahaya@nifty.com>
2018-07-07 07:53:48 +0900

    Add test for io.popen():lines()

--- t/iolua.lua
+++ t/iolua.lua
@@ -16,9 +16,29 @@ else
     print("NG: write-open,write-close,io-lines")
 end
 
-local fd,msg = io.open(":::","w")
+local fd,err = io.open(":::","w")
 if fd then
     print("NG: invalid-open")
 else
-    print("OK: invalid-open:",msg)
+    print("OK: invalid-open:",err)
+end
+
+local fd,err = io.open(tmpfn,"r")
+for line in fd:lines() do
+    if line == "HOGEHOGE" then
+        print("OK: ",line)
+    else
+        print("NG: ",line)
+    end
+end
+fd:close()
+
+local fd,err = io.popen("cmd.exe /c \"echo AHAHA\"","r")
+if fd then
+    for line in fd:lines() do
+        print("OK>",line)
+    end
+    fd:close()
+else
+    print("NG: ",err)
 end
