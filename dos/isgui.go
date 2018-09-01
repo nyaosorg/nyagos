cc9f2147c5ffb1c0467121efeb430da06b04a9ee
Kaoru HAYAMA <iyahaya@nifty.com>
2018-09-01 16:31:17 +0900

    Use "debug/pe" instead of vendor/github.com/hillu/go-pefile to judge executables are GUI application or not.
    Removed vendor directory.

--- dos/isgui.go
+++ dos/isgui.go
@@ -1,22 +1,25 @@
 package dos
 
 import (
-	"io/ioutil"
-
-	"github.com/hillu/go-pefile"
+	"debug/pe"
 )
 
+const _IMAGE_SUBSYSTEM_WINDOWS_GUI = 2
+
 func isGui(fname string) (bool, error) {
-	bin, err := ioutil.ReadFile(fname)
+	file, err := pe.Open(fname)
 	if err != nil {
 		return false, err
 	}
-	pe, err := pefile.Parse(bin)
-	if err != nil {
-		return false, err
+	defer file.Close()
+	opt := file.OptionalHeader
+	if opt32, ok := opt.(*pe.OptionalHeader32); ok {
+		return opt32.Subsystem == _IMAGE_SUBSYSTEM_WINDOWS_GUI, nil
+	} else if opt64, ok := opt.(*pe.OptionalHeader64); ok {
+		return opt64.Subsystem == _IMAGE_SUBSYSTEM_WINDOWS_GUI, nil
+	} else {
+		return false, nil
 	}
-	opt := pe.OptionalHeader
-	return (opt != nil && opt.Subsystem == pefile.IMAGE_SUBSYSTEM_WINDOWS_GUI), nil
 }
 
 // IsGui returns true if fname is Windows GUI Application
