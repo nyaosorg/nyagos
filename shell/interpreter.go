b6207b16f00ef785bd4667aaa64f16b51ab57c93
Kaoru HAYAMA <iyahaya@nifty.com>
2018-12-02 13:21:00 +0900

    shell/interpreter.go: Move syscall.* to interpreter_windows.go

--- shell/interpreter.go
+++ shell/interpreter.go
@@ -5,12 +5,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"reflect"
 	"strings"
 	"sync"
-	"syscall"
 
 	"github.com/zetamatta/go-findfile"
 
@@ -207,51 +205,11 @@ func (cmd *Cmd) spawnvpSilent(ctx context.Context) (int, error) {
 	if defined.DBG {
 		print("exec.LookPath(", cmd.args[0], ")==", fullpath, "\n")
 	}
+
 	if WildCardExpansionAlways {
 		cmd.args = findfile.Globs(cmd.args)
 	}
-	if cmd.UseShellExecute {
-		// GUI Application
-		cmdline := makeCmdline(cmd.args[1:], cmd.rawArgs[1:])
-		return 0, dos.ShellExecute("open", fullpath, cmdline, "")
-	}
-	if UseSourceRunBatch {
-		lowerName := strings.ToLower(cmd.args[0])
-		if strings.HasSuffix(lowerName, ".cmd") || strings.HasSuffix(lowerName, ".bat") {
-			rawargs := cmd.RawArgs()
-			args := make([]string, len(rawargs))
-			args[0] = encloseWithQuote(fullpath)
-			for i, end := 1, len(rawargs); i < end; i++ {
-				args[i] = rawargs[i]
-			}
-			// Batch files
-			return RawSource(args, ioutil.Discard, false, cmd.Stdin, cmd.Stdout, cmd.Stderr)
-		}
-	}
-
-	cmdline := makeCmdline(cmd.args, cmd.rawArgs)
-
-	procAttr := &os.ProcAttr{
-		Env:   os.Environ(),
-		Files: []*os.File{cmd.Stdin, cmd.Stdout, cmd.Stderr},
-		Sys:   &syscall.SysProcAttr{CmdLine: cmdline},
-	}
-
-	process, err := os.StartProcess(cmd.args[0], cmd.args[1:], procAttr)
-	if err != nil {
-		return 255, err
-	}
-	processState, err := process.Wait()
-	if err != nil {
-		return 254, err
-	}
-	if processState.Success() {
-		return 0, nil
-	}
-	if t, ok := processState.Sys().(syscall.WaitStatus); ok {
-		return t.ExitStatus(), nil
-	}
-	return 253, nil
+	return cmd.startProcess()
 }
 
 type AlreadyReportedError struct {
---
+++ shell/interpreter_windows.go
@@ -0,0 +1,55 @@
+package shell
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"syscall"
+
+	"github.com/zetamatta/nyagos/dos"
+)
+
+func (cmd *Cmd) startProcess() (int, error) {
+	if cmd.UseShellExecute {
+		// GUI Application
+		cmdline := makeCmdline(cmd.args[1:], cmd.rawArgs[1:])
+		return 0, dos.ShellExecute("open", cmd.args[0], cmdline, "")
+	}
+	if UseSourceRunBatch {
+		lowerName := strings.ToLower(cmd.args[0])
+		if strings.HasSuffix(lowerName, ".cmd") || strings.HasSuffix(lowerName, ".bat") {
+			rawargs := cmd.RawArgs()
+			args := make([]string, len(rawargs))
+			args[0] = encloseWithQuote(cmd.args[0])
+			for i, end := 1, len(rawargs); i < end; i++ {
+				args[i] = rawargs[i]
+			}
+			// Batch files
+			return RawSource(args, ioutil.Discard, false, cmd.Stdin, cmd.Stdout, cmd.Stderr)
+		}
+	}
+
+	cmdline := makeCmdline(cmd.args, cmd.rawArgs)
+
+	procAttr := &os.ProcAttr{
+		Env:   os.Environ(),
+		Files: []*os.File{cmd.Stdin, cmd.Stdout, cmd.Stderr},
+		Sys:   &syscall.SysProcAttr{CmdLine: cmdline},
+	}
+
+	process, err := os.StartProcess(cmd.args[0], cmd.args[1:], procAttr)
+	if err != nil {
+		return 255, err
+	}
+	processState, err := process.Wait()
+	if err != nil {
+		return 254, err
+	}
+	if processState.Success() {
+		return 0, nil
+	}
+	if t, ok := processState.Sys().(syscall.WaitStatus); ok {
+		return t.ExitStatus(), nil
+	}
+	return 253, nil
+}
