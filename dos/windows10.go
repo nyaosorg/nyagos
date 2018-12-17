e0712b8a627fcde72e43ab22ded72d68f9b70c89
Kaoru HAYAMA <iyahaya@nifty.com>
2018-12-18 00:57:01 +0900

    On dos/windows10.go, use "golang.org/x/sys/windows".SetConsoleMode...

--- dos/windows10.go
+++ dos/windows10.go
@@ -1,42 +1,28 @@
 package dos
 
 import (
-	"unsafe"
+	"golang.org/x/sys/windows"
 )
 
-// var kernel32 = syscall.NewLazyDLL("kernel32")
+const enableVirtualTerminalProcessing uint32 = 0x0004
 
-// const stdInputHandle = uintptr(1) + ^uintptr(10)
-const stdOutputHandle = uintptr(1) + ^uintptr(11)
-const stdErrorHandle = uintptr(1) + ^uintptr(12)
-const enableVirtualTerminalProcessing uintptr = 0x0004
-
-var procGetStdHandle = kernel32.NewProc("GetStdHandle")
-var procGetConsoleMode = kernel32.NewProc("GetConsoleMode")
-var procSetConsoleMode = kernel32.NewProc("SetConsoleMode")
-
-func doEnableVirtualTerminalProcessing(handle uintptr) (func(), error) {
-	var mode uintptr
-	console, _, _ := procGetStdHandle.Call(handle)
-	rc, _, err := procGetConsoleMode.Call(console, uintptr(unsafe.Pointer(&mode)))
-	if rc == 0 {
+func doEnableVirtualTerminalProcessing(console windows.Handle) (func(), error) {
+	var mode uint32
+	err := windows.GetConsoleMode(console, &mode)
+	if err != nil {
 		return func() {}, err
 	}
-	deferFunc := func() { procSetConsoleMode.Call(console, mode) }
-
-	rc, _, err = procSetConsoleMode.Call(console, mode|enableVirtualTerminalProcessing)
-	if rc == 0 {
-		return deferFunc, err
-	}
-	return deferFunc, nil
+	deferFunc := func() { windows.SetConsoleMode(console, mode) }
+	err = windows.SetConsoleMode(console, mode|enableVirtualTerminalProcessing)
+	return deferFunc, err
 }
 
 // EnableStdoutVirtualTerminalProcessing enables Windows10's native ESCAPE SEQUENCE support on STDOUT
 func EnableStdoutVirtualTerminalProcessing() (func(), error) {
-	return doEnableVirtualTerminalProcessing(stdOutputHandle)
+	return doEnableVirtualTerminalProcessing(windows.Stdout)
 }
 
 // EnableStderrVirtualTerminalProcessing enables Windows10's native ESCAPE SEQUENCE support on STDERR
 func EnableStderrVirtualTerminalProcessing() (func(), error) {
-	return doEnableVirtualTerminalProcessing(stdErrorHandle)
+	return doEnableVirtualTerminalProcessing(windows.Stderr)
 }
