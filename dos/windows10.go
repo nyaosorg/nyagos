939b971b853c78f1dc3d7e841f4e08fc833a2cc1
Kaoru HAYAMA <iyahaya@nifty.com>
2018-12-22 18:35:20 +0900

    ( Add dos.ChangeConsoleMode )

--- dos/windows10.go
+++ dos/windows10.go
@@ -4,25 +4,46 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-const enableVirtualTerminalProcessing uint32 = 0x0004
+type ModeOp interface {
+	Op(mode uint32) uint32
+}
 
-func doEnableVirtualTerminalProcessing(console windows.Handle) (func(), error) {
+type ModeReset uint32
+
+func (this ModeReset) Op(mode uint32) uint32 {
+	return mode &^ uint32(this)
+}
+
+type ModeSet uint32
+
+func (this ModeSet) Op(mode uint32) uint32 {
+	return mode | uint32(this)
+}
+
+func ChangeConsoleMode(console windows.Handle, ops ...ModeOp) (func(), error) {
 	var mode uint32
 	err := windows.GetConsoleMode(console, &mode)
 	if err != nil {
 		return func() {}, err
 	}
-	deferFunc := func() { windows.SetConsoleMode(console, mode) }
-	err = windows.SetConsoleMode(console, mode|enableVirtualTerminalProcessing)
-	return deferFunc, err
+	restore := func() { windows.SetConsoleMode(console, mode) }
+
+	newMode := mode
+	for _, op1 := range ops {
+		newMode = op1.Op(newMode)
+	}
+	err = windows.SetConsoleMode(console, newMode)
+	return restore, err
 }
+
+const enableVirtualTerminalProcessing uint32 = 0x0004
 
 // EnableStdoutVirtualTerminalProcessing enables Windows10's native ESCAPE SEQUENCE support on STDOUT
 func EnableStdoutVirtualTerminalProcessing() (func(), error) {
-	return doEnableVirtualTerminalProcessing(windows.Stdout)
+	return ChangeConsoleMode(windows.Stdout, ModeSet(enableVirtualTerminalProcessing))
 }
 
 // EnableStderrVirtualTerminalProcessing enables Windows10's native ESCAPE SEQUENCE support on STDERR
 func EnableStderrVirtualTerminalProcessing() (func(), error) {
-	return doEnableVirtualTerminalProcessing(windows.Stderr)
+	return ChangeConsoleMode(windows.Stderr, ModeSet(enableVirtualTerminalProcessing))
 }
