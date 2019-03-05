769c85e2b0931458852fd65ebf4eb20d1916bdba
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-03-05 21:51:51 +0900

    Revert "make.cmd: install goversioninfo.exe into %GOBIN% rather than the current folder"
    This reverts commit 8cf7e12a86f8a824bb3b29e0c5401554f84a48bf.

--- make.cmd
+++ make.cmd
@@ -80,8 +80,7 @@ function Go-Fmt{
 }
 
 function Make-SysO($version) {
-    $exepath = (Download-Exe "github.com/josephspurrier/goversioninfo/cmd/goversioninfo")
-    Write-Verbose "Use $exepath"
+    Download-Exe "github.com/josephspurrier/goversioninfo/cmd/goversioninfo" "goversioninfo.exe"
     if( $version -match "^\d+[\._]\d+[\._]\d+[\._]\d+$" ){
         $v = $version.Split("[\._]")
     }else{
@@ -92,7 +91,7 @@ function Make-SysO($version) {
     }
     Write-Verbose "version=$version"
 
-    & $exepath `
+    .\goversioninfo.exe `
         "-file-version=$version" `
         "-product-version=$version" `
         "-icon=Etc\nyagos.ico" `
@@ -109,21 +108,24 @@ function Make-SysO($version) {
 }
 
 
-function Download-Exe($url){
-    $exename = $url.Split("/")[-1] + ".exe"
-    $exepath = (Join-Path (Join-Path (go env GOPATH).Split(";")[0] "bin") $exename)
-
-    if( Test-Path $exepath ){
-        Write-Verbose "Found $exepath"
-        return $exepath
+function Download-Exe($url,$exename){
+    if( Test-Path $exename ){
+        Write-Verbose "Found $exename"
+        return
     }
     Write-Verbose "$exename not found."
     $private:GO111MODULE = $env:GO111MODULE
     $env:GO111MODULE = "off"
-    Write-Verbose "$ go get $url"
-    go get $url
+    Write-Verbose "$ go get -d $url"
+    go get -d $url
+    $workdir = (Join-Path (Join-Path (go env GOPATH).Split(";")[0] "src") $url)
+    $cwd = (Get-Location)
+    Set-Location $workdir
+    Write-Verbose "$ go build $exename on $workdir"
+    go build
+    Do-Copy $exename $cwd
+    Set-Location $cwd
     $env:GO111MODULE = $private:GO111MODULE
-    return $exepath
 }
 
 function Build([string]$version="",[string]$tags="",[string]$target="") {
