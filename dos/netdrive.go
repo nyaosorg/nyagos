72f8b3cf3c8bb41a0e9bc3e31284bc70e4410170
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-03-23 22:34:54 +0900

    Simplify dos/GetNetDrives

--- dos/netdrive.go
+++ dos/netdrive.go
@@ -1,33 +1,28 @@
 package dos
 
 import (
-	"fmt"
-	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
-var mpr = syscall.NewLazyDLL("mpr")
-var wNetGetConnectionW = mpr.NewProc("WNetGetConnectionW")
+var mpr = windows.NewLazyDLL("mpr")
+var procWNetGetConnectionW = mpr.NewProc("WNetGetConnectionW")
 
-func WNetGetConnection(localName string) (string, error) {
-	localNamePtr, localNameErr := syscall.UTF16PtrFromString(localName)
-	if localNameErr != nil {
-		return "", localNameErr
-	}
+func _WNetGetConnection(drive uint16) (string, error) {
+	localName := []uint16{drive, ':', 0}
 	var buffer [1024]uint16
 	size := uintptr(len(buffer))
 
-	rc, _, err := wNetGetConnectionW.Call(
-		uintptr(unsafe.Pointer(localNamePtr)),
+	rc, _, err := procWNetGetConnectionW.Call(
+		uintptr(unsafe.Pointer(&localName[0])),
 		uintptr(unsafe.Pointer(&buffer[0])),
 		uintptr(unsafe.Pointer(&size)))
 
 	if uint32(rc) != 0 {
 		return "", err
 	}
-	return syscall.UTF16ToString(buffer[:]), nil
+	return windows.UTF16ToString(buffer[:]), nil
 }
 
 type NetDrive struct {
@@ -43,7 +38,7 @@ func GetNetDrives() ([]*NetDrive, error) {
 	result := []*NetDrive{}
 	for _, d := range drives {
 		if d.Type == windows.DRIVE_REMOTE {
-			path, err := WNetGetConnection(fmt.Sprintf("%c:", d.Letter))
+			path, err := _WNetGetConnection(uint16(d.Letter))
 			if err == nil {
 				node := &NetDrive{Letter: d.Letter, Remote: path}
 				result = append(result, node)
