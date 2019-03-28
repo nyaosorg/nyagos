6f08da5a01dc4208c2f9380337cc765add402c27
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-03-28 20:23:33 +0900

    ( Implemented mains.luaLineFilter() from (*luaFilterStream).ReadLine )

--- mains/linefilter.go
+++ mains/linefilter.go
@@ -16,39 +16,40 @@ type luaFilterStream struct {
 	L Lua
 }
 
-func (lfs *luaFilterStream) ReadLine(ctx context.Context) (context.Context, string, error) {
-	ctx, line, err := lfs.Stream.ReadLine(ctx)
-	if err != nil {
-		return ctx, "", err
-	}
-
-	L := lfs.L
-
+func luaLineFilter(ctx context.Context, L Lua, line string) string {
 	stackPos := L.GetTop()
 	defer L.SetTop(stackPos)
 
 	nyagosTable, ok := L.GetGlobal("nyagos").(*lua.LTable)
 	if !ok {
-		return ctx, line, err
+		return line
 	}
 	luaFilter, ok := L.GetField(nyagosTable, "filter").(*lua.LFunction)
 	if !ok {
-		return ctx, line, err
+		return line
 	}
 
 	L.Push(luaFilter)
 	L.Push(lua.LString(line))
 	defer setContext(L, getContext(L))
 	setContext(L, ctx)
-	err = L.PCall(1, 1, nil)
+	err := L.PCall(1, 1, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		return ctx, line, nil
+		return line
 	}
 	newLine, ok := L.Get(-1).(lua.LString)
 	L.Pop(1)
 	if !ok {
-		return ctx, line, nil
+		return line
 	}
-	return ctx, string(newLine), nil
+	return string(newLine)
+}
+
+func (lfs *luaFilterStream) ReadLine(ctx context.Context) (context.Context, string, error) {
+	ctx, line, err := lfs.Stream.ReadLine(ctx)
+	if err != nil {
+		return ctx, "", err
+	}
+	return ctx, luaLineFilter(ctx, lfs.L, line), err
 }
