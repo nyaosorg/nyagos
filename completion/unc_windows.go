864765df75551f8aa1c6f45b0caaf17a412a29c5
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-04-28 15:43:50 +0900

    Caching UNCPath Completion

--- completion/unc_windows.go
+++ completion/unc_windows.go
@@ -11,31 +11,45 @@ import (
 var rxUNCPattern1 = regexp.MustCompile(`^\\\\[^\\/]*$`)
 var rxUNCPattern2 = regexp.MustCompile(`^(\\\\[^\\/]+)\\[^\\/]*$`)
 
+var cacheServerNames []string
+var cacheNodeNames = map[string][]string{}
+
 func uncComplete(str string) ([]Element, error) {
 	if rxUNCPattern1.MatchString(str) {
 		server := strings.ToUpper(str)
+		if cacheServerNames == nil {
+			dos.EachMachine(func(n *dos.NetResource) bool {
+				cacheServerNames = append(cacheServerNames, n.RemoteName())
+				return true
+			})
+		}
 		result := []Element{}
-		err := dos.EachMachine(func(n *dos.NetResource) bool {
-			server1 := n.RemoteName()
+		for _, server1 := range cacheServerNames {
 			if strings.HasPrefix(strings.ToUpper(server1), server) {
 				result = append(result, Element1(server1+`\`))
 			}
-			return true
-		})
-		return result, err
+		}
+		return result, nil
 	}
 	if m := rxUNCPattern2.FindStringSubmatch(str); m != nil {
 		server := m[1]
-		path := strings.ToUpper(str)
+		r, ok := cacheNodeNames[server]
+		if !ok {
+			r := []string{}
+			dos.EachMachineNode(server, func(n *dos.NetResource) bool {
+				r = append(r, n.RemoteName())
+				return true
+			})
+			cacheNodeNames[server] = r
+		}
 		result := []Element{}
-		err := dos.EachMachineNode(server, func(n *dos.NetResource) bool {
-			remoteName := n.RemoteName()
-			if strings.HasPrefix(strings.ToUpper(remoteName), path) {
-				result = append(result, Element1(remoteName+`\`))
+		path := strings.ToUpper(str)
+		for _, node := range r {
+			if strings.HasPrefix(strings.ToUpper(node), path) {
+				result = append(result, Element1(node+`\`))
 			}
-			return true
-		})
-		return result, err
+		}
+		return result, nil
 	}
 	return nil, errors.New("not support")
 }
