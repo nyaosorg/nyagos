f8d9d6a0b3d27b88f0a49ef90101f7998b7e8354
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-05-02 14:09:22 +0900

    (#288) Use ShellExecuteEx as api to run GUI application.

--- dos/shellexecute.go
+++ dos/shellexecute.go
@@ -8,8 +8,26 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+type _ShellExecuteInfo struct {
+	size          uint32
+	mask          uint32
+	hwnd          uintptr
+	verb          *uint16
+	file          *uint16
+	parameter     *uint16
+	directory     *uint16
+	show          int
+	instApp       uintptr
+	idList        uintptr
+	class         *uint16
+	keyClass      uintptr
+	hotkey        uint32
+	iconOrMonitor uintptr
+	hProcess      uintptr
+}
+
 var shell32 = windows.NewLazySystemDLL("shell32.dll")
-var procShellExecute = shell32.NewProc("ShellExecuteW")
+var procShellExecute = shell32.NewProc("ShellExecuteExW")
 
 const (
 	// EDIT is the action "edit" for ShellExecute
@@ -26,39 +44,49 @@ const (
 	RUNAS = "runas"
 )
 
-// ShellExecute calls ShellExecute-API: edit,explore,open and so on.
-func shellExecute(action string, path string, param string, directory string) error {
-	actionP, actionErr := windows.UTF16PtrFromString(action)
-	if actionErr != nil {
-		return actionErr
-	}
-	pathP, pathErr := windows.UTF16PtrFromString(path)
-	if pathErr != nil {
-		return pathErr
-	}
-	paramP, paramErr := windows.UTF16PtrFromString(param)
-	if paramErr != nil {
-		return paramErr
-	}
-	directoryP, directoryErr := windows.UTF16PtrFromString(directory)
-	if directoryErr != nil {
-		return directoryErr
-	}
-	status, _, err := procShellExecute.Call(
-		uintptr(0),
-		uintptr(unsafe.Pointer(actionP)),
-		uintptr(unsafe.Pointer(pathP)),
-		uintptr(unsafe.Pointer(paramP)),
-		uintptr(unsafe.Pointer(directoryP)),
-		windows.SW_SHOWNORMAL)
+const (
+	_SEE_MASK_UNICODE = 0x4000
+)
 
-	if status <= 32 {
+// ShellExecute calls ShellExecute-API: edit,explore,open and so on.
+func shellExecute(action string, path string, param string, directory string) (err error) {
+	var p _ShellExecuteInfo
+
+	p.size = uint32(unsafe.Sizeof(p))
+
+	p.mask = _SEE_MASK_UNICODE
+
+	p.verb, err = windows.UTF16PtrFromString(action)
+	if err != nil {
+		return err
+	}
+	p.file, err = windows.UTF16PtrFromString(path)
+	if err != nil {
+		return err
+	}
+	p.parameter, err = windows.UTF16PtrFromString(param)
+	if err != nil {
+		return err
+	}
+	p.directory, err = windows.UTF16PtrFromString(directory)
+	if err != nil {
+		return err
+	}
+
+	p.show = 1
+
+	status, _, err := procShellExecute.Call(uintptr(unsafe.Pointer(&p)))
+
+	if status == 0 {
+		// ShellExecute and ShellExecuteExA's error is lower than 32
+		// But, ShellExecuteExW's error is FALSE.
+
 		if err != nil {
 			return err
 		} else if err = windows.GetLastError(); err != nil {
 			return err
 		} else {
-			return fmt.Errorf("Error(%d) in ShellExecuteW()", status)
+			return fmt.Errorf("Error(%d) in ShellExecuteExW()", status)
 		}
 	}
 	return nil
