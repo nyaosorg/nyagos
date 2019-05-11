239586c1e820cafb5a60ca71da98901732a81f3e
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-05-11 20:58:02 +0900

    Add option --chdir=

--- frame/option_windows.go
+++ frame/option_windows.go
@@ -1,21 +1,28 @@
 package frame
 
 import (
-	"github.com/zetamatta/nyagos/dos"
+	"os"
 	"strings"
+
+	"github.com/zetamatta/nyagos/dos"
 )
 
 func tryOsOption(arg string) bool {
-	const option = "--netuse="
+	const netuse_opt = "--netuse="
+	const chdir_opt = "--chdir="
 
-	if !strings.HasPrefix(arg, option) {
+	if strings.HasPrefix(arg, netuse_opt) {
+		arg = arg[len(netuse_opt):]
+		piece := strings.SplitN(arg, "=", 2)
+		if len(piece) != 2 {
+			return false
+		}
+		dos.NetUse(piece[0], piece[1])
+		return true
+	} else if strings.HasPrefix(arg, chdir_opt) {
+		os.Chdir(arg[len(chdir_opt):])
+		return true
+	} else {
 		return false
 	}
-	arg = arg[len(option):]
-	piece := strings.SplitN(arg, "=", 2)
-	if len(piece) != 2 {
-		return false
-	}
-	dos.NetUse(piece[0], piece[1])
-	return true
 }
