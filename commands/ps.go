7883778bf4a7b9a96195aa1226add8e33b4acfa0
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-05-14 07:57:48 +0900

    ps: print [self] on the line where nyagos.exe itself exists

--- commands/ps.go
+++ commands/ps.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/mitchellh/go-ps"
 )
@@ -13,8 +14,14 @@ func cmdPs(ctx context.Context, cmd Param) (int, error) {
 		return 1, err
 	}
 	fmt.Fprintf(cmd.Out(), "%6s %6s %s\n", "PID", "PPID", "COMMAND")
+	self := os.Getpid()
 	for _, p := range processes {
-		fmt.Fprintf(cmd.Out(), "%6d %6d %s\n", p.Pid(), p.PPid(), p.Executable())
+		fmt.Fprintf(cmd.Out(), "%6d %6d %s", p.Pid(), p.PPid(), p.Executable())
+		if self == p.Pid() {
+			fmt.Fprintln(cmd.Out(), " [self]")
+		} else {
+			fmt.Fprintln(cmd.Out())
+		}
 	}
 	return 0, nil
 }
