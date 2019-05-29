e3e6adf3da1383a1abf426d479adba4941a6f9e5
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-05-29 09:08:18 +0900

    t\utf8.lua: improbe test to show detail

--- t/utf8.lua
+++ t/utf8.lua
@@ -1,11 +1,19 @@
 local sample = "あいうえお"
-assert( utf8.len(sample) == 5)
+local r = utf8.len(sample)
+print( "len('"..sample.."')==",r)
+assert( r == 5)
 local r1,r2 = utf8.len(sample,3,-4)
 print(r1,r2)
-assert( r1 == 3)
+assert( r1 == nil and r2 == 3)
 
-assert( utf8.len(sample,1,-1) == 5)
-assert( utf8.len(sample,1,-2) == 5)
+local r1,r2 = utf8.len(sample,1,-1)
+print(r1,r2)
+assert( r1 == 5)
+
+local r1,r2 = utf8.len(sample,1,-2)
+print(r1,r2)
+assert( r1 == 5)
+
 assert( utf8.len(sample,1,-3) == 5)
 assert( utf8.len(sample,1,-4) == 4)
 assert( utf8.len(sample,1,-5) == 4)
