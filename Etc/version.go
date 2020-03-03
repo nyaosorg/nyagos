c70644e1e0e05f2fbaa5816caef16749e6640145
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-04 01:46:43 +0900

    Fix: Etc/mkversioninfo.go was called on Linux

--- Etc/version.go
+++ Etc/version.go
@@ -1,3 +1,5 @@
+// +build windows
+
 package etc
 
 //go:generate cmd /c go run mkversioninfo.go version.txt version.txt < versioninfo.json > v.json && go run github.com/josephspurrier/goversioninfo/cmd/goversioninfo -icon=nyagos.ico -o ..\nyagos.syso v.json && del v.json
