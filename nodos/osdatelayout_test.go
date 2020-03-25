ae28f578b589321b8fd7a112ff422667deb80184
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-03-25 12:11:41 +0900

    Wrote unit-test for "nodos".OsDateFormat

---
+++ nodos/osdatelayout_test.go
@@ -0,0 +1,34 @@
+package nodos
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+)
+
+func testOsDateLayout() error {
+	d, err := OsDateLayout()
+	if err != nil {
+		return fmt.Errorf("OsDatelayout: %s", err)
+	}
+	expectBin, err := exec.Command("cmd.exe", "/c", "echo", "%DATE%").Output()
+	if err != nil {
+		return fmt.Errorf("exec.Command: %w", err)
+	}
+	expect := strings.TrimSpace(string(expectBin))
+	result := time.Now().Format(d)
+	//println("expect:",expect)
+	//println("result:",result)
+	if expect != result {
+		return fmt.Errorf("OsDatelayout: differs: '%s' != '%s'", expect, result)
+	}
+	return err
+}
+
+func TestOsDatelayout(t *testing.T) {
+	if err := testOsDateLayout(); err != nil {
+		t.Fatal(err.Error())
+	}
+}
