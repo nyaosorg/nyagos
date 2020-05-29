e0daafa3cf2cd9efa77fd1e1ed47c7ac30238a98
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-05-29 21:26:21 +0900

    Fix: change drive(`x:`) did not push the last directory to directory history

--- commands/commands.go
+++ commands/commands.go
@@ -40,6 +40,7 @@ var backslashPattern = regexp.MustCompile(`^\\(\w*)$`)
 func Exec(ctx context.Context, cmd Param) (int, bool, error) {
 	name := strings.ToLower(cmd.Arg(0))
 	if len(name) == 2 && strings.HasSuffix(name, ":") {
+		pushCdHistory()
 		_, err := nodos.Chdrive(name)
 		return 0, true, err
 	}
