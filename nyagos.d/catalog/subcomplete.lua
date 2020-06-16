7649225b9c167643dafc287ffd8ca197c304ba40
HAYAMA_Kaoru <iyahaya@nifty.com>
2020-06-17 00:31:03 +0900

    Caching subcommands to complete

--- nyagos.d/catalog/subcomplete.lua
+++ nyagos.d/catalog/subcomplete.lua
@@ -9,113 +9,168 @@ end
 
 share.maincmds = {}
 
+function get_local()
+    local dir=nyagos.pathjoin(nyagos.env.LOCALAPPDATA,"NYAOS_ORG")
+    local stat=nyagos.stat(dir)
+    if not stat then
+        nyagos.exec('mkdir "'..dir..'"')
+    end
+    return dir
+end
+
+function load_subcommands_cache(fname)
+    fname = nyagos.pathjoin(get_local(),fname)
+    local fd=io.open(fname,"r")
+    if not fd then
+        return nil
+    end
+    local list = {}
+    for line in fd:lines() do
+        list[#list+1] = line
+    end
+    fd:close()
+    return list
+end
+
+function save_subcommands_cache(fname,list)
+    fname = nyagos.pathjoin(get_local(),fname)
+    local fd=io.open(fname,"w")
+    if not fd then
+        return
+    end
+    for i=1,#list do
+        fd:write(list[i].."\n")
+    end
+    fd:close()
+end
+
 -- git
-local githelp=io.popen("git help -a 2>nul","r")
-local hubhelp=io.popen("hub help -a 2>nul","r")
-if githelp then
-    local gitcmds={}
-    local hub=false
-    if hubhelp then
-      hub=true
-      local startflag = false
-      local found=false
-      for line in hubhelp:lines() do
-        if not found then
-          if startflag then
-            -- skip blank line
-            if string.match(line,"%S") then
-              -- found commands
-              for word in string.gmatch(line, "%S+") do
-                gitcmds[ #gitcmds+1 ] = word
+
+share.maincmds["git"] = load_subcommands_cache("git-subcommands.txt")
+share.maincmds["hub"] = load_subcommands_cache("hub-subcommands.txt")
+if not share.maincmds["git"] then
+    local githelp=io.popen("git help -a 2>nul","r")
+    local hubhelp=io.popen("hub help -a 2>nul","r")
+    if githelp then
+        local gitcmds={}
+        local hub=false
+        if hubhelp then
+          hub=true
+          local startflag = false
+          local found=false
+          for line in hubhelp:lines() do
+            if not found then
+              if startflag then
+                -- skip blank line
+                if string.match(line,"%S") then
+                  -- found commands
+                  for word in string.gmatch(line, "%S+") do
+                    gitcmds[ #gitcmds+1 ] = word
+                  end
+                  found = true
+                end
               end
-              found = true
+              if string.match(line,"hub custom") then
+                startflag = true
+              end
             end
           end
-          if string.match(line,"hub custom") then
-            startflag = true
-          end
+          hubhelp:close()
         end
-      end
-      hubhelp:close()
-    end
-    for line in githelp:lines() do
-        local word = string.match(line,"^ +(%S+)")
-        if nil ~= word then
-          gitcmds[ #gitcmds+1 ] = word
+        for line in githelp:lines() do
+            local word = string.match(line,"^ +(%S+)")
+            if nil ~= word then
+              gitcmds[ #gitcmds+1 ] = word
+            end
         end
-    end
-    githelp:close()
-    if #gitcmds > 1 then
-        local maincmds = share.maincmds
-        maincmds["git"] = gitcmds
-        if hub then
-          maincmds["hub"] = gitcmds
+        githelp:close()
+        if #gitcmds > 1 then
+            local maincmds = share.maincmds
+            maincmds["git"] = gitcmds
+            save_subcommands_cache("git-subcommands.txt",gitcmds)
+            if hub then
+              maincmds["hub"] = gitcmds
+              save_subcommands_cache("hub-subcommands.txt",gitcmds)
+            end
+            share.maincmds = maincmds
         end
-        share.maincmds = maincmds
     end
 end
 
 -- Subversion
-local svnhelp=nyagos.eval("svn help 2>nul","r")
-if string.len(svnhelp) > 5 then
-    local svncmds={}
-    for line in string.gmatch(svnhelp,"[^\n]+") do
-        local m=string.match(line,"^ +([a-z]+)")
-        if m then
-            svncmds[ #svncmds+1 ] = m
+share.maincmds["svn"] = load_subcommands_cache("svn-subcommands.txt")
+if not share.maincmds["svn"] then
+    local svnhelp=nyagos.eval("svn help 2>nul","r")
+    if string.len(svnhelp) > 5 then
+        local svncmds={}
+        for line in string.gmatch(svnhelp,"[^\n]+") do
+            local m=string.match(line,"^ +([a-z]+)")
+            if m then
+                svncmds[ #svncmds+1 ] = m
+            end
         end
-    end
-    if #svncmds > 1 then
-        local maincmds = share.maincmds
-        maincmds["svn"] = svncmds
-        share.maincmds = maincmds
+        if #svncmds > 1 then
+            local maincmds = share.maincmds
+            maincmds["svn"] = svncmds
+            share.maincmds = maincmds
+            save_subcommands_cache("svn-subcommands.txt",svncmds)
+        end
     end
 end
 
 -- Mercurial
-local hghelp=nyagos.eval("hg debugcomplete 2>nul","r")
-if string.len(hghelp) > 5 then
-    local hgcmds={}
-    for line in string.gmatch(hghelp,"[^\n]+") do
-        for word in string.gmatch(line,"[a-z]+") do
-            hgcmds[#hgcmds+1] = word
+share.maincmds["hg"] = load_subcommands_cache("hg-subcommands.txt")
+if not share.maincmds["hg"] then
+    local hghelp=nyagos.eval("hg debugcomplete 2>nul","r")
+    if string.len(hghelp) > 5 then
+        local hgcmds={}
+        for line in string.gmatch(hghelp,"[^\n]+") do
+            for word in string.gmatch(line,"[a-z]+") do
+                hgcmds[#hgcmds+1] = word
+            end
         end
-    end
-    if #hgcmds > 1 then
-        local maincmds=share.maincmds
-        maincmds["hg"] = hgcmds
-        share.maincmds = maincmds
+        if #hgcmds > 1 then
+            local maincmds=share.maincmds
+            maincmds["hg"] = hgcmds
+            share.maincmds = maincmds
+            save_subcommands_cache("hg-subcommands.txt",hgcmds)
+        end
     end
 end
 
 -- Rclone
-local rclonehelp=io.popen("rclone --help 2>nul","r")
-if rclonehelp then
-  local rclonecmds={}
-  local startflag = false
-  local flagsfound=false
-  for line in rclonehelp:lines() do
-      if not flagsfound then
-          if string.match(line,"Available Commands:") then
-            startflag = true
-          end
-          if string.match(line,"Flags:") then
-              flagsfound = true
-          end
-          if startflag then
-            local m=string.match(line,"^%s+([a-z]+)")
-            if m then
-                rclonecmds[ #rclonecmds+1 ] = m
-            end
+
+share.maincmds["rclone"] = load_subcommands_cache("rclone-subcommands.txt")
+if not share.maincmds["rclone"] then
+    local rclonehelp=io.popen("rclone --help 2>nul","r")
+    if rclonehelp then
+      local rclonecmds={}
+      local startflag = false
+      local flagsfound=false
+      for line in rclonehelp:lines() do
+          if not flagsfound then
+              if string.match(line,"Available Commands:") then
+                startflag = true
+              end
+              if string.match(line,"Flags:") then
+                  flagsfound = true
+              end
+              if startflag then
+                local m=string.match(line,"^%s+([a-z]+)")
+                if m then
+                    rclonecmds[ #rclonecmds+1 ] = m
+                end
+              end
           end
       end
-  end
-  rclonehelp:close()
-  if #rclonecmds > 1 then
-      local maincmds=share.maincmds
-      maincmds["rclone"] = rclonecmds
-      share.maincmds = maincmds
-  end
+      rclonehelp:close()
+      if #rclonecmds > 1 then
+          local maincmds=share.maincmds
+          maincmds["rclone"] = rclonecmds
+          share.maincmds = maincmds
+          save_subcommands_cache("rclone-subcommands.txt",rclonecmds)
+      end
+    end
 end
 
 for cmd,subcmdData in pairs(share.maincmds or {}) do
