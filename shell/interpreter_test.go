bc7fc38ea8e9605c5cae1b6cda6b72a4bbe6f1db
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-05-31 00:45:38 +0900

    Rewrite: interpreter test

--- shell/interpreter_test.go
+++ shell/interpreter_test.go
@@ -1,17 +1,29 @@
-package shell
+package shell_test
 
 import (
 	"context"
-	"fmt"
+	"os"
 	"testing"
+	"path/filepath"
+
+	"github.com/zetamatta/nyagos/shell"
 )
 
 func TestInterpret(t *testing.T) {
 	ctx := context.Background()
-	_, err := New().Interpret(ctx, "ls | cat -n > hogehoge")
+	tempFilePath := filepath.Join(os.TempDir(),"hogehoge")
+	_, err := shell.New().Interpret(ctx, `cmd /c "echo 12345" > "%TEMP%\hogehoge"`)
 	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("no error")
+		t.Fatalf("Fail: %s",err.Error())
+	}
+
+	tempFileData,err := os.ReadFile(tempFilePath)
+	if err != nil {
+		t.Fatalf("Fail: `%s` not found",tempFilePath)
+	}
+	defer os.Remove(tempFilePath)
+	if data := string(tempFileData) ; data != "12345\r\n" {
+		t.Fatalf("Fail: %s's contents is expected as \"12345\\r\\n\", but %v",
+				tempFilePath, string(tempFileData))
 	}
 }
