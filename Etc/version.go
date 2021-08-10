8a235f0a643c8ebe98a3a85b11aae73e8517ea07
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-08-10 21:39:54 +0900

    Add go-generate-code for second icon (but disabled)

--- Etc/version.go
+++ Etc/version.go
@@ -2,4 +2,8 @@
 
 package etc
 
+// for default icon
 //go:generate cmd /c go run mkversioninfo.go version.txt version.txt < versioninfo.json > v.json && go run github.com/josephspurrier/goversioninfo/cmd/goversioninfo -icon=nyagos.ico -o ..\nyagos.syso v.json && del v.json
+
+// for second icon (disabled)
+////go:generate cmd /c go run mkversioninfo.go version.txt version.txt < versioninfo.json > v.json && go run github.com/josephspurrier/goversioninfo/cmd/goversioninfo -icon=nyagos32x32.ico -icon=nyagos16x16.ico -o ..\nyagos.syso v.json && del v.json
