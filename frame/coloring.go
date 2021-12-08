4a8fc5905ff2e4e80220c4a097717146acd8bf24
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-12-08 10:43:48 +0900

    Coloring: show & | > < with green

--- frame/coloring.go
+++ frame/coloring.go
@@ -48,6 +48,8 @@ func (s *_Coloring) Next(codepoint rune) int {
 		color = readline.Magenta
 	} else if (bits & optionBit) != 0 {
 		color = readline.Yellow
+	} else if codepoint == '&' || codepoint == '|' || codepoint == '<' || codepoint == '>' {
+		color = readline.Green
 	} else {
 		color = readline.White
 	}
