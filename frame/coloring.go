d63e5e7716a1daa0de6a6d8e99667ff4035f6fcc
HAYAMA_Kaoru <iyahaya@nifty.com>
2021-12-12 00:17:16 +0900

    Change the background-color of the Ideographic Space (U+3000) red

--- frame/coloring.go
+++ frame/coloring.go
@@ -40,6 +40,8 @@ func (s *_Coloring) Next(codepoint rune) int {
 	color := readline.White
 	if unicode.IsControl(codepoint) {
 		color = readline.Blue
+	} else if codepoint == '\u3000' {
+		color = 41
 	} else if (bits & percentBit) != 0 {
 		color = readline.Cyan
 	} else if (bits & backquotedBit) != 0 {
