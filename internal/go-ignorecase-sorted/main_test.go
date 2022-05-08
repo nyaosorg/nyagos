1e53c78f4a0955d69cb6b60f2a60ca86cebf96d9
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-05-08 11:19:58 +0900

    go-ignorecase-sorted: Add ascend test

--- internal/go-ignorecase-sorted/main_test.go
+++ internal/go-ignorecase-sorted/main_test.go
@@ -6,6 +6,34 @@ import (
 	"github.com/nyaosorg/nyagos/internal/go-ignorecase-sorted"
 )
 
+func TestAscend(t *testing.T) {
+	var dic ignoreCaseSorted.Dictionary[int]
+
+	dic.Set("x", 7)
+	dic.Set("y", 8)
+	dic.Set("Z", 9)
+
+	expect := []struct {
+		key   string
+		value int
+	}{
+		{key: "x", value: 7},
+		{key: "y", value: 8},
+		{key: "Z", value: 9},
+	}
+	for p := dic.Ascend(); p != nil; p = p.Next() {
+		if expect[0].key != p.Key {
+			t.Fatalf("'%s' != '%s'", expect[0].key, p.Key)
+			return
+		}
+		if expect[0].value != p.Value {
+			t.Fatalf("'%d' != '%d'", expect[0].value, p.Value)
+			return
+		}
+		expect = expect[1:]
+	}
+}
+
 func TestDesend(t *testing.T) {
 	var dic ignoreCaseSorted.Dictionary[int]
 
