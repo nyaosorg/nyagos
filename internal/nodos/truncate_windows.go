c877028517fa3b8872364a03fbd6ac98b0d6525a
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-11-05 14:13:56 +0900

    (#428) `rmdir /s` fails to remove symbolic link (reparse point)
    https://github.com/nyaosorg/nyagos/issues/428

--- internal/nodos/truncate_windows.go
+++ internal/nodos/truncate_windows.go
@@ -3,42 +3,51 @@ package nodos
 import (
 	"fmt"
 	"io"
-	"os"
 	"path/filepath"
 
 	"golang.org/x/sys/windows"
+
+	"github.com/nyaosorg/go-windows-findfile"
 )
 
 // Truncate is same as os.RemoveAll but report files to remove.
 func truncate(folder string, whenError func(string, error) bool, out io.Writer) error {
 	attr, err := GetFileAttributes(folder)
 	if err != nil {
-		return fmt.Errorf("%s: %w", folder, err)
+		return fmt.Errorf("%s: GetFileAttributes: %w", folder, err)
 	}
 	if (attr & REPARSE_POINT) == 0 {
 		// Only not junction, delete files under folder.
-		files, err := os.ReadDir(folder)
-		if err != nil {
-			return err
-		}
-		for _, f := range files {
+		subDirectories := []string{}
+		err1 := findfile.Walk(filepath.Join(folder, "*"), func(f *findfile.FileInfo) bool {
 			if f.Name() == "." || f.Name() == ".." {
-				continue
+				return true
 			}
 			fullpath := filepath.Join(folder, f.Name())
-			var err error
-			if f.IsDir() {
-				fmt.Fprintf(out, "%s\\\n", fullpath)
-				err = Truncate(fullpath, whenError, out)
+			if f.IsDir() || f.IsReparsePoint() {
+				subDirectories = append(subDirectories, fullpath)
 			} else {
 				fmt.Fprintln(out, fullpath)
 				SetFileAttributes(fullpath, windows.FILE_ATTRIBUTE_NORMAL)
-				err = windows.Unlink(fullpath)
-			}
-			if err != nil {
-				if whenError != nil && !whenError(fullpath, err) {
-					return fmt.Errorf("%s: %s", fullpath, err.Error())
+				if err = windows.Unlink(fullpath); err != nil {
+					if whenError != nil && !whenError(fullpath, err) {
+						err = fmt.Errorf("%s: windows.Unlink: %w", fullpath, err)
+						return false
+					}
 				}
+			}
+			return true
+		})
+		if err1 != nil {
+			return fmt.Errorf("%s: findfile.Walk: %w", folder, err1)
+		}
+		if err != nil {
+			return err
+		}
+		for _, subDirectory := range subDirectories {
+			fmt.Fprintf(out, "%s\\\n", subDirectory)
+			if err := Truncate(subDirectory, whenError, out); err != nil {
+				return err
 			}
 		}
 	}
@@ -46,7 +55,7 @@ func truncate(folder string, whenError func(string, error) bool, out io.Writer) 
 		SetFileAttributes(folder, attr&^windows.FILE_ATTRIBUTE_READONLY)
 	}
 	if err := windows.Rmdir(folder); err != nil {
-		return fmt.Errorf("%s: %w", folder, err)
+		return fmt.Errorf("%s: windows.Rmdir: %w", folder, err)
 	}
 	return nil
 }
