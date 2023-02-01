30d54269f97fb06ca906483c4e4beea2e8b1a2c9
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-02-02 07:51:36 +0900

    Use the constant "debug/pe".IMAGE_SUBSYSTEM_WINDOWS_GUI to judge GUI application or not

--- internal/nodos/isgui_windows.go
+++ internal/nodos/isgui_windows.go
@@ -4,8 +4,6 @@ import (
 	"debug/pe"
 )
 
-const imageSubsystemWindowsGui = 2
-
 func isGui(fname string) (bool, error) {
 	file, err := pe.Open(fname)
 	if err != nil {
@@ -14,9 +12,9 @@ func isGui(fname string) (bool, error) {
 	defer file.Close()
 	opt := file.OptionalHeader
 	if opt32, ok := opt.(*pe.OptionalHeader32); ok {
-		return opt32.Subsystem == imageSubsystemWindowsGui, nil
+		return opt32.Subsystem == pe.IMAGE_SUBSYSTEM_WINDOWS_GUI, nil
 	} else if opt64, ok := opt.(*pe.OptionalHeader64); ok {
-		return opt64.Subsystem == imageSubsystemWindowsGui, nil
+		return opt64.Subsystem == pe.IMAGE_SUBSYSTEM_WINDOWS_GUI, nil
 	} else {
 		return false, nil
 	}
