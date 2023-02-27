f754433f227d8a4b30bb0bb8717ed15d524f0ebf
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-02-28 01:04:28 +0900

    Fix: t/lua/tst_raweval.lua failed

--- t/lua/tst_raweval.lua
+++ t/lua/tst_raweval.lua
@@ -1,5 +1,16 @@
 -- lua_f t_raweval.lua --
-local x = nyagos.raweval('cmd','/c','echo AHAHA')
-if x ~= "AHAHA\r\n" then
+local expect
+local result
+
+if nyagos.env.OS == "Windows_NT" then
+    result = nyagos.raweval('cmd','/c','echo AHAHA')
+    expect = "AHAHA\r\n"
+else
+    result = nyagos.raweval('echo','AHAHA')
+    expect = "AHAHA\n"
+end
+
+if result ~= expect then
+    print(string.format("expect '%s', but '%s'",expect,result))
     os.exit(1)
 end
