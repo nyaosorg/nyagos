dc0d3d5b822a9404c30b3bb8c6a2c5371c59fab1
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-17 11:51:27 +0900

    Implement `skk{ export="ENVNAME" }`

--- internal/functions/skk.go
+++ internal/functions/skk.go
@@ -1,6 +1,9 @@
 package functions
 
 import (
+	"os"
+	"strings"
+
 	"github.com/nyaosorg/go-readline-ny"
 	"github.com/nyaosorg/go-readline-ny/keys"
 	"github.com/nyaosorg/go-readline-skk"
@@ -12,8 +15,12 @@ func CmdSkk(args []any) []any {
 	cfg := &skk.Config{
 		BindTo: readline.GlobalKeyMap,
 	}
+	var exportVar string
 	for len(args) > 0 {
 		if table, ok := args[0].(map[any]any); ok {
+			if value, okok := table["export"].(string); okok {
+				exportVar = value
+			}
 			if value, okok := table["user"].(string); okok {
 				cfg.UserJisyoPath = value
 				// println("user:", value)
@@ -48,5 +55,22 @@ func CmdSkk(args []any) []any {
 		return []any{nil, err.Error()}
 	}
 	onexit.Register(func() { skkMode.SaveUserJisyo() })
+	if exportVar != "" {
+		var buffer strings.Builder
+		for _, value := range cfg.SystemJisyoPaths {
+			if buffer.Len() > 0 {
+				buffer.WriteByte(';')
+			}
+			buffer.WriteString(value)
+		}
+		if cfg.UserJisyoPath != "" {
+			if buffer.Len() > 0 {
+				buffer.WriteByte(';')
+			}
+			buffer.WriteString("user=")
+			buffer.WriteString(cfg.UserJisyoPath)
+		}
+		os.Setenv(exportVar, buffer.String())
+	}
 	return []any{true}
 }
