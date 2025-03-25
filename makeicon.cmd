d2d9f3b1fa2aa2414a3562370e341773ded59856
HAYAMA_Kaoru <iyahaya@nifty.com>
2025-03-25 21:09:27 +0900

    makeicon.cmd: modified the title of the shortcut
    `NihongoYet...` -> `Nihongo Yet...`

--- makeicon.cmd
+++ makeicon.cmd
@@ -4,4 +4,4 @@ if "%INSTALLDIR%" == "" set "INSTALLDIR=%~dp0"
 if not exist "%INSTALLDIR%\nyagos.exe" set "INSTALLDIR=%~dp0"
 if not "%2" == "" set "INSTALLDIR=%2"
 
-powershell "$wsh=New-Object -Com WScript.Shell; $sc=$wsh.CreateShortcut([IO.Path]::Combine([Environment]::GetFolderPath('Desktop'),'NihongoYet Another GOing Shell.lnk')); $sc.TargetPath='%INSTALLDIR%\nyagos.exe'; $sc.WorkingDirectory='%USERPROFILE%' ; $sc.Description='Nihongo Yet Another GOing Shell'; $sc.HotKey='CTRL+ALT+N' ; $sc.Save();"
+powershell "$wsh=New-Object -Com WScript.Shell; $sc=$wsh.CreateShortcut([IO.Path]::Combine([Environment]::GetFolderPath('Desktop'),'Nihongo Yet Another GOing Shell.lnk')); $sc.TargetPath='%INSTALLDIR%\nyagos.exe'; $sc.WorkingDirectory='%USERPROFILE%' ; $sc.Description='Nihongo Yet Another GOing Shell'; $sc.HotKey='CTRL+ALT+N' ; $sc.Save();"
