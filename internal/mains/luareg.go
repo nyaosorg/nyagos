81ed98e2ced37aa57aa315d19d8fb7286b360613
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2025-11-17 20:59:41 +0900

    Implement `getLuaRegistry`, `setLuaRegistry` and `pushLuaRegistry` in `internal/mains`

---
+++ internal/mains/luareg.go
@@ -0,0 +1,41 @@
+package mains
+
+import (
+	"github.com/yuin/gopher-lua"
+)
+
+const readlineLuaRegistryKey = "nyagos.readline"
+
+func getLuaRegistry(L Lua, key string) any {
+	tbl, ok := L.Get(lua.RegistryIndex).(*lua.LTable)
+	if !ok {
+		return nil
+	}
+	ud, ok := L.GetField(tbl, key).(*lua.LUserData)
+	if !ok {
+		return nil
+	}
+	return ud.Value
+}
+
+func setLuaRegistry(L Lua, key string, value any) {
+	reg, ok := L.Get(lua.RegistryIndex).(*lua.LTable)
+	if !ok {
+		return
+	}
+	if value != nil {
+		ud := L.NewUserData()
+		ud.Value = value
+		L.SetField(reg, key, ud)
+	} else {
+		L.SetField(reg, key, lua.LNil)
+	}
+}
+
+func pushLuaRegistry(L Lua, key string, value any) func() {
+	orig := getLuaRegistry(L, key)
+	setLuaRegistry(L, key, value)
+	return func() {
+		setLuaRegistry(L, key, orig)
+	}
+}
